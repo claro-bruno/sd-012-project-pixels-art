80e4fd173de576d6f5d17751c51df9e9dd1af9a6
Armando <a.guerra58@hotmail.com>
2021-05-05 16:00:52 -0300

    3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.querySelectorAll(".color")[0].style.background="black"
-document.querySelectorAll(".color")[1].style.background="violet"
-document.querySelectorAll(".color")[2].style.background="salmon"
-document.querySelectorAll(".color")[3].style.background="yellow"
+document.querySelectorAll(".color")[0].style.background="black";
+document.querySelectorAll(".color")[1].style.background="violet";
+document.querySelectorAll(".color")[2].style.background="salmon";
+document.querySelectorAll(".color")[3].style.background="yellow";
+
+
