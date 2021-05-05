a45abeaea9c038e738cab9b15c5ec8f79aca33bc
RyanLaiber <ryanlaiber@gmail.com>
2021-05-05 18:54:23 -0300

    adiciona requisito 8 colore pixels ao clicar

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let cores = document.getElementsByClassName("color");
 let QuadroDePixels = document.getElementById("pixel-board");
 
 //funções para formar quadro de pixels
-
 function formaLinha(linhas){
     for (let linha = 0; linha < linhas; linha += 1){
         let divLinha = document.createElement("div");
@@ -27,6 +26,9 @@ function formaPixelsLinha (colunas){
 };
 formaPixelsLinha(colunas);
 
+//adiciona caminho para pixels
+let pixels = document.getElementsByClassName("pixel");
+
 //função de selecionar cor
 function selecionaCor(event){
     let corAntes = document.querySelector(".selected");
@@ -34,7 +36,19 @@ function selecionaCor(event){
     event.target.classList.add("selected");
 };
 
-//adiciona eventListener
+//adiciona eventListener às cores
 for (let cor of cores){
     cor.addEventListener("click", selecionaCor);
+};
+
+//função que muda a cor dos pixels
+function mudaCorPixel(event){
+    let corSelecionada = document.querySelector(".selected");
+    let corDoClick = corSelecionada.id;
+    event.target.style.backgroundColor = corDoClick;
+};
+
+//adiciona eventlistener nos pixels
+for (let element of pixels){
+    element.addEventListener("click", mudaCorPixel);
 };
