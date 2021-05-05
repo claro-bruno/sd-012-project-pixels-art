83e87f95f7f750928b112e783ff1d5f5d6c023cd
Thiago Prado <thiagopc_dev@hotmail.com>
2021-05-05 19:25:25 -0300

    Desafio 8

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function handleSelect(event) {
   }
   
   
+  function paintPaleta() {
+    let colors =['black','red','blue','yellow']
+    let paleta = document.getElementsByClassName('color');
+    for (let i = 0; i < paleta.length; i += 1){
+        paleta[i].style.backgroundColor = colors[i];
+    }
+  }
+  paintPaleta();
+
+//Exercicio 8
+  function changePixelColor() {
+    let selectPixel = document.querySelector('.pixel');
+        let getColor = document.getElementsByClassName('color selected');
+        let SelectedColor = getColor[0].style.backgroundColor;
+    
+    selectPixel = addEventListener('click', (e) => {
+          e.target.style.backgroundColor = getColor[0].style.backgroundColor;
+      });
+      
+    }
+    changePixelColor();
