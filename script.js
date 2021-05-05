ef7f8665181027a0798c05b6614601d1ac02da71
Anderson B <andbmarques@gmail.com>
2021-05-05 15:08:53 -0300

    Requisito 4 e 5 adicionados

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    gerarPixelBoard();
+}
+
+function gerarPixelBoard(){
+    let pixelBoard = document.getElementById('pixel-board');
+
+    for(let i = 0; i < 5; i++){
+        
+        for(let j = 0; j < 5; j++){
+            let cell = document.createElement('div')
+            cell.style.border = '1px solid black'
+            cell.style.width = '40px'
+            cell.style.height = '40px'
+            cell.style.background = 'white'
+            cell.className = 'pixel'
+            pixelBoard.appendChild(cell)
+        }
+    }
+}
