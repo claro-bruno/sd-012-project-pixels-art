0ea6c105ad64b53b111852793359b2a94d950c6b
camila-mp <ca.malvp@gmail.com>
2021-05-05 17:21:08 -0300

    primeira tentativa de completar requisito 4 com JS

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let nomesDasCores = ['black', 'yellow', 'blue', 'green', 'red']
+//função que atribui ids e colore o fundo de cada cor
+let nomesDasCores = ['black', 'yellow', 'blue', 'green', 'red'] //background colors
 
 function idsStyleDivsCores() {
     let listaCores = document.getElementsByClassName('color'); //captura lista de cores html
     for (index = 0; index < listaCores.length; index += 1){
-        let cor = listaCores[index]; //captura cor html
-        cor.id = nomesDasCores[index]; //adiciona id de cada cor
-        cor.style.backgroundColor = nomesDasCores[index];
+        let cor = listaCores[index]; //captura div da cor no html
+        cor.id = nomesDasCores[index]; //adiciona id de cada div
+        cor.style.backgroundColor = nomesDasCores[index]; //adiciona cor de cada div
     };
 };
 
 idsStyleDivsCores();
+
+//função para criar o quadro de pixels
+let linhasQuadroPixels = document.getElementsByClassName('linha-pixel'); //lista linhas quadro
+
+function quadroPixels() {
+    for (index = 0; index < linhasQuadroPixels.length; index += 1){ //percorre cada linha
+        for (index = 0; index <linhasQuadroPixels.length; index += 1){ //cria pixels de cada linha
+            let pixel = document.createElement('div');
+            linhasQuadroPixels[index].appendChild(pixel);
+            pixel.className='pixel';
+            pixel.style.backgroundColor='white';
+        };
+    };
+};
+
+quadroPixels()
