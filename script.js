91b63fa294e1244c7ecd54df03e5b2d9a1559c67
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-05 16:31:59 -0300

    Refatorado todo o codigo

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const pixelBoard = document.querySelector('#pixel-board');
-const color = document.querySelectorAll('.color');
+const colorsToPick = ['black', 'blue', 'yellow', 'red'];
+const palette = document.querySelector('#color-palette');
 
+// Criando a paleta de cores
+function createPalette() {
+  for (let i = 0; i < 4; i += 1) {
+    const paletteDiv = document.createElement('div');
+    paletteDiv.classList.add('color');
+    if (i === 0) {
+      paletteDiv.classList.add('selected');
+    }
+    paletteDiv.style.backgroundColor = colorsToPick[i];
+    palette.appendChild(paletteDiv);
+  }
+}
+
+createPalette();
+
+// Adiciona quebra de linha para createBoard
 function addNewLine(j) {
   if (j === 4) {
     const br = document.createElement('br');
@@ -8,6 +25,7 @@ function addNewLine(j) {
   }
 }
 
+// Cria o Board
 function createBoard() {
   for (let i = 0; i < 5; i += 1) {
     for (let j = 0; j < 5; j += 1) {
@@ -21,11 +39,19 @@ function createBoard() {
 
 createBoard();
 
+// Adicionar e remover a classe selected da paleta de cores.
+const color = document.querySelectorAll('.color');
 for (let i = 0; i < color.length; i += 1) {
   color[i].addEventListener('click', () => {
+    console.log('aqui');
     const selected = document.querySelector('.selected');
     selected.classList.remove('selected');
     color[i].classList.add('selected');
-    console.log(color[i]);
+    console.log(color[i].css('background-color'));
   });
 }
+
+// const boardPaint = document.querySelectorAll('.pixel');
+// for (let i = 0; i < boardPaint.length; i += 1) {
+// boardPaint[i].addEventListener.
+// }
