583228c61256e4767686d176e0710a761b006d0c
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-05 16:27:03 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function paletaCores(cores) {
-    let getContainer = document.querySelectorAll('#color-palete');
-    for (let index = 0;index < cores.length; index += 1){
-        
-    }
-}
+let getContainer = document.querySelectorAll('#color-palete');
