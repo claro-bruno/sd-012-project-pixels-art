34d9c60f131ac1afca04e64c37791cf3ae28a882
Renan_Miyashita <renanmiyashita@gmail.com>
2021-05-05 15:53:12 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,13 @@
+let redSquare = document.querySelector('#color1');
+redSquare.style.backgroundColor = 'red'
+
+let blueSquare = document.querySelector('#color2');
+blueSquare.style.backgroundColor = 'blue'
+
+let yellowSquare = document.querySelector('#color3');
+yellowSquare.style.backgroundColor = 'yellow'
+
+let greenSquare = document.querySelector('#color4');
+greenSquare.style.backgroundColor = 'green'
+
+
