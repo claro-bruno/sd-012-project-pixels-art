bc9c22dc0a8e1dccccec097855b6f427d133f4e4
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-05 17:03:51 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const randomColors = document.querySelectorAll('.color');
+const colorizeBorder = 'green';
 
-console.log(randomColors);
+function selectedColorPrimary() {
+  const onloadBlackColor = document.querySelector('div', '.color', '.selected');
+  const pixels = document.querySelectorAll('.pixel');
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', () => {
+      pixels[index].style.background = 'black';
+    });
+  }
+  onloadBlackColor.style.borderColor = colorizeBorder;
+}
+
+window.onload = () => {
+  selectedColorPrimary();
+};
