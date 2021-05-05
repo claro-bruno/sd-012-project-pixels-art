60fcbea50f6ec19b0a168176831728d79e72d25b
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-05 16:32:26 -0300

    Resolve requisito 6

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const color1 = document.querySelectorAll('.color')[0];
+    const color2 = document.querySelectorAll('.color')[1];
+    const color3 = document.querySelectorAll('.color')[2];
+    const color4 = document.querySelectorAll('.color')[3];
+
+    color1.classList.add('selected');
+}
