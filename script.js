17a30c16be4c008fe0e0c055ae55c14e832b102a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 15:24:55 -0300

    adding the fourth requirement

---
+++ script.js
@@ -0,0 +1,21 @@
+const pixelBoard = document.querySelector('#pixel-board');
+
+let medidas = 5;
+
+function creatingPixelDivs (linhas) {
+    for (let row = 1; row <= linhas; row += 1) {
+        const divRow = document.createElement('div');
+        pixelBoard.appendChild(divRow);
+        creatingPixelDivsCollumns(divRow, linhas);
+    }
+}
+
+function creatingPixelDivsCollumns(row, colunas) {
+    for (let column = 1; column <= colunas; column += 1) {
+        const divColumn = document.createElement('div');
+        divColumn.classList.add('pixel');
+        row.appendChild(divColumn);
+    }
+}
+
+creatingPixelDivs(medidas);
