ab7fc69f7de66331537074ceaaf116fc7c8ea794
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-05 18:57:24 -0300

    questao 4

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-const pixel = document.getElementsByClassName('pixel1');
-const pixel = document.getElementsByClassName('pixel2');
-const pixel = document.getElementsByClassName('pixel3');
-const pixel = document.getElementsByClassName('pixel4');
-const pixel = document.getElementsByClassName('pixel5');
 
 
