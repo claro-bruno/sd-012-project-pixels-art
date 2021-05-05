c2df9d8cee5808468843a26485f2648d18d035cd
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-05-05 15:40:20 -0300

    Requesito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".color")[0].style.backgroundColor ="black";
+document.querySelectorAll(".color")[1].style.backgroundColor ="blue";
+document.querySelectorAll(".color")[2].style.backgroundColor ="red";
+document.querySelectorAll(".color")[3].style.backgroundColor ="green";
