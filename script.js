677dbb2eaa181b65f2a2048abcca3f3e6cbde1aa
Eric Faria <ericdeofaria@gmail.com>
2021-05-05 15:22:03 -0300

    Requisito 03 e correções de lint

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-window.onload = function() {
-    createPalette();
-};
-
 const colorPalette = document.getElementById('color-palette'); 
 
 function createPalette() {
-    
+  let div = document.createElement('div');
+  div.className = 'color';
+  div.style.backgroundColor = 'black';
+  colorPalette.appendChild(div);
+  for (let index = 0; index < 3; index += 1) {
     let div = document.createElement('div');
     div.className = 'color';
-    div.style.backgroundColor = 'black';
-    colorPalette.appendChild(div);
-    for (let index = 0; index < 3; index += 1) {
-        let div = document.createElement('div');
-        div.className = 'color';
-        div.style.backgroundColor = randomRGB();
-        colorPalette.appendChild(div); 
-    }
-
+    div.style.backgroundColor = randomRGB();
+    colorPalette.appendChild(div); 
+  }
 }
 
 function randomRGB() {
-    let r, g, b;
-    r = parseInt(Math.random() * 255);
-    g = parseInt(Math.random() * 255);
-    b = parseInt(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+  let r, g, b;
+  r = parseInt(Math.random() * 255);
+  g = parseInt(Math.random() * 255);
+  b = parseInt(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
 }
+
+window.onload = function pageLoad () {
+  createPalette();
+};
