b7bba4673a8b0e962bff3e7a0e3f13ab1655c91c
Stefano <stefanooliveira1@gmail.com>
2021-05-05 16:28:36 -0300

    Ex 3 - finaliza ex3

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+//ex2
 function paletaCores(nomeCores) {
     let getContainer = document.querySelector('#color-palette');
-    console.log(getContainer)
     for (let index = 0; index < nomeCores.length; index += 1) {
         let newColor = document.createElement('div');
-        console.log(newColor)
         newColor.className = 'color';
         newColor.style.backgroundColor = nomeCores[index];
         //newColor.innerText = nomeCores[index]; 
@@ -14,7 +13,23 @@ function paletaCores(nomeCores) {
 }
 
 let cores = ['blue', 'green', 'red', 'black'];
+
+let corSubs = [];
+//ex3
+function organizaPreto (coresE) {
+for (let i = 1; i < cores.length; i += 1) {
+    if (coresE[0] !== 'black') {
+        corSubs = cores[0];
+        coresE[0] = 'black';
+    }
+    if (coresE[i] === 'black') {
+        coresE[i] = corSubs
+    }
+}
+}
+
+organizaPreto(cores);
 paletaCores(cores);
 
-// ex3
+
 
