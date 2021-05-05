c92f5440c46f22bb1e99c941b51efd3df506c4aa
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-05 19:05:40 -0300

    correcao req 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let cores = document.getElementsByClassName("color");
-let paleta = document.getElementById("color-pallete");
+let paleta = document.getElementById("color-palette");
 
     paleta.children[0].style.backgroundColor = "blue";
     paleta.children[1].style.backgroundColor = "yellow";
