dce89f3a1ab0aafba113bbba89ce574cbbdc6293
matheus <matheus111904@hotmail.com>
2021-05-05 15:42:31 -0300

    teste requesito 2

---
+++ script.js
@@ -0,0 +1,8 @@
+let corPaleta = document.getElementsByClassName('color');
+function verificaCor (){
+    if (corPaleta === "white"){
+        return "Cor inválida!"
+    } else if (corPaleta === corPaleta){
+        return "Não pode cor repetida!"
+}
+verificaCor ()
