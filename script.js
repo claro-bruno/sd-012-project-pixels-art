1c85b63d535f6ebb82701a50d16725293a190693
André Moreno <deco616@hotmail.com>
2021-05-05 18:19:07 -0300

    Mudando class para pixel

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function gerarBoard(numero) {
     for (let index = 0; index < numero; index += 1){
         console.log(index);
         let primeiraDiv = document.createElement('div');
+        primeiraDiv.className = 'linha';
         pixelBoard.appendChild(primeiraDiv);
         for (let index2 = 0; index2 < numero; index2 += 1){
           let divInterna = document.createElement('div');
