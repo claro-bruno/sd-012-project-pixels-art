ce54c97892445818c44a8d1d1958150242efa34c
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-05 18:13:50 -0300

    adiciona tabela e escolha de tamanho de tabela

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let listaCores = document.getElementById('color-pallete');
+let listaCores = document.getElementById('color-palette');
 for (index = 0 ; index < 4; index +=1 ){
    let colorSquare = document.createElement('li');
    colorSquare.className = 'color';
@@ -10,8 +10,27 @@ let colorArray = [ 'black' ,'red', 'purple', 'yellow'];
 for(index = 0 ; index < colorList.length ; index +=1){
     colorList[index].style.backgroundColor = colorArray[index]
 
+
 }
 
-function createPixelBoard (){
+function createPixelBoard (number){
+    const pixelBoard = document.getElementById('pixel-board');
+    if(number > 50){
+        number = 50;
+    }
+    if (number < 5){
+        number = 5;
+    }
+    for (let indexColum = 0; indexColum < number; indexColum += 1) {
+      const pixelRow = document.createElement('div');
+      pixelRow.className = 'pixelRow';
+      pixelBoard.appendChild(pixelRow);
     
+      for (let indexLine = 0; indexLine < number; indexLine += 1) {
+        const pixelCell = document.createElement('div');
+        pixelCell.className = 'pixel';
+        pixelRow.appendChild(pixelCell);
+      }
+    }
 }
+createPixelBoard(5);
