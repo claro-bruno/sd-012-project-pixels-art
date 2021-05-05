7f3bf33b31929861de2ee4ba09ec6cd167820dc5
José Carlos <kalarruda@hotmail.com>
2021-05-05 15:24:15 -0300

    2-desafio

---
+++ script.js
@@ -0,0 +1,6 @@
+let coresPaleta = document.querySelectorAll('.color');
+console.log(coresPaleta);
+coresPaleta[0].style.backgroundColor = 'black';
+coresPaleta[1].style.backgroundColor = 'red';
+coresPaleta[2].style.backgroundColor = 'blue';
+coresPaleta[3].style.backgroundColor = 'green';
