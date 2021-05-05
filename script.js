2297dc080f337bea05d9866854852a96339152bc
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-05 17:53:43 -0300

    verificando erro do npx cypress open

---
+++ script.js
@@ -0,0 +1,7 @@
+const pixel = document.getElementsByClassName('pixel1');
+const pixel = document.getElementsByClassName('pixel2');
+const pixel = document.getElementsByClassName('pixel3');
+const pixel = document.getElementsByClassName('pixel4');
+const pixel = document.getElementsByClassName('pixel5');
+
+
