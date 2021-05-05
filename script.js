4e888ca34dfa74c4e8c144297ff2c8c1dba7145d
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-05 16:34:24 -0300

    Satisfaz requisito 8

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const colors = document.getElementsByClassName('color');
-
-for (let index = 0; index < colors.length; index += 1) {
-  colors[index].addEventListener('click', select);
-}
-
 function select(e) {
   const currentColor = document.querySelector('.selected');
   currentColor.classList.remove('selected');
 
   const colorSelect = e.target;
   colorSelect.classList.add('selected');
+}
+
+function paint(e) {
+  const currentPixel = e.target;
+  const currentColor = document.querySelector('.selected').style.backgroundColor;
+  currentPixel.style.backgroundColor = currentColor;
+}
+
+const color = document.getElementsByClassName('color');
+const pixel = document.getElementsByClassName('pixel');
+
+for (let index = 0; index < color.length; index += 1) {
+  color[index].addEventListener('click', select);
+}
+
+for (let index = 0; index < pixel.length; index += 1) {
+  pixel[index].addEventListener('click', paint);
 }
