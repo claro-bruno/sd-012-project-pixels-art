ae54cdd8bc9c481a48a50876ce59b95ff0372efe
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-05 16:48:47 -0300

    Feitos requisito 2 e 3

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const colors = document.getElementsByClassName('color');
 
+function inputColorsPalett(arrayElements, arrayColors) {
+  for (let index = 0; index < arrayElements.length; index += 1) {
+    console.log(arrayColors[index]);
+    arrayElements[index].style.backgroundColor = arrayColors[index];
+  }
+}
+
+const RGBColors = ['#000000', '#800000', '#008000', '#000080'];
+
+inputColorsPalett(colors, RGBColors);
