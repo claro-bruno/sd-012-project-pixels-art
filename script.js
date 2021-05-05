3c539d7106b0c75a804c3653a64df33d2c98fd4a
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-05 17:30:22 -0300

    Resolve requisito 4

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 // Cria paleta de cores dinamicamente:
-let numberOfColors = 4;
+const numberOfColors = 4;
 function createPalleteColors(number) {
-    for (let index = 0; index < number; index += 1) {
-        let pallete = document.getElementById("color-palette");
-        let elementPallete = document.createElement('div');
-        elementPallete.className = 'color';
-        pallete.appendChild(elementPallete);
-    }
+  for (let index = 0; index < number; index += 1) {
+    const pallete = document.getElementById('color-palette');
+    const elementPallete = document.createElement('div');
+    elementPallete.className = 'color';
+    pallete.appendChild(elementPallete);
+  }
 }
 createPalleteColors(numberOfColors);
 // Adiciona Cores dinamicamente a paleta de cores:
-let palleteColors = ['black', 'lime', 'aqua', 'deeppink']; // Array com cores para serem adicionadas.
+const paletteColors = ['black', 'lime', 'aqua', 'deeppink']; // Array com cores para serem adicionadas.
 function addColors(arrColors) {
-    let colorPallete = document.getElementsByClassName('color');
-    for (let index = 0; index < colorPallete.length; index += 1) {
-        colorPallete[index].style.backgroundColor = arrColors[index];
+  const colorPallete = document.getElementsByClassName('color');
+  for (let index = 0; index < colorPallete.length; index += 1) {
+    colorPallete[index].style.backgroundColor = arrColors[index];
+  }
 }
+addColors(paletteColors);
+// Cria função que adiciona pixel
+function createPixelFrame() {
+  const board = document.getElementById('pixel-board');
+  const elementBoard = document.createElement('div');
+  elementBoard.className = 'pixel';
+  board.appendChild(elementBoard);
 }
-addColors(palleteColors);
+// Cria Quadro de pixels
+const pixelBoardBase = 5;
+for (let lineIndex = 0; lineIndex < pixelBoardBase; lineIndex += 1) {
+  for (let index = 0; index < pixelBoardBase; index += 1) {
+    createPixelFrame();
+  }
+  const lineBreak = document.createElement('br');
+  document.getElementById('pixel-board').appendChild(lineBreak);
+}
