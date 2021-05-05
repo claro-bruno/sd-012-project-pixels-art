d7b40d7477e5ffbba142a7960b7b0430fc4bfdec
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-05-05 16:39:07 -0300

    concluindo requisto 02: Atribuindo uma cor para cada bloco com JS

---
+++ script.js
@@ -0,0 +1,5 @@
+let cores = document.querySelectorAll(".color")
+cores[0].style.background = "black";
+cores[1].style.background = "red";
+cores[2].style.background = "blue";
+cores[3].style.background = "green";
