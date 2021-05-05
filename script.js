3608b4d818d9772d2aa2be3b1f55a53608887c56
Patrick Dack <patrickdack89@gmail.com>
2021-05-05 16:10:06 -0300

    Requisito 2 - Adicionado cores para todas a div colors, e ajuste refente a borda das divs.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 window.onload = () => {
 let color1 = document.getElementsByClassName('color')[0];
 color1.style.backgroundColor = 'black';
+let color2 = document.getElementsByClassName('color')[1];
+color2.style.backgroundColor = 'red';
+let color3 = document.getElementsByClassName('color')[2];
+color3.style.backgroundColor = 'blue';
+let color4 = document.getElementsByClassName('color')[3];
+color4.style.backgroundColor = 'green';
 
 
 
