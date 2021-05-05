e9d2e1d88682def8a6f305bacf91f8c907107c2c
Sass <dudu_sass12@hotmail.com>
2021-05-05 20:21:45 -0300

    tentando consertar o erro

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let colorblack = document.getElementByClassName('.color');
-colorblack.classlist.add("selected");
-
 window.onload = function BlackColor() {
-  let colorblack = document.querySelectorAll(".color")[0];
+  let colorblack = document.getElementsByClassName(".color");
   for (let index = 0; index < colorblack.length; index+=1 ) { 
   colorblack[index].classList.remove('selected');
   }
   colorblack.classList.add("selected");
   }
   BlackColor();
+
+  //exercicio 9 
+  function clearBoard() {
+    let pixels = document.querySelectorAll('.pixel');
+    for (let key of pixels) {
+      //key.document.style.background = 'white';
+    }
+  }
+
+  function Clear() {
+    let Button = document.getElementById('clear-board');
+    Button.addEventListener('click',clearBoard)
+  }
