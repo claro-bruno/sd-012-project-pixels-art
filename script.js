11e7b4400a9dfc950d949cf05aa87320a2c9d14f
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-05 17:56:45 -0300

    ajuste lint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function paletteGenerator() {
-  let colors = ['black', 'green', 'blue', 'orange'];
-  let paletteSection = document.getElementById('color-palette');
+  const colors = ['black', 'green', 'blue', 'orange'];
+  const paletteSection = document.getElementById('color-palette');
 
   for (let index = 0; index < colors.length; index += 1) {
     let createDiv = document.createElement('div');
@@ -15,16 +15,14 @@ function paletteGenerator() {
 paletteGenerator();
 
 function tableGenerator() {
-  let canvasSize = 5;
-  let tableTag = document.querySelector('table');
+  const canvasSize = 5;
+  const tableTag = document.querySelector('table');
 
   for (let index = 0; index < canvasSize; index += 1) {
-    let tableRow = document.createElement('tr')
+    const tableRow = document.createElement('tr');
     tableTag.appendChild(tableRow);
     tableRow.style.backgroundColor = 'white';
-
   }
-
   let tableRows = tableTag.children;
   for (let indexLine = 0; indexLine < tableRows.length; indexLine += 1) {
     for (let index = 0; index < canvasSize; index += 1) {
