815c2fc967373fb42b1e304f1e0997d17dbef360
Nuwanda <morato_dev@protonmail.com>
2021-05-05 17:08:22 -0300

    tabela dos pixels feita

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 window.onload = function colorPaletteGenerator() {
-  let colorPaletteDiv = document.getElementsByClassName('color');
+  let colorPaletteSection = document.getElementsByClassName('color');
   // The first color must be black
-  colorPaletteDiv[0].style.backgroundColor = 'black'
+  colorPaletteSection[0].style.backgroundColor = 'black';
   // Generating all other colors randomly
-  for (let index = 1; index < colorPaletteDiv.length; index += 1) {
+  for (let index = 1; index < colorPaletteSection.length; index += 1) {
     let redColorParameter = parseInt(Math.random() * 255);
     let greenColorParameter = parseInt(Math.random() * 255);
     let blueColorParameter = parseInt(Math.random() * 255);
-    colorPaletteDiv[index].style.backgroundColor = `rgb(${redColorParameter}, ${greenColorParameter}, ${blueColorParameter})`;
+    colorPaletteSection[index].style.backgroundColor = `rgb(${redColorParameter}, ${greenColorParameter}, ${blueColorParameter})`;
   }
 };
+
+function pixelCreation() {
+  let maxRow = 5;
+  let maxLine = 5;
+  let pixelSection = document.querySelector('#pixel-board')
+  for (let counter = 0; counter < maxLine; counter += 1) {
+    let eachRowDiv = document.createElement('div');
+    eachRowDiv.className = 'rowDiv'
+    pixelSection.appendChild(eachRowDiv);
+    for (let index = 0; index < maxRow; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      eachRowDiv.appendChild(pixel);
+    }
+  }
+
+
+  console.log('foi');
+}
+pixelCreation()
