69b7798dc43619ba71f8a5f96db957b021ca908a
Gabriel Takaki <gabitj@icloud.com>
2021-05-05 16:38:44 -0300

    Resolve 4 e 5(3)

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const colorPalete = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
 const colors = ['#000', '#405d27', '#034f84', '#c94c4c'];
 
 
-for(let index = 0; index < colors.length; index+=1) {
+function colorSelector() {
+    for(let index = 0; index < colors.length; index+=1) {
         const eachColor = document.createElement('div');
         const color = colors[index];
         eachColor.innerText = color;
         eachColor.className = 'color';
         colorPalete.appendChild(eachColor);
     };
-    
-const pixelBoard = document.getElementById('pixel-board');
-for(let index = 0; index < 25; index+=1) {
-    const eachPixel = document.createElement('div');
-    const pixel = index;
-    eachPixel.innerText = pixel;
-    eachPixel.className = 'pixel';
-    pixelBoard.appendChild(eachPixel);
-};
+    colorPalete.firstChild.className = 'color selected';
+}
+colorSelector();    
+
+function drawingBoarder() {    
+    for(let index = 0; index < 25; index+=1) {
+        const eachPixel = document.createElement('div');
+        const pixel = index;
+        eachPixel.innerText = pixel;
+        eachPixel.className = 'pixel';
+        pixelBoard.appendChild(eachPixel);
+        pixelBoard.style.maxWidth = index*8.75 + 'px';
+    };
+}    
+drawingBoarder();
