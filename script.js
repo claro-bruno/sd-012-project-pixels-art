94f2f3a61db57ebc4c84cba43bdd968d4f4be2c3
Armando <a.guerra58@hotmail.com>
2021-05-05 15:49:14 -0300

    testando

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelectorAll(".color")[0].style.background="black"
+document.querySelectorAll(".color")[1].style.background="violet"
+document.querySelectorAll(".color")[2].style.background="salmon"
+document.querySelectorAll(".color")[3].style.background="yellow"
