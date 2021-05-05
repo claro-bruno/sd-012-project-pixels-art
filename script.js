4bf2394fab232272b80966d16868bd0dcabf2d45
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-05 15:33:11 -0300

    requisito 3 e 4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function paletteGenerator (){
+function paletteGenerator() {
   let colors = ['black', 'green', 'blue', 'orange'];
   let paletteSection = document.getElementById('color-palette');
-  
 
-  for (let index = 0; index< colors.length; index += 1){
+
+  for (let index = 0; index < colors.length; index += 1) {
     let createDiv = document.createElement('div');
     paletteSection.appendChild(createDiv);
     createDiv.className = 'color';
@@ -11,3 +11,27 @@ function paletteGenerator (){
   }
 }
 paletteGenerator();
+
+function tableGenerator() {
+  let canvasSize = 5;
+  let tableTag = document.querySelector('table');
+
+  for (let index = 0; index < canvasSize; index += 1) {
+    let tableRow = document.createElement('tr')
+    tableTag.appendChild(tableRow);
+    tableRow.className = 'pixel';
+  }
+
+  let tableRows = tableTag.children;
+  for (let indexLine = 0; indexLine < tableRows.length; indexLine += 1) {
+    for (let index = 0; index < canvasSize; index += 1){
+      let tableData = document.createElement('td');
+      tableRows[indexLine].appendChild(tableData);
+      tableData.className = 'pixel';
+    }
+  }
+
+
+
+}
+tableGenerator();
