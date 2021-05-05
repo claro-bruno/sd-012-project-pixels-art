8bd4cf31bb5e18df2de91d4f9e6fe3b37812148e
Abner <gedeaoabner@gmail.com>
2021-05-05 16:31:51 -0300

    cria função para alterar dinamicamente o tamanho dos pixels e define o tamanho como 40px - desafio 5

--- script.js
+++ script.js
@@ -29,18 +29,28 @@ function criaPixel() {
     divLinha.push(criaLinha);
     lugar.appendChild(divLinha[indexColuna]);
     for (let indexLinha = 0; indexLinha < linha; indexLinha += 1) {
-        let pixel = document.createElement("div");
-        pixel.setAttribute("class", "pixel");
-        divLinha[indexColuna].appendChild(pixel);
-        pixel.setAttribute("id", "pixel" + idPixel);
-        idPixel += 1;
+      let pixel = document.createElement("div");
+      pixel.setAttribute("class", "pixel");
+      divLinha[indexColuna].appendChild(pixel);
+      pixel.setAttribute("id", "pixel" + idPixel);
+      idPixel += 1;
     }
   }
-  
-  
-  // lugar.appendChild(pixel);
+  tamanhoPixel(40);
 }
 
+function tamanhoPixel(tamanho) {
+  let tamanhoBorda = 1;
+  let todosPixels = document.getElementsByClassName("pixel");
+  for (let index = 0; index < todosPixels.length; index += 1) {
+    todosPixels[index].style.width = tamanho + "px";
+    todosPixels[index].style.height = tamanho + "px";
+  }
+  let todasLinhas = document.getElementsByClassName("linha");
+  for (let index = 0; index < todasLinhas.length; index += 1) {
+    todasLinhas[index].style.height = tamanho + (tamanhoBorda * 2) + "px";
+  }
+}
 
 coresPaleta();
 criaPixel();
