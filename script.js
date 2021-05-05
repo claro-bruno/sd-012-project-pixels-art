61ae0a5447bee1090ab748fc099ae5ebd3fb1c9e
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-05 16:39:20 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,4 @@
+let blockGreen = document.getElementsByClassName('color')[0];
+let blockRed = document.getElementsByClassName('color')[1];
+let blockYellow = document.getElementsByClassName('color')[2];
+let blockBlue = document.getElementsByClassName('color')[3];
