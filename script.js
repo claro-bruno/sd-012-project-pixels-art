b1d1108b712de00946e13c1e7e70a150a425039a
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-05 15:11:21 -0300

    (2) Adiciona paleta de cores

---
+++ script.js
@@ -0,0 +1,12 @@
+let colors = ['black', 'green', 'blue', 'red'];
+
+for (let color = 0; color < colors.length; color += 1) {
+    let box = document.createElement('div');
+    box.className = 'color';
+    box.style.width = '40px';
+    box.style.height = '40px';
+    box.style.border = '1px solid black';
+    box.style.display = 'inline-block'
+    box.style.backgroundColor = colors[color];
+    document.querySelector('#color-pallete').appendChild(box)
+}
