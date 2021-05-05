5dde4ee0b870b94a673f4f8d4d4c7a1485a02827
Teika <shimworks.tk@gmail.com>
2021-05-05 15:47:44 -0300

    resolve requisito 04

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function alteraCor () {
-  let cores = ['black', 'blue', 'red', 'green'];
-  let paletaCores = document.querySelectorAll('.color')
+function alteraCor() {
+  const cores = ['black', 'blue', 'red', 'green'];
+  let paletaCores = document.querySelectorAll('.color');
   for (let index = 0; index < paletaCores.length; index += 1) {
-    paletaCores[index].style.backgroundColor = cores[index]
+    paletaCores[index].style.backgroundColor = cores[index];
   }
 }
-alteraCor ();
+alteraCor();
