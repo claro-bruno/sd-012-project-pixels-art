35dff34cc8b4b8e148e963eca4a7106607a077a8
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 15:38:01 -0300

    adding requirements 5 and 6

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let medidas = 5;
 function creatingPixelDivs (linhas) {
     for (let row = 1; row <= linhas; row += 1) {
         const divRow = document.createElement('div');
+        divRow.classList.add('tr');
         pixelBoard.appendChild(divRow);
         creatingPixelDivsCollumns(divRow, linhas);
     }
@@ -14,8 +15,14 @@ function creatingPixelDivsCollumns(row, colunas) {
     for (let column = 1; column <= colunas; column += 1) {
         const divColumn = document.createElement('div');
         divColumn.classList.add('pixel');
+        divColumn.classList.add('td');
         row.appendChild(divColumn);
     }
 }
 
 creatingPixelDivs(medidas);
+
+window.onload = function () {
+    const blackColor = document.querySelector('#first-color');
+    blackColor.classList.add('selected');
+};
