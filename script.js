097a1ac6fe800f097e8beac01e3c9dc5d00b240c
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-05 17:19:55 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ window.onload = initialBrush;
 function initialBrush () {
   document.querySelector('#color1').classList.add('selected');
 }
+
+function changeBrush (event) {
+  if (event.target.classList.contains('color')) {
+    let palette = document.getElementsByClassName('color');
+    for (let index = 0; index < palette.length; index += 1) {
+      palette[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
+}
+
+let colorPalette = document.querySelector('#color-palette');
+
+colorPalette.addEventListener('click', changeBrush);
