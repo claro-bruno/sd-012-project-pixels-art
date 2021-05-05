72ca1bdc3dd1afb5f148f6876c21b4861e736da0
Guilherme Cesconeto <cesconeto@gmail.com>
2021-05-05 18:08:27 -0300

    Espero ter resolvido até o 9

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const board = document.querySelector('#pixel-board');
 let paletteSize = 4;
 let boardSize = 5;
 let numberOfPixels = boardSize * boardSize;
-let selected
+let selected = 'black';
 
 let paletteWidth = (paletteSize * 45);
 let boardWidth = (boardSize * 42);
@@ -12,7 +12,23 @@ let boardWidth = (boardSize * 42);
 // palette.style.width = paletteWidth + 'px';
 // board.style.width = boardWidth + 'px';
 
-document.querySelector('body').addEventListener('click', clicked);
+document.addEventListener('click', function(eventObject){
+  let target = eventObject.target
+  if ( target.classList.contains('pixel')){
+    target.style.backgroundColor = selected;
+  }
+  if ( target.classList.contains('color')){
+    resetPaletteClasses();
+    target.className = ("color selected");
+    selected = window.getComputedStyle(target).backgroundColor
+  }
+});
+
+function resetPaletteClasses (){
+  for(let index = 0; index < color.length; index += 1){
+    color[index].className = ('color');
+  }
+}
 
 createBoard();
 
@@ -21,21 +37,14 @@ createBoard();
 // Cria ou reseta a board com numero de elementos definidos e com pixels em branco
 
 
-function clicked (eventObject){
-  let target = eventObject.target
-  if ( target.classList.contains('pixel')){
 
-  }
-  if ( target.classList.contains('color')){
-    
-  }
-}
 
 function createBoard() {
   board.innerHTML = '';
   for (let index = 0; index < numberOfPixels; index += 1){
   let div = document.createElement("div");
   div.className = ('pixel');
+  div.style.backgroundColor = 'white';
   board.appendChild(div);
   }
 }
