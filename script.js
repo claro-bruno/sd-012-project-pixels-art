43d9172ca970e65a16b40d6c3b50c38ffcd48b2b
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-05 20:42:52 -0300

    questao 5

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+function colunns(){
+    let n = 5;
+    let symbol = document.getElementById('color-palette')[0];
+    let inputLine = '';
 
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
+}
 
