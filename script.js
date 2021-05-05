498105f41ce7ff6bdfcf1e2e1225d747c2b19734
Roberval Filho <rslfilho@gmail.com>
2021-05-05 15:55:31 -0300

    R4 - Resolvendo

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-function createColorPaletteDiv(bgColor) {
-  const colorPaletteSection = document.getElementById('color-palette');
+function createBlock(bgColor, className, parentClassName) {
+  const colorPaletteSection = document.getElementById(parentClassName);
   const colorPaletteDiv = document.createElement('div');
 
-  colorPaletteDiv.className = 'color';
+  colorPaletteDiv.className = className;
   colorPaletteDiv.style.backgroundColor = bgColor;
 
   colorPaletteSection.appendChild(colorPaletteDiv);
 }
 
-const paletteColors = ['black', 'blue', 'green', 'yellow'];
-for (let index = 0; index < paletteColors.length; index += 1) {
-  createColorPaletteDiv(paletteColors[index]);
+function createPaletteBlocks(colors) {
+  const paletteColors = colors;
+  for (let index = 0; index < paletteColors.length; index += 1) {
+    createBlock(paletteColors[index], 'color', 'color-palette');
+  }
 }
+
+function createBoardBlocks(size) {
+  const boardSize = size;
+  const pixelBoard = document.querySelector('#pixel-board');
+  const boardWidth = size * 40 + size * 2 + 1;
+  pixelBoard.style.width = `${boardWidth}px`;
+
+  for (let indexLine = 0; indexLine < boardSize; indexLine += 1) {
+    for (let indexCollumn = 0; indexCollumn < boardSize; indexCollumn += 1) {
+      createBlock('white', 'pixel', 'pixel-board');
+    }
+  }
+}
+
+createPaletteBlocks(['black', 'blue', 'green', 'yellow']);
+createBoardBlocks(5);
