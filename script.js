87840b3c9d09d0b07ff7ec44cfef41f1c5bd42b2
Martin <escorpmartin97@gmail.com>
2021-05-05 17:06:02 -0300

    adiciona exercise 2

---
+++ script.js
@@ -0,0 +1,7 @@
+// function paletaCores (cores) {
+//     let getContainer = document.querySelector('#color-palette');
+//     console.log(getContainer);
+//     for(let index = 0; index <= getContainer; index += 1) {
+//         getContainer
+//     }
+// }
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function paletaCores (cores) {
+    let getContainer = document.querySelector('#color-palette');
+    console.log(getContainer);
+    for(let index = 0; index < 5; index += 1) {
+        getContainer
+    }
+}
