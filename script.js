f34f4547b43809f02d03b5bcc5ff9a173a0398c1
Bruno Augusto <brunaugusto@gmail.com>
2021-05-05 18:35:53 -0300

    Primeiros Requisitiso Correcoes

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function createQuadradinhos() {
     for (let index = 0; index < 5; index += 1) {
         const getPixelBoard = document.querySelector('#pixel-board');
         const quadradoItem = document.createElement('div');
-        const paragrafo = document.createElement('p');
+        const paragrafo = document.createElement('br');
         getPixelBoard.appendChild(paragrafo);
         for(let indexLinha = 0; indexLinha < 5; indexLinha += 1) {
             const quadradoItem = document.createElement('div');
-            quadradoItem.className = 'pixels';
+            quadradoItem.className = 'pixel';
             quadradoItem.style.width = '40px';
             quadradoItem.style.height = '40px';
             quadradoItem.style.border = 'solid 1px black';
             quadradoItem.style.lineHeight = '0';
             quadradoItem.style.display = 'inline-block';
+            quadradoItem.style.backgroundColor = 'white';
+    
             getPixelBoard.appendChild(quadradoItem);
         }
 
