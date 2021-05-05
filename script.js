e2c25d858e1d759d3171855bed399a032a1d991d
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-05 19:12:27 -0300

    resolucao req 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let cores = document.getElementsByClassName("color");
 let paleta = document.getElementById("color-palette");
 
-    paleta.children[0].style.backgroundColor = "blue";
-    paleta.children[1].style.backgroundColor = "yellow";
-    paleta.children[2].style.backgroundColor = "red";
-    paleta.children[3].style.backgroundColor = "green";
+paleta.children[0].style.backgroundColor = "black";
+paleta.children[1].style.backgroundColor = "yellow";
+paleta.children[2].style.backgroundColor = "red";
+paleta.children[3].style.backgroundColor = "green";
