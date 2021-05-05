911bcde7849337e263ebbd2f1fab387beb7c5e4d
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-05 15:54:37 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log('Conectado');
 
+const colorClass = document.getElementsByClassName('color');
+for (let index = 0; index < colorClass.length; index += 1) {
+  colorClass[index].addEventListener('click', function (event) {
+    for (let index2 = 0; index2 < colorClass.length; index2 += 1) {
+      if (colorClass[index2].classList.contains('selected')) {
+        colorClass[index2].classList.remove('selected');
+      }
+    }
+    event.target.classList.add('selected');
+  });
+};
