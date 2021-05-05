48a3b3548b52bc07739b0b54f1645db421f25f02
Martin <escorpmartin97@gmail.com>
2021-05-05 18:46:13 -0300

    ex 4

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    let section = document.querySelector('.pixel');
+    console.log(section)
+    
+    function pixel (section) {
+        for (let index = 0; index < 26; index += 1) {
+       
+        }
+    }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,2 @@
-function paletaCores () {
-    let getContainer = document.querySelector('#color-palette');
-    
-    for (let index = 0; index < 6; index += 1) {
-        console.log(getContainer)
-    }
-}
+  let section = document.querySelector('.pixel');
+    console.log(section)
