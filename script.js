df402ce1c5f14327b452de0d1a76ab5f36f09f63
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-05 16:49:54 -0300

    Resolve requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
-window.onload = function() {
-  function selectedColor() {
+window.onload = function () {
+  function selectedColorInitial() {
     let selectedBlackColor = document.querySelector('.black');
-    selectedBlackColor.className += ' selected';
-  }
-  selectedColor()
-};
+    let selectedRedColor = document.querySelector('.red');
+    let selectedBlueColor = document.querySelector('.blue');
+    let selectedGreenColor = document.querySelector('.green');
 
+    // ao iniciar a pagina, o black é selected
+    selectedBlackColor.className += ' selected';
+
+    //seleciona black
+    selectedBlackColor.addEventListener('click', function (){
+        selectedBlackColor.className += ' selected';
+        selectedRedColor.classList.remove('selected');
+        selectedBlueColor.classList.remove('selected');
+        selectedGreenColor.classList.remove('selected');
+    });
+
+    //seleciona red
+    selectedRedColor.addEventListener('click', function (){
+        selectedRedColor.className += ' selected';
+        selectedBlackColor.classList.remove('selected');
+        selectedBlueColor.classList.remove('selected');
+        selectedGreenColor.classList.remove('selected');
+    });
+
+    //seleciona blue
+    selectedBlueColor.addEventListener('click', function (){
+        selectedBlueColor.className += ' selected';
+        selectedBlackColor.classList.remove('selected');
+        selectedRedColor.classList.remove('selected');
+        selectedGreenColor.classList.remove('selected');
+    });
+
+      //seleciona green
+      selectedGreenColor.addEventListener('click', function (){
+        selectedGreenColor.className += ' selected';
+        selectedBlackColor.classList.remove('selected');
+        selectedRedColor.classList.remove('selected');
+        selectedBlueColor.classList.remove('selected');
+    });
+
+
+
+  }
+  selectedColorInitial();
+
+  
+};
