d54394c143b94e25a33ba9ffe3e6dcea99a57198
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-05 15:15:53 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    let pixel_board = document.getElementById("pixel-board");
+    for (let i = 0; i < 5; i++) {
+        let linha_pixel = document.createElement("div");
+        pixel_board.style.display = "block";
+        pixel_board.appendChild(linha_pixel);
+        for (let i = 0; i < 5; i++) {
+            let pixel = document.createElement("div");
+            pixel.className = "pixel";
+            pixel.style.backgroundColor = "white";
+            pixel.style.display = "inline-block";
+            linha_pixel.appendChild(pixel);
+        }
+    }
+}
