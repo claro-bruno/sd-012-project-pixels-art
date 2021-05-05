0b6f2d2b1b75b56c8617d5c47fe6f00d9e446f31
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-05-05 15:19:19 -0300

    criando os elementos e implementando no js as cores da paleta

---
+++ script.js
@@ -0,0 +1,9 @@
+//COLOCANDO AS CORES NA PALETA ATRAVES DAS CLASSES:
+
+const black = document.getElementsByClassName('color')[0];
+
+const red = document.getElementsByClassName('color')[1];
+
+const yellow = document.getElementsByClassName('color')[2];
+
+const blue = document.getElementsByClassName('color')[3];
