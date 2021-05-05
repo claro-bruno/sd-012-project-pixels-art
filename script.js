690e838df0d0b0b57dc1703f3a513dea55a9e8bd
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-05 16:02:22 -0300

    Retirada a declaraçao da cor da paleta no css e colorida a primeira cor de preto via JS

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = paletaDeCores;
+
+function paletaDeCores (){
+const paletacores = document.getElementsByClassName('color');
+
+paletacores[0].style.backgroundColor = 'black'
+
+
+}
+
