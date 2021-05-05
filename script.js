868289823633fb87ad5674a1a78149ef4c60f80e
Daniel <danielbped@gmail.com>
2021-05-05 15:02:39 -0300

    Resolve requisito 2

---
+++ script.js
@@ -0,0 +1,15 @@
+let paletaCores = document.querySelector('#color-palette');
+
+let listaCores = ['red', 'yellow', 'blue', 'green'];
+
+for (let index = 0; index < listaCores.length; index += 1) {
+  let novaCor = document.createElement('li');
+  let novaCorSize = '30px';
+  novaCor.className = 'color';
+  novaCor.style.listStyle = 'none';
+  novaCor.style.width = novaCorSize;
+  novaCor.style.height = novaCorSize;
+  novaCor.style.border = '1px solid black';
+  novaCor.style.backgroundColor = listaCores[index];
+  paletaCores.appendChild(novaCor);
+}
