cb5f1558b2e263969cadf9da3cab75c72279fb9a
André Moreno <deco616@hotmail.com>
2021-05-05 18:38:32 -0300

    Ajustando requisito 5 e adicionnando requisito 6

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 window.onload = function () {
 
-
+selecionar();
 gerarBoard(5);
 }
 
 function gerarBoard(numero) {
-    let pixelBoard = document.getElementById('pixel-board');
-    for (let index = 0; index < numero; index += 1){
-        console.log(index);
-        let primeiraDiv = document.createElement('div');
-        primeiraDiv.className = 'linha';
-        pixelBoard.appendChild(primeiraDiv);
-        for (let index2 = 0; index2 < numero; index2 += 1){
-          let divInterna = document.createElement('div');
-          divInterna.className = 'pixel';
-          primeiraDiv.appendChild(divInterna);
+  let pixelBoard = document.getElementById('pixel-board');
+  for (let index = 0; index < numero; index += 1){
+    console.log(index);
+    let primeiraDiv = document.createElement('div');
+    primeiraDiv.className = 'linha';
+    pixelBoard.appendChild(primeiraDiv);
+    for (let index2 = 0; index2 < numero; index2 += 1){
+      let divInterna = document.createElement('div');
+      divInterna.className = 'pixel';
+      primeiraDiv.appendChild(divInterna);
     }
-  }
-  
+  } 
+}
+
+function selecionar() {
+  let corSelecionada = document.getElementById('color1');
+  corSelecionada.classList.add('selected');  
 }
