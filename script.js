453a5b073468c231a18a7155df3a0df2062cb0cc
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-05 18:06:00 -0300

    mais ajustes lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function paletteGenerator() {
   const paletteSection = document.getElementById('color-palette');
 
   for (let index = 0; index < colors.length; index += 1) {
-    let createDiv = document.createElement('div');
+    const createDiv = document.createElement('div');
     paletteSection.appendChild(createDiv);
     createDiv.className = 'color';
     createDiv.style.backgroundColor = colors[index];
@@ -23,10 +23,10 @@ function tableGenerator() {
     tableTag.appendChild(tableRow);
     tableRow.style.backgroundColor = 'white';
   }
-  let tableRows = tableTag.children;
+  const tableRows = tableTag.children;
   for (let indexLine = 0; indexLine < tableRows.length; indexLine += 1) {
     for (let index = 0; index < canvasSize; index += 1) {
-      let tableData = document.createElement('td');
+      const tableData = document.createElement('td');
       tableRows[indexLine].appendChild(tableData);
       tableData.className = 'pixel';
       tableData.style.backgroundColor = 'white';
@@ -36,34 +36,34 @@ function tableGenerator() {
 tableGenerator();
 
 function colorSelected() {
-  let selectedColor = document.getElementsByClassName('color selected');
-  let colorPalette = document.getElementById('color-palette');
+  const selectedColor = document.getElementsByClassName('color selected');
+  const colorPalette = document.getElementById('color-palette');
 
   colorPalette.addEventListener('click', function (event) {
     selectedColor[0].classList.remove('selected');
     event.target.classList.add('selected');
-  })
-
+  });
 }
+
 colorSelected();
 
 function paintPixel() {
-  let canvas = document.getElementById('pixel-board');
+  const canvas = document.getElementById('pixel-board');
 
   canvas.addEventListener('click', function (event) {
-    let selectedColor = document.getElementsByClassName('color selected')[0].style.backgroundColor;
+    const selectedColor = document.getElementsByClassName('color selected')[0].style.backgroundColor;
     event.target.style.backgroundColor = selectedColor;
-  })
+  });
 }
 paintPixel();
 
 function clearCanvas() {
-  let clearButton = document.getElementById('clear-board');
-  let canvas = document.getElementsByClassName('pixel');
+  const clearButton = document.getElementById('clear-board');
+  const canvas = document.getElementsByClassName('pixel');
   clearButton.addEventListener('click', function () {
     for (let index = 0; index < canvas.length; index += 1) {
       canvas[index].style.backgroundColor = 'white';
     }
-  })
+  });
 }
 clearCanvas();
