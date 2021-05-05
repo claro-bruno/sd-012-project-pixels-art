6ca1b3be7258d8699ebabb3b6502ec8a425fc42c
Josimar Schneider <josimarschneider@gmail.com>
2021-05-05 16:28:08 -0400

    paleta de quatro cores distintas finalizada

---
+++ script.js
@@ -0,0 +1,4 @@
+const cor1 = document.querySelectorAll('.color')[0].style.backgroundColor = 'black';
+const cor2 = document.querySelectorAll('.color')[1].style.backgroundColor = 'red';
+const cor3 = document.querySelectorAll('.color')[2].style.backgroundColor = 'blue';
+const cor4 = document.querySelectorAll('.color')[3].style.backgroundColor = 'orange';
