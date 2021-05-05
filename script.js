6825c684511efff878e5f5fc78edcf374a673a20
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-05 15:47:27 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 window.onload = function () {
-    setColor();
+    let colorsList = [
+        'black',
+        'blue',
+        'yellow',
+        'pink'
+      ];
+    let pixelsColor = document.querySelectorAll('.color');
+
+    setInitialColors();
+    changeSelectedColor();
+
+    function setInitialColors() {    
+        for (index = 0; index < pixelsColor.length; index += 1) {
+            pixelsColor[index].style.backgroundColor = colorsList[index];
+            if(colorsList[index] === 'black'){
+                pixelsColor[index].className += ' selected';
+            };
+        };
+      };
+    
+      function changeSelectedColor(){
+        for (index = 0; index < pixelsColor.length; index += 1) {
+            let currentDiv = pixelsColor[index];
+            currentDiv.addEventListener('click', nowSelected);
+        };
+      }
+
+      function nowSelected(e){
+        for (index = 0; index < pixelsColor.length; index += 1) {
+           let currentDiv = pixelsColor[index];
+           currentDiv.className = 'color';
+        };
+
+          e.target.className = 'color selected';
+      };
+    
 };
 
-function setColor() {
-    let divsColor = document.querySelectorAll('.color');
-    let colorsList = [
-      'black',
-      'blue',
-      'yellow',
-      'pink'
-    ];
-
-    for (index = 0; index < divsColor.length; index += 1) {
-        divsColor[index].style.backgroundColor = colorsList[index];
-        if(colorsList[index] === 'black'){
-            divsColor[index].className += ' selected';
-        };
-    };
-  };
