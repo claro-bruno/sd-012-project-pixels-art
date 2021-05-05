8b6e8258a048b3548683601e1ae89ee189a6b4a0
Heder Waine <hederw@hotmail.com>
2021-05-05 18:07:20 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
 document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
 document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
 document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
+
+window.onload = function(){
+
+    
+    let getBoard = document.querySelector('#pixel-board');
+    
+
+    function pixelBoard(size){
+        
+        for(let row = 0; row < size; row += 1){
+            let pixelRow = document.createElement('div');
+
+            pixelRow.className = 'pixel-row';
+            getBoard.appendChild(pixelRow);
+
+            for(let cell = 0; cell < size; cell += 1){
+                let coluna = document.createElement('div');
+                coluna.className = 'pixel';
+                pixelRow.appendChild(coluna);
+    
+            }
+
+        }
+    }
+    pixelBoard(5);
+}
