14e68fb838c639fb0c842c55cabe0e25cacbfce4
RyanLaiber <ryanlaiber@gmail.com>
2021-05-05 17:31:14 -0300

    adiciona requisito 6 cor inicial black selecionada

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let QuadroDePixels = document.getElementById("pixel-board");
 //funções para formar quadro de pixels
 
 function formaLinha(linhas){
-    for (let linha=0; linha<linhas; linha+=1){
+    for (let linha = 0; linha < linhas; linha += 1){
         let divLinha = document.createElement("div");
         divLinha.className = "linha";
         QuadroDePixels.appendChild(divLinha);
@@ -17,7 +17,7 @@ formaLinha(linhas);
 function formaPixelsLinha (colunas){
     let listLinhas = document.getElementsByClassName("linha");
     for (let l of listLinhas){
-        for (let coluna=0; coluna<colunas; coluna+=1){
+        for (let coluna = 0; coluna < colunas; coluna += 1){
             let divPixel = document.createElement("div");
             divPixel.className = "pixel";
             l.appendChild(divPixel);
@@ -25,3 +25,5 @@ function formaPixelsLinha (colunas){
     };
 };
 formaPixelsLinha(colunas);
+
+//função de selecionar cor
