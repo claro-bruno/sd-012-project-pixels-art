fce3a01222df6a3c63206b2b09502855088a6edb
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-05-05 15:35:37 -0300

    adicionando cor de fundo a paleta de cores

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //COLOCANDO AS CORES NA PALETA ATRAVES DAS CLASSES:
 
-const black = document.getElementsByClassName('color')[0];
+const black = document.querySelectorAll('.color')[0].style.backgroundColor = 'black';
 
-const red = document.getElementsByClassName('color')[1];
+const red = document.querySelectorAll('.color')[1].style.backgroundColor = 'red';
 
-const yellow = document.getElementsByClassName('color')[2];
+const yellow = document.querySelectorAll('.color')[2].style.backgroundColor = 'yellow';
 
-const blue = document.getElementsByClassName('color')[3];
+const blue = document.querySelectorAll('.color')[3].style.backgroundColor = 'blue';
+
+//CRIANDO A PALETA DE CORES:
+
+
