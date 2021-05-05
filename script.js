ef43b0951a94c990b68baf0c216f7f07826143d9
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-05 16:08:52 -0300

    Adicionada todas as cores na paleta via JS

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-window.onload = paletaDeCores;
+window.onload = paletaDeCores();
 
 function paletaDeCores (){
 const paletacores = document.getElementsByClassName('color');
 
-paletacores[0].style.backgroundColor = 'black'
-
-
+paletacores[0].style.backgroundColor = 'black';
+paletacores[1].style.backgroundColor = 'yellow';
+paletacores[2].style.backgroundColor = '#ff9933';
+paletacores[3].style.backgroundColor = '#009933';
 }
 
