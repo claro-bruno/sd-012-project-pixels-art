be325580aaa6255fa29ca5262ddcbb03bed0140a
Abner <gedeaoabner@gmail.com>
2021-05-05 16:12:23 -0300

    cria tabela de pixels em branco - desafio 4

--- script.js
+++ script.js
@@ -6,15 +6,38 @@
 let cor = ["rgb(0, 0, 0, 1)", "rgb(255, 0, 0, 1)", "rgb(0, 100, 255, 1)", "rgb(0, 255, 0, 1)"];
 
 function coresPaleta() {
-    let paleta = document.getElementsByClassName("color");
-    console.log(paleta);
-    for (let index = 0; index < paleta.length; index += 1) {
-        paleta[index].setAttribute("id", "cor" + (index+1));
-        if (paleta[index].id == ("cor"+(index+1))) {
-            console.log("OLá");
-            paleta[index].style.backgroundColor = cor[index];
-        }
+  let paleta = document.getElementsByClassName("color");
+  console.log(paleta);
+  for (let index = 0; index < paleta.length; index += 1) {
+    paleta[index].setAttribute("id", "cor" + (index+1));
+    if (paleta[index].id === ("cor" + (index+1))) {
+      console.log("OLÁ");
+      paleta[index].style.backgroundColor = cor[index];
     }
+  }
 }
 
-coresPaleta();  
+function criaPixel() {
+  let linha = 5;
+  let coluna = 5;
+  let divLinha = [];
+  let lugar = document.getElementById("pixel-board");
+  for (let indexColuna = 0; indexColuna < coluna; indexColuna += 1) {
+    let criaLinha = document.createElement("div");
+    criaLinha.setAttribute("class", "linha");
+    divLinha.push(criaLinha);
+    lugar.appendChild(divLinha[indexColuna]);
+    for (let indexLinha = 0; indexLinha < linha; indexLinha += 1) {
+        let pixel = document.createElement("div");
+        pixel.setAttribute("class", "pixel");
+        divLinha[indexColuna].appendChild(pixel);
+    }
+  }
+  
+  
+  // lugar.appendChild(pixel);
+}
+
+
+coresPaleta();
+criaPixel();
