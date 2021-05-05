d97ebdd59d7427d90313a859b887988e1fd1a640
Caio César <caiovg32@gmail.com>
2021-05-05 17:57:14 -0300

    req.all

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+function palleteCreate () {
+    let colorPallete = document.querySelector("#color-palette")
 
+    let firstElement = document.createElement("div");
+    firstElement.className = "color selected";
+    firstElement.style.backgroundColor = "black";
+
+    colorPallete.appendChild(firstElement);
+
+
+    for (let index = 0; index < 3; index += 1){
+        let element = document.createElement("div");
+        element.className = "color";
+        element.style.backgroundColor = randomcolor();
+        colorPallete.appendChild(element);
+    };
+};
+
+palleteCreate();
+
+
+function randomcolor () {
+    return "#" + parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, "0");
+}
 
 
 function selectedColor (event){
@@ -14,15 +37,4 @@ for (let index = 0; index < colorsList.length; index += 1) {
     colorsList[index].addEventListener("click", selectedColor);
 };
 
-function pintando (event) {
-    let colorSelected = document.querySelector(".selected").style.backgroundColor;
-    event.target.style.backgroundColor = colorSelected;
-};
-
-let pixelsList = document.querySelectorAll(".pixel");
-
-for(let index = 0; index < pixelsList.length; index += 1) {
-    pixelsList[index].style.backgroundColor = "white";
-    pixelsList[index].addEventListener("click", pintando);
-};
 
