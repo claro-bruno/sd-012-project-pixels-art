905eae525cf468fc118d28a9b98b72ad4442af3b
matheus <matheus111904@hotmail.com>
2021-05-05 16:46:56 -0300

    cores paleta refatorado

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudarCorPaleta (array){
+    let arrayCores = ["black","yellow", "red", "green"];
+    let paleta = document.getElementsByClassName('color')
+    for (index = 0; index < paleta.length; index += 1){
+        for (index = 0; index < arrayCores.length; index += 1){
+            paleta[index].style.backgroundColor = arrayCores[index];
+            console.log(arrayCores[index])
+        }
+    }
+}
+mudarCorPaleta();
