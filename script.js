867b1d894041609dfe8a216f0f95caf4db4da35d
Armando <a.guerra58@hotmail.com>
2021-05-05 17:43:18 -0300

    8 concluida

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-document.querySelectorAll(".color")[0].style.background="black";
-document.querySelectorAll(".color")[1].style.background="violet";
-document.querySelectorAll(".color")[2].style.background="salmon";
-document.querySelectorAll(".color")[3].style.background="yellow";
+document.querySelectorAll(".color")[0].style.background = "black";
+document.querySelectorAll(".color")[1].style.background = "violet";
+document.querySelectorAll(".color")[2].style.background = "salmon";
+document.querySelectorAll(".color")[3].style.background = "yellow";
 
 window.onload = document.querySelectorAll(".color")[0].className = "color selected"
 
 
-let black =  document.getElementsByClassName("color")[0];
+let black = document.getElementsByClassName("color")[0];
 let violet = document.getElementsByClassName("color")[1];
 let salmon = document.getElementsByClassName("color")[2];
 let yellow = document.getElementsByClassName("color")[3];
 
 
-function changeSelected(event){
+function changeSelected(event) {
 
-let changes = document.querySelector(".selected");
+  let changes = document.querySelector(".selected");
 
-changes.classList.remove("selected");
-event.target.classList.add("selected");
+  changes.classList.remove("selected");
+  event.target.classList.add("selected");
 
 }
 
-black.addEventListener("click" , changeSelected);
-violet.addEventListener("click" , changeSelected);
-salmon.addEventListener("click" , changeSelected);
-yellow.addEventListener("click" , changeSelected);
+black.addEventListener("click", changeSelected);
+violet.addEventListener("click", changeSelected);
+salmon.addEventListener("click", changeSelected);
+yellow.addEventListener("click", changeSelected);
+
+
+let pixels = document.getElementById("pixel-board");
+
+pixels.addEventListener("click", function(event){ 
+
+event.target.style.backgroundColor = document.querySelector(".selected").style.backgroundColor;
+
+})
+
+
+
+
 
