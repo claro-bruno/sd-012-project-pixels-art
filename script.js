daa564f07fd4d458ac4f19cc19104d287812c31d
José Carlos <kalarruda@hotmail.com>
2021-05-05 16:47:39 -0300

    4-desafio

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let coresPaleta = document.querySelectorAll('.color');
+const coresPaleta = document.querySelectorAll('.color');
 console.log(coresPaleta);
 coresPaleta[0].style.backgroundColor = 'black';
 coresPaleta[1].style.backgroundColor = 'red';
 coresPaleta[2].style.backgroundColor = 'blue';
 coresPaleta[3].style.backgroundColor = 'green';
+
+const quantidadePixels = 5;
+const pixelBoard = document.querySelector('#pixel-board');
+for (index = 0; index < quantidadePixels*quantidadePixels; index += 1) {
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  pixelBoard.appendChild(pixel);
+}
