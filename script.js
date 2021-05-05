3ee4a97e6eeb96376ba96f1d8e215d7f384ad9fd
thales daniel <thalesdaniel2013@gmail.com>
2021-05-05 19:40:48 -0300

    feito os quadrinhos, agora so falta conseguir tirar as bordas

---
+++ script.js
@@ -0,0 +1,5 @@
+for (index = 0; index < 25; index += 1) {
+    let criacao = document.createElement("div")
+    let classe = criacao.className = 'pixel'
+    const div = document.querySelector('#pixel-board').appendChild(criacao)
+}
