5c7aff8154e69e923ca23d3ab0615a380bb49a62
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 20:55:46 -0300

    fixing a error while resazing the board and renaming some functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pixelBoard = document.querySelector('#pixel-board');
 
-function creatingPixelDivsCollumns(row, colunas) {
+function creatingPixelBoardCollumns(row, colunas) {
   for (let column = 1; column <= colunas; column += 1) {
     const divColumn = document.createElement('div');
     divColumn.classList.add('pixel');
@@ -9,16 +9,16 @@ function creatingPixelDivsCollumns(row, colunas) {
   }
 }
 
-function creatingPixelDivs(linhas) {
+function creatingPixelBoardRows(linhas) {
   for (let row = 1; row <= linhas; row += 1) {
     const divRow = document.createElement('div');
     divRow.classList.add('tr');
     pixelBoard.appendChild(divRow);
-    creatingPixelDivsCollumns(divRow, linhas);
+    creatingPixelBoardCollumns(divRow, linhas);
   }
 }
 
-creatingPixelDivs(5);
+creatingPixelBoardRows(5);
 
 const paletteColors = document.querySelectorAll('.color');
 
@@ -35,19 +35,20 @@ function addingEventListenersToPalette(elements) {
 
 addingEventListenersToPalette(paletteColors);
 
-const pixels = document.querySelectorAll('.pixel');
+let pixels = document.querySelectorAll('.pixel');
 
-function addingEventListenersToPixels() {
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', () => {
+function addingEventListenersToPixels(elements) {
+  for (let index = 0; index < elements.length; index += 1) {
+    elements[index].addEventListener('click', (event) => {
       const selColor = document.querySelector('.selected');
-      const BackCol = window.getComputedStyle(selColor, null).getPropertyValue('background-color');
-      pixels[index].style.backgroundColor = BackCol;
+      const bgColor = window.getComputedStyle(selColor, null).getPropertyValue('background-color');
+      const pixelClicked = event.target;
+      pixelClicked.style.backgroundColor = bgColor;
     });
   }
 }
 
-addingEventListenersToPixels();
+addingEventListenersToPixels(pixels);
 
 function clearingTheBoard() {
   const button = document.querySelector('#clear-board');
@@ -63,7 +64,7 @@ clearingTheBoard();
 const inputButton = document.querySelector('#generate-board');
 const input = document.querySelector('#board-size');
 
-function boardSize() {
+function changingBoardSize() {
   inputButton.addEventListener('click', () => {
     if (input.value === '') return alert('Board inválido!');
     const divsRowsPixels = pixelBoard.querySelectorAll('.tr');
@@ -72,11 +73,13 @@ function boardSize() {
     }
     if (input.value < 5) input.value = 5;
     if (input.value > 50) input.value = 50;
-    creatingPixelDivs(input.value);
+    creatingPixelBoardRows(input.value);
+    pixels = document.querySelectorAll('.pixel');
+    addingEventListenersToPixels(pixels);
   });
 }
 
-boardSize();
+changingBoardSize();
 
 window.onload = () => {
   const blackColor = document.querySelector('#first-color');
