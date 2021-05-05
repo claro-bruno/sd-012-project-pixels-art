35f691458bbe5607a974dd36bc3bfb66928ba504
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 16:08:43 -0300

    adding requirement 8

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ const pixelBoard = document.querySelector('#pixel-board');
 
 let medidas = 5;
 
-function creatingPixelDivs (linhas) {
-    for (let row = 1; row <= linhas; row += 1) {
-        const divRow = document.createElement('div');
-        divRow.classList.add('tr');
-        pixelBoard.appendChild(divRow);
-        creatingPixelDivsCollumns(divRow, linhas);
-    }
-}
-
 function creatingPixelDivsCollumns(row, colunas) {
     for (let column = 1; column <= colunas; column += 1) {
         const divColumn = document.createElement('div');
@@ -20,9 +11,33 @@ function creatingPixelDivsCollumns(row, colunas) {
     }
 }
 
+function creatingPixelDivs(linhas) {
+    for (let row = 1; row <= linhas; row += 1) {
+        const divRow = document.createElement('div');
+        divRow.classList.add('tr');
+        pixelBoard.appendChild(divRow);
+        creatingPixelDivsCollumns(divRow, linhas);
+    }
+}
+
+const paletteColors = document.querySelectorAll('.color'); 
+
+function addingEventListenersToPalette(elements) {
+    for (index = 0; index < elements.length; index += 1) {
+        elements[index].addEventListener('click', function(event) {
+            for (index = 0; index < elements.length; index += 1) {
+            elements[index].classList.remove('selected');    
+            }
+            event.target.classList.toggle('selected');
+        });
+    }   
+}
+
+addingEventListenersToPalette(paletteColors);
+
 creatingPixelDivs(medidas);
 
-window.onload = function () {
+window.onload = function() {
     const blackColor = document.querySelector('#first-color');
     blackColor.classList.add('selected');
 };
