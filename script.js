542c61f611bfddc5ced58a0e8975063ae3cd252a
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-05 18:52:35 -0300

    resolucao do 2 requisito

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let cores = document.getElementsByClassName("color");
+let paleta = document.getElementById("color-pallete");
+
+    paleta.children[0].style.backgroundColor = "blue";
+    paleta.children[1].style.backgroundColor = "yellow";
+    paleta.children[2].style.backgroundColor = "red";
+    paleta.children[3].style.backgroundColor = "green";
