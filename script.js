83c68395ff40bb2ae84471ebcdcbd6d129c320b0
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-05 18:53:36 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 window.onload = function() {
     let pixel_board = document.getElementById("pixel-board");
-    for (let i = 0; i < 5; i++) {
-        let linha_pixel = document.createElement("div");
-        linha_pixel.style.display = "block";
-        linha_pixel.style.height = "40px";
-        pixel_board.appendChild(linha_pixel);
-        for (let i = 0; i < 5; i++) {
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
-            pixel.style.backgroundColor = "white";
-            pixel.style.display = "inline-block";
-            pixel.style.width = "40px";
-            pixel.style.height = "40px";
-            pixel.style.border = "1px solid black";
-            linha_pixel.appendChild(pixel);
+    let board_size = document.getElementById("board-size");
+    let generate_board = document.getElementById("generate-board");
+    generate_board.addEventListener("click", setBoardSize);
+    let control_board = 0;
+
+    function createBoard(size) {
+        if(size < 5) {
+            size = 5;
+        }
+        if(size > 50) {
+            size = 50;
+        }
+        for (let i = 0; i < size; i++) {
+            let linha_pixel = document.createElement("div");
+            linha_pixel.style.display = "block";
+            linha_pixel.style.height = "40px";
+            pixel_board.appendChild(linha_pixel);
+            for (let i = 0; i < size; i++) {
+                let pixel = document.createElement("div");
+                pixel.className = "pixel";
+                pixel.style.backgroundColor = "white";
+                pixel.style.display = "inline-block";
+                pixel.style.width = "40px";
+                pixel.style.height = "40px";
+                pixel.style.border = "1px solid black";
+                linha_pixel.appendChild(pixel);
+            }
         }
     }
+
+    function setBoardSize() {
+        if(board_size.value == 0 || board_size.value < 0) {
+            alert("Board inválido!");
+        }
+        else{
+            if(control_board === 0) {
+                createBoard(board_size.value);
+                control_board++;
+            }
+            else{
+                let div = document.getElementById("pixel-board");
+                let size = div.childElementCount;
+                for(let i = 0; i < size; i++) {
+                    div.removeChild(div.firstChild);
+                }
+                createBoard(board_size.value);
+            }
+            let pixels = document.querySelectorAll(".pixel");
+            for(let i = 0; i < pixels.length; i++) {
+                pixels[i].addEventListener("click", paintPixel);
+            }
+        }
+    }
+
     let colors = document.querySelectorAll(".color");
     for(let i = 0; i < colors.length; i++) {
         colors[i].addEventListener("click", selectColor);
@@ -24,6 +62,7 @@ window.onload = function() {
     colors[1].style.backgroundColor = "red";
     colors[2].style.backgroundColor = "green";
     colors[3].style.backgroundColor = "blue";
+
     function selectColor(event) {
         for(let j = 0; j < colors.length; j++) {
             if(colors[j].className == "color selected") {
@@ -32,10 +71,7 @@ window.onload = function() {
         }
         event.target.className = "color selected";
     }
-    let pixels = document.querySelectorAll(".pixel");
-    for(let i = 0; i < pixels.length; i++) {
-        pixels[i].addEventListener("click", paintPixel);
-    }
+
     function paintPixel(event) {
         let cor;
         for(let i = 0; i < colors.length; i++) {
@@ -45,11 +81,15 @@ window.onload = function() {
         }
         event.target.style.backgroundColor = cor;
     }
+
     let clear = document.getElementById("clear-board");
     clear.addEventListener("click", clearBoard);
+
     function clearBoard() {
+        let pixels = document.querySelectorAll(".pixel");
         for (let i = 0; i < pixels.length; i++) {
             pixels[i].style.backgroundColor = "white";
         }
     }
+
 }
