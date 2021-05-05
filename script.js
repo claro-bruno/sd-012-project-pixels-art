5a7b0aa7d1187951f55850d6d31289315f95e886
Janderson Soares <jandinsm@gmail.com>
2021-05-05 17:19:37 -0300

    Desafio 2 borda 1px

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Pega os 4 p e seleciona
 let list = document.querySelectorAll(".color")
+// Variavel com 4 cores 
 let cores = ["black", "purple", "red", "blue"]
-    // let cores = ["black", "red", "blue", "purple"];
+
+// Define a cor de cada das 4 lista para cada cor das variavel cores
 function paletaArco(){
         for (let index = 0; index < list.length; index++) {
            list[index].style.background = cores[index];
