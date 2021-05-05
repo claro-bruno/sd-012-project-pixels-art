ffecda29904ed32ee0660cc7abde19df87da0552
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-05 16:30:57 -0300

    adding requirement 9

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ creatingPixelDivs(medidas);
 const paletteColors = document.querySelectorAll('.color'); 
 
 function addingEventListenersToPalette(elements) {
-    for (index = 0; index < elements.length; index += 1) {
+    for (let index = 0; index < elements.length; index += 1) {
         elements[index].addEventListener('click', function(event) {
             for (index = 0; index < elements.length; index += 1) {
             elements[index].classList.remove('selected');    
@@ -40,20 +40,31 @@ addingEventListenersToPalette(paletteColors);
 const pixels = document.querySelectorAll('.pixel');
 
 function addingEventListenersToPixels(elements) {
-    for (index = 0; index < elements.length; index += 1) {
+    for (let index = 0; index < elements.length; index += 1) {
         elements[index].addEventListener('click', function(event) {
             const selectedColor = document.querySelector('.selected');
-            let selectedBackgroundColor = window.getComputedStyle(selectedColor, null).getPropertyValue("background-color");
+            let selectedBackgroundColor = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
             event.target.style.backgroundColor = selectedBackgroundColor;
             console.log(selectedColor.style.backgroundColor);
             event.target.classList.toggle('teste');
         });
-    } 
+    }
 }
 
 addingEventListenersToPixels(pixels);
 
-window.onload = function() {
+function clearingTheBoard(elements) {
+    const button = document.querySelector('#clear-board');
+    button.addEventListener('click', function() {
+        for (let index = 0; index < elements.length; index += 1) {
+            elements[index].style.backgroundColor = 'white';
+        }
+    });
+}
+
+clearingTheBoard(pixels);
+
+window.onload = function () {
     const blackColor = document.querySelector('#first-color');
     blackColor.classList.add('selected');
 };
