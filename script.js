b910dcfff6959b4e22b6b850adb028c7c9ed1085
Augusto <augustocm2002@yahoo.com.br>
2021-05-05 20:28:17 -0300

    Fazendo requisito 2

---
+++ script.js
@@ -0,0 +1,11 @@
+function ola (){
+let colorPalette = document.getElementsByClassName("color");
+
+
+for (let index = 0; index <colorPalette.length; index += 1){
+    let colorPalette = colorPalette[index];
+    return colorPalette
+}
+
+}
+console.log(ola)
