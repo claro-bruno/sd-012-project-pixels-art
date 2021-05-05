63d337e9de7d23da0b89b143121af9240d68f07c
Ramon Paschoal <rpaschoal90@gmail.com>
2021-05-05 17:28:25 -0300

    Exercicio 4

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 let capturaColorPalette = document.querySelector('#color-palette');
 
-
 //Exercício 2 e 3:
 
-let paleta = ['Preto', 'Vermelho', 'Azul', 'Amarelo'];
+let paleta = ['black', 'red', 'blue', 'yellow'];
 function lista (array) {
     for(index = 0; index < paleta.length; index += 1) {
         let criarLi = document.createElement('li');
         capturaColorPalette.appendChild(criarLi);
         criarLi.className = 'color';
         let capturaLi = document.getElementsByTagName('li')
-        if(array[index] === 'Preto'){
-            capturaLi[index].style.backgroundColor = 'black';
-        } else if(array[index] === 'Vermelho'){
-            capturaLi[index].style.backgroundColor = 'red';
-        } else if(array[index] === 'Azul'){
-            capturaLi[index].style.backgroundColor = 'blue';
-        } else if(array[index] === 'Amarelo'){
-            capturaLi[index].style.backgroundColor = 'yellow';
+        if(array[index] === 'black'){
+            capturaLi[index].style.backgroundColor = array[index];
+        } else if(array[index] === 'red'){
+            capturaLi[index].style.backgroundColor = array[index];
+        } else if(array[index] === 'blue'){
+            capturaLi[index].style.backgroundColor = array[index];
+        } else if(array[index] === 'yellow'){
+            capturaLi[index].style.backgroundColor = array[index];
         }
     }
 }
 
 lista (paleta)
+
+//===========================================================
+
+// Exercício 4:
+
+let contadorLinha = 0;
+let contadorColuna = 0;
+let quadroPixel = document.createElement('section');
+    document.body.appendChild(quadroPixel);
+    quadroPixel.id = 'pixel-board'
+function criaQuadro (tamanho) {
+    for(let index = 0; index < tamanho; index += 1) {
+        let linhaQuadro = document.createElement('div');
+        quadroPixel.appendChild(linhaQuadro);
+        for (let index = 0; index < tamanho; index += 1) {
+            let criaPixels = document.createElement('li')
+            linhaQuadro.appendChild(criaPixels);
+            criaPixels.className = 'pixel';
+        }
+    }
+   
+}
+
+criaQuadro(5)
 
