b8a9ebda2bb1a8965f8f2bc1f6a8982ab1d46034
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-05 19:16:31 -0300

    feita a grid e adicionada a função de passar a cor selecionada para o pixel escolhido através de click

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 window.onload = function () {
 
     let colorSelection = document.getElementById('color-palette');
+    let selectedColor = undefined;
+    let boardPixels = document.querySelector('#pixel-board');
+
+
+    createBoard();
 
     colorSelection.addEventListener('click', function () {
-        let selectedColor = event.target.id;
-        let nomeDaCorEscolhida = document.querySelector(".selected-color");
+        selectedColor = event.target.id;
+        let colorName = document.querySelector(".selected-color");
 
-        nomeDaCorEscolhida.innerHTML = selectedColor.toUpperCase();
+        colorName.innerHTML = selectedColor.toUpperCase();
+    });
+
+    boardPixels.addEventListener("click", function () {
+        event.target.id = selectedColor;
     })
+
+    function createBoard() {
+        for (let pixel = 0; pixel < 25; pixel++) {
+            let square = document.createElement('div');
+            square.className = "color";
+            square.id = "erased";
+            document.querySelector('#pixel-board').appendChild(square);
+            boardPixels.style.width = 210 + "px";
+            boardPixels.style.height = 210 + 'px';
+        }
+    };
+
 }
+
