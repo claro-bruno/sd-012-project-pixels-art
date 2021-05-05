27a7885d0a6547d7bbcb608fdef533cdc0298f00
Eric Faria <ericdeofaria@gmail.com>
2021-05-05 20:25:06 -0300

    Algumas correcoes de lint

--- script.js
+++ script.js
@@ -19,32 +19,32 @@ function createPalette() {
   div.style.backgroundColor = 'black';
   colorPalette.appendChild(div);
   for (let index = 0; index < 3; index += 1) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.className = 'color';
     div.style.backgroundColor = randomRGB();
-    colorPalette.appendChild(div); 
+    colorPalette.appendChild(div);
   }
 }
 
-function createBoard () { 
+function resetBoard() {
+  while (pixelBoard.firstChild) {
+    pixelBoard.removeChild(pixelBoard.lastChild);
+  }
+}
+
+function createBoard() { 
   let size = boardSize.value;
   if (size < 5 || size > 50 || size == null) {
-    window.alert('Board inválido!')
+    window.alert('Board inválido!');
     if (size < 5 || size == null) { size = 5; }
     if (size > 50) { size = 50; }
-  } 
+  }
   resetBoard();
   pixelBoard.style.width = (size * 42) + 'px';
   for (let row = 1; row <= size*size; row += 1) {
     let div = document.createElement('div');
     div.className = 'pixel';
     pixelBoard.appendChild(div);
-  }
-}
-  
-function resetBoard() {
-  while (pixelBoard.firstChild) {
-    pixelBoard.removeChild(pixelBoard.lastChild);
   }
 }
 
@@ -69,7 +69,7 @@ function clearBoard() {
   }
 }
 
-window.onload = function pageLoad () {
+window.onload = function pageLoad() {
   createPalette();
   pixelBoard.addEventListener('click', changeColor);
   for (let index = 0; index < pallete.length; index += 1) {
