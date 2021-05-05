a1c70b665f7a1e3fe9ba40cd5155d3721f93e269
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-05 17:26:15 -0300

    Feitos requisito 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// array com as cores da paleta
 const colors = document.getElementsByClassName('color');
 
+// insere as cores passadas como parametros como background no elementos passados como parametro
 function inputColorsPalett(arrayElements, arrayColors) {
   for (let index = 0; index < arrayElements.length; index += 1) {
     console.log(arrayColors[index]);
@@ -7,6 +9,25 @@ function inputColorsPalett(arrayElements, arrayColors) {
   }
 }
 
+// array com as cores iniciais do padrão RGB
 const RGBColors = ['#000000', '#800000', '#008000', '#000080'];
 
 inputColorsPalett(colors, RGBColors);
+
+const pixelBoard = document.getElementById('pixel-board');
+
+function createPixelGrind(lines, colums, fatherElement) {
+  for (let index = 0; index < lines; index += 1) {
+    const divLine = document.createElement('div');
+    divLine.className = 'pixel-line';
+    for (let indexCol = 0; indexCol < colums; indexCol += 1) {
+      const divColum = document.createElement('div');
+      divColum.className = 'pixel';
+      divColum.style.backgroundColor = '#ffffff';
+      divLine.appendChild(divColum);
+    }
+    fatherElement.appendChild(divLine);
+  }
+}
+
+createPixelGrind(5, 5, pixelBoard);
