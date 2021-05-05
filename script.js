6ed8d3f4711e6ae574c093c14a1511a6afd7cf8e
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-05 20:09:45 -0300

    pausando tudo

---
+++ script.js
@@ -0,0 +1,15 @@
+let div1 = document.createElement('div1');
+div1.className = 'color';
+div.appendChild(div1);
+
+let div2 = document.createElement('div2');
+div2.className = 'color';
+div.appendChild(div2);
+
+let div3 = document.createElement('div3');
+div3.className = 'color';
+div.appendChild(div3);
+
+let div4 = document.createElement('div4');
+div4.className = 'color';
+div.appendChild(div4);
