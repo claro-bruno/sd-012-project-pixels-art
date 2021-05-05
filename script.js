cd86ac9b9d8a208221520798ca42dc7204b92236
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-05-05 17:06:04 -0300

    Criado quadro de pixels direto no html e css, futuramente sera substituido pela forma dinamica

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 window.onload = paletaDeCores();
 
+quadroBranco();
+
 function paletaDeCores (){
 
-const paletacores = document.getElementsByClassName('color');
+const paletaCores = document.getElementsByClassName('color');
 
-paletacores[0].style.backgroundColor = 'black';
-paletacores[1].style.backgroundColor = 'yellow';
-paletacores[2].style.backgroundColor = '#ff9933';
-paletacores[3].style.backgroundColor = '#009933';
+paletaCores[0].style.backgroundColor = 'black';
+paletaCores[1].style.backgroundColor = 'yellow';
+paletaCores[2].style.backgroundColor = '#ff9933';
+paletaCores[3].style.backgroundColor = '#009933';
+
+// Função para gerar os pixels do quadro branco //
+
+function quadroBranco (){
+    let alturaQuadro = 5;
+    let larguraQuadro = 5;
+    const quadroBranco = document.getElementById('pixel-board');
+    const criarPixel = document.createElement('div');
+
+
+    for (let index = 0; index < alturaQuadro; index += 1) {
+    for (let indice = 0; indice < larguraQuadro; indice += 1) {
+        criarPixel.className('pixel')
+        quadroBranco.appendChild(criarPixel);
+    }
 }
 
+}
+}
