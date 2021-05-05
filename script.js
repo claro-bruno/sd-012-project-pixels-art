18290651c4f0b4f544fd34f6bcd386478800df5d
alestori <alestori7@gmail.com>
2021-05-05 11:07:32 -0300

    Adds color palette items dynamically

---
+++ script.js
@@ -0,0 +1,17 @@
+const createPalette = (color) => {
+  const palette = document.getElementById('color-palette');
+  const colorSquare = document.createElement('span');
+  colorSquare.style.backgroundColor = color;
+  colorSquare.classList.add('color');
+  colorSquare.id = color;
+  palette.appendChild(colorSquare);
+
+  if (color === 'black') {
+    colorSquare.classList.add('selected');
+    colorSquare.style.boxShadow = '2px 2px 2px grey';
+  }
+};
+createPalette('black');
+createPalette('red');
+createPalette('green');
+createPalette('blue');
