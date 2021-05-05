196a56bb5b1cb3ee6cebc506d86e318a4ead5a4b
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-05 18:41:36 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ document.getElementsByClassName("color")[2].style.backgroundColor = "blue"
 document.getElementsByClassName("color")[2].innerHTML = "Blue"
 document.getElementsByClassName("color")[3].style.backgroundColor = "orange"
 document.getElementsByClassName("color")[3].innerHTML = "Orange"
+
+// funcoes para formar o quadro de pixels
+function linhasQuadrado (linhas){
+    for (let index = 0; index < linhas; index+=1){
+        let linha = document.createElement("div");
+        linha.className = "linha";
+        document.getElementById("pixel-board").appendChild(linha);
+    };
+};
+linhasQuadrado(5);
+
+function preencherLinha (colunas){
+    let listaLinha = document.getElementsByClassName("linha");
+    for (let element of listaLinha){
+        for (let coluna = 0; coluna < colunas; coluna +=1){
+            let listaColuna = document.createElement("div");
+            listaColuna.className = "pixel";
+            element.appendChild(listaColuna);
+        };
+    };
+};
+preencherLinha(5);
