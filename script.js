41e2e70de33ee86a999aa1427f6082f0bd39542e
matheus <matheus111904@hotmail.com>
2021-05-05 17:59:09 -0300

    desafio 4 teste

--- script.js
+++ script.js
@@ -4,8 +4,36 @@ function mudarCorPaleta (array){
     for (index = 0; index < paleta.length; index += 1){
         for (index = 0; index < arrayCores.length; index += 1){
             paleta[index].style.backgroundColor = arrayCores[index];
-            console.log(arrayCores[index])
         }
     }
 }
 mudarCorPaleta();
+
+let quadroPixel = document.createElement('table');
+    document.body.appendChild(quadroPixel);
+    quadroPixel.id = 'pixel-board'
+function createBoard (pixeis) {
+    for(let index = 0; index < pixeis; index += 1) {
+        let lineCanvas = document.createElement('tr');
+        quadroPixel.appendChild(lineCanvas);
+        for (let index = 0; index < pixeis; index += 1) {
+            let criaPixels = document.createElement('td')
+            lineCanvas.appendChild(criaPixels);
+            criaPixels.className = 'pixel';
+        }
+    }
+}
+createBoard(5);
+
+// function createPixels (){
+//     let pixels = document.getElementsByClassName('canvas')
+//     for (let index = 0; index <= columns; index += 1){
+//         let criarColuna = document.createElement('tr')
+//         pixels.append('tr');
+        
+//     }
+//     for (let index = 0; index <= lines; index += 1){
+//         let criarLinha = document.createElement('td')
+//     }
+// }
+// createPixels();
