94ac48409c39869a3b36b2cd39723f7fca9d06f1
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-05 17:58:41 -0300

    Tenta requisito 7 e conclui requisito 6

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    
+}
+
+function selectAColor(event) {
+    let color = document.querySelectorAll('.color');
+    for (let index = 0; index < color.length; index += 1) {
+        color[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+}
+let pickAColor = document.querySelector('#color-palette');
+pickAColor.addEventListener('click', selectAColor);
