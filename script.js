d8cbd479b514eeae2fce89c388c7af139bd78df9
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-05 19:40:10 -0300

    insere a resposta do requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable sonarjs/no-extra-arguments */
+/* eslint-disable sonarjs/no-duplicate-string */
 /* eslint-disable no-undef */
 function colorPallete() {
   const colors = ['black', 'blue', 'red', 'green'];
@@ -6,10 +10,43 @@ function colorPallete() {
     colorsPalette[index].style.backgroundColor = colors[index];
   }
 }
-colorPallete();
 
 function getInicialColor() {
   const inicialColor = document.querySelector('.selected');
   inicialColor.style.backgroundColor = 'black';
 }
-getInicialColor();
+
+function selectColor(event) {
+  const selectedPaletteColor = document.querySelector('.selected');
+  selectedPaletteColor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+function addEventOnClick() {
+  const getColorClasses = document.querySelectorAll('.color');
+  for (let index = 0; index < getColorClasses.length; index += 1) {
+    getColorClasses[index].addEventListener('click', selectColor);
+  }
+}
+
+function changePixelColor(event) {
+  const paletteColorSelected = document.querySelector('.selected');
+  const pixelClicked = event.target;
+
+  pixelClicked.style.backgroundColor = paletteColorSelected.style.backgroundColor;
+}
+
+function getColor(event) {
+  let selectedColor = document.querySelector('.selected').style.backgroundColor;
+  event.target.style.backgroundColor = selectedColor;
+}
+
+function getColorEvent() {
+  let square = document.getElementsByClassName('pixel');
+  for (let index = 0; index < square.length; index += 1) {
+    square[index].addEventListener('click', getColor);
+  }
+}
+
+colorPallete();
+addEventOnClick();
