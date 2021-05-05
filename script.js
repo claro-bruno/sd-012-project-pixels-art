aee15f5b1f0178d63222032a8241bd7f9111086f
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-05-05 19:24:50 -0300

    exercicio 6 - declarando a classe selected

--- script.js
+++ script.js
@@ -13,9 +13,23 @@ function quadroPixel(){
   for(let linha = 1; linha <= 5;linha +=1){
       for(let coluna =1 ; coluna <= 5 ; coluna +=1){
           let quadrinho = document.createElement('div');
-          quadrinho.className = 'pixel'
+          quadrinho.className = 'pixel';
           quadro.appendChild(quadrinho)
       }
   };
 };
 quadroPixel();
+
+function corInicial(){
+//adiciona a nova classe das classes color
+let cor = document.querySelectorAll('.color');
+cor[0].className += ' selected';
+for(let ICor = 0; ICor < cor.length;ICor+=1){
+  cor[ICor]
+}
+
+// muda a cor dos pixel para a mesma cor do item com classe selected
+
+
+}
+corInicial();
