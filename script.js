5d1d097621e4b053f8a464df4b65406bf20c66bf
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-05 19:27:06 -0300

    Ajustes de erros marcados pelo lint

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.onload = initialBrush;
-
-function initialBrush () {
+function initialBrush() {
   document.querySelector('#color1').classList.add('selected');
 }
 
-function changeBrush (event) {
+window.onload = initialBrush;
+
+function changeBrush(event) {
   if (event.target.classList.contains('color')) {
-    let palette = document.getElementsByClassName('color');
+    const palette = document.getElementsByClassName('color');
     for (let index = 0; index < palette.length; index += 1) {
       palette[index].classList.remove('selected');
     }
@@ -14,25 +14,18 @@ function changeBrush (event) {
   }
 }
 
-let colorPalette = document.querySelector('#color-palette');
+const colorPalette = document.querySelector('#color-palette');
 
 colorPalette.addEventListener('click', changeBrush);
 
-function pixelMark () {
-  let canvas = document.getElementsByClassName('pixel');
-  for (let index = 0; index < canvas.length; index += 1) {
-    canvas[index].id = 'pixel' + index;
-  }
-}
-pixelMark();
-
-function paint (event) {
+function paint(event) {
   if (event.target.classList.contains('pixel')) {
-    //let brushColor = document.querySelector('.selected').style.backgroundColor;
-    event.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+    const paletteColor = document.querySelector('.selected');
+    const brushColor = window.getComputedStyle(paletteColor).backgroundColor;
+    event.target.style.backgroundColor = brushColor;
   } 
 }
 
-let frame = document.querySelector('#pixel-board');
+const frame = document.querySelector('#pixel-board');
 
 frame.addEventListener('click', paint);
