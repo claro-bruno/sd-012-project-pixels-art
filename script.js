dafdc04a893149ee3621f5a18a1e9ac671e19c3a
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-05-05 16:41:21 -0300

    entregando requisito 4 comentado passo a passo para o CHECK

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ const yellow = document.querySelectorAll('.color')[2].style.backgroundColor = 'y
 
 const blue = document.querySelectorAll('.color')[3].style.backgroundColor = 'blue';
 
-//CRIANDO A PALETA DE CORES:
+//CRIANDO A TABELA DE PIXELS:
+
+//CRIA VARIAVEL PARA RESGATAR O ELEMENTO PELO ID:
+const pixelBoard = document.getElementById('pixel-board');
+
+//IMPLEMENTANDO A LOGICA PARA CRIAÇÃO DO QUADRO DE PIXEL:
+//O PRIMEIRO FOR ELE CRIA UM LAÇO DE REPETIÇÃO PARA PERCORRER AS 5 POSIÇOES DO QUADRO, CRIEI UMA VARIAVEL QUE CRIA UM ELEMENTO DIV ONDE ARMAZENA AS CLASSES PARA ESTILIZAÇÃO E MANIPULAÇÃO, DEPOIS COLOQUEI ELA COMO FILHA DA DIV PAI DO QUADRO (pixelBoard)
+//O SEGUNDO FOR PERCORRE TBM AS 5 POSIÇÕES DO QUADRO, POREM PARA DETERMINAR AS CONDIÇÕES DE CADA PIXEL (eachPixel), CRIEI UMA VARIAVEL PARA CRIAR UM ELEMENTO div, COLOQUEI CADA QUADRO DE PIXEL A CLASSE pixel, E COLOQUEI A div eachPixel COMO FILHA DA div linhaDePixel, AO FINAL ADICIONEI A COR DE FUNDO COMO BRANCA PARA CADA QUADRADO DE PIXEL (eachPixel.style.backgroundColor = 'white';) 
+
+for (let index = 0; index < 5; index += 1) {
+    const linhaDePixel = document.createElement('div');
+    linhaDePixel.className = 'pixelLine';
+    pixelBoard.appendChild(linhaDePixel);
+    
+    for (let index = 0; index < 5; index += 1) {
+      const eachPixel = document.createElement('div');
+      eachPixel.className = 'pixel';
+      linhaDePixel.appendChild(eachPixel);
+      eachPixel.style.backgroundColor = 'white';
+    }
+  }
+
 
 
