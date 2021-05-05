5a8040f4455aab63fd19e821a06b3e17a931b588
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-05 17:04:41 -0300

    Insere os requisitos 2, 3 e 4

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+/* eslint-disable no-undef */
+function colorPallete() {
+  const colors = ['black', 'blue', 'red', 'green'];
+  let colorsPalette = document.querySelectorAll('.color');
+  for (let index = 0; index < colorsPalette.length; index += 1) {
+    colorsPalette[index].style.backgroundColor = colors[index];
+  }
+}
+colorPallete();
