a95597a2274a4afb8efc73f24a60a43c95e7076b
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-05 16:02:07 -0300

    requisito 6

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function paletteGenerator() {
   let colors = ['black', 'green', 'blue', 'orange'];
   let paletteSection = document.getElementById('color-palette');
 
-
   for (let index = 0; index < colors.length; index += 1) {
     let createDiv = document.createElement('div');
     paletteSection.appendChild(createDiv);
     createDiv.className = 'color';
     createDiv.style.backgroundColor = colors[index];
+    if (index === 0){
+      createDiv.classList.add('selected');
+    }
   }
 }
 paletteGenerator();
