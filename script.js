ff24b56efd96d89e16b770f3abbc9b51623181d5
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-05-05 16:49:38 -0300

    Requisito 8 - Adiciona funcionalidade que pinta o pixel com a cor selecionada

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 window.onload = function () {
   const getColorPalette = document.getElementById('color-palette');
   const getColors = document.getElementsByClassName('color');
+  const getPixelBoard = document.getElementById('pixel-board')
 
   function changeColorSelected(event) {
-    let colorSelected = document.querySelector('.selected');
+    const colorSelected = document.querySelector('.selected');
     colorSelected.classList.remove('selected');
     event.target.classList.add('selected');
   }
   for (let color of getColors) {
     color.addEventListener('click', changeColorSelected);
   }
+
+  function fillPixel(event){
+   const refreshColorSelected = document.querySelector('.selected');
+   const style = getComputedStyle(refreshColorSelected)
+   event.target.style.backgroundColor = style.backgroundColor;
+  }
+  getPixelBoard.addEventListener('click',fillPixel);
+
+  
 };
