b473d780afe717f930c70259b3b4fb93926eda34
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-05 15:59:26 -0300

    Feito o requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const pixelBoard = document.querySelector('#pixel-board');
+const color = document.querySelectorAll('.color');
 
-for (let i = 0; i < 5; i += 1) {
-  for (let j = 0; j < 5; j += 1) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixelBoard.appendChild(pixel);
-    if (j === 4) {
-      const br = document.createElement('br');
-      pixelBoard.appendChild(br);
+function addNewLine(j) {
+  if (j === 4) {
+    const br = document.createElement('br');
+    pixelBoard.appendChild(br);
+  }
+}
+
+function createBoard() {
+  for (let i = 0; i < 5; i += 1) {
+    for (let j = 0; j < 5; j += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelBoard.appendChild(pixel);
+      addNewLine(j);
     }
   }
+}
+
+createBoard();
+
+for (let i = 0; i < color.length; i += 1) {
+  color[i].addEventListener('click', () => {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    color[i].classList.add('selected');
+    console.log(color[i]);
+  });
 }
