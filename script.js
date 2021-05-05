73be23168239fde4996be8d74fce342d877f75ae
André Moreno <deco616@hotmail.com>
2021-05-05 18:10:05 -0300

    Colocando logica da matriz de Divs

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+
+
+gerarBoard(5);
+}
+
+function gerarBoard(numero) {
+    let pixelBoard = document.getElementById('pixel-board');
+    for (let index = 0; index < numero; index += 1){
+        console.log(index);
+        let primeiraDiv = document.createElement('div');
+        pixelBoard.appendChild(primeiraDiv);
+        for (let index2 = 0; index2 < numero; index2 += 1){
+          let divInterna = document.createElement('div');
+          divInterna.className = 'box';
+          primeiraDiv.appendChild(divInterna);
+    }
+  }
+  
+}
