dd071646b29af60e91e6525a8efe4149a3e0d2c2
Emerson Filho <emerson.dev.machado@gmail.com>
2021-05-05 16:39:52 -0400

    resolve o requisito obrigatorio 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-const caixaDeCorDaPaleta = document.querySelectorAll('.color');
+const gerarPaletaDeCorAleatoria = () => {
+    const caixaDeCorDaPaleta = document.querySelectorAll('.color');
 
-const randomColorPalette = () => {
     caixaDeCorDaPaleta[0].style.backgroundColor = 'black';
     
     for (let index = 1; index < caixaDeCorDaPaleta.length; index += 1) {
@@ -11,4 +11,28 @@ const randomColorPalette = () => {
         }
         caixaDeCorDaPaleta[index].style.backgroundColor = `rgb(${randomColor})`;
     }
+}
+
+const gerarTelaDePintura = (tamanho) => {
+    const areaDaTelaDePintura = document.querySelector('#telaDePintura')
+    
+    for (let altura = 1; altura <= tamanho; altura += 1) {
+        let pixel = document.createElement('div')
+        pixel.className = 'alturaDaTelaDePintura'
+        areaDaTelaDePintura.appendChild(pixel)
+    }
+
+    const linha = document.querySelectorAll('.alturaDaTelaDePintura')
+    
+    for (let index = 0; index <= linha.length; index += 1) {
+        for (let largura = 1; largura <= tamanho; largura += 1) {
+            let pixel = document.createElement('div')
+            pixel.className = 'pixel'
+            linha[index].appendChild(pixel)
+        }
+    }
+}
+
+const mudarTamanhoTelaDePintura = () => {
+    const inputDoTamanhoDaTela = document.querySelector('#tamanhoTelaDePintura').value   
 }
