ea36c8782e380255a559a45eb34743bbe1c77231
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-05-05 16:21:28 -0300

    colocando a cor preta na primeira posicao da paleta

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function cores(){
 let cores = document.querySelectorAll('.color');
-let arraycolor = ['#0d3b66','#faf0ca','#f4d35e','#ee964b','#f95738']
+let arraycolor = ['#000','#faf0ca','#f4d35e','#ee964b','#f95738']
 for(let index =0; index < cores.length;index +=1){
 cores[index].style.backgroundColor = arraycolor[index];
 }
