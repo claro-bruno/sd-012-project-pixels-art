102835da7d4e15d4c4103e56b7d3c56b82556e3b
RyanLaiber <ryanlaiber@gmail.com>
2021-05-05 19:09:51 -0300

    adiciona requisito 9 botao limpa quadro

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-//variáveis
+//  variáveis
 let linhas = 5;
 let colunas = 5;
 let cores = document.getElementsByClassName("color");
 let QuadroDePixels = document.getElementById("pixel-board");
+let botao = document.getElementById("clear-board");
 
-//funções para formar quadro de pixels
+//  funções para formar quadro de pixels
 function formaLinha(linhas){
     for (let linha = 0; linha < linhas; linha += 1){
         let divLinha = document.createElement("div");
@@ -26,29 +27,39 @@ function formaPixelsLinha (colunas){
 };
 formaPixelsLinha(colunas);
 
-//adiciona caminho para pixels
+//  adiciona caminho para pixels
 let pixels = document.getElementsByClassName("pixel");
 
-//função de selecionar cor
+//  função de selecionar cor
 function selecionaCor(event){
     let corAntes = document.querySelector(".selected");
     corAntes.classList.remove("selected");
     event.target.classList.add("selected");
 };
 
-//adiciona eventListener às cores
+//  adiciona eventListener às cores
 for (let cor of cores){
     cor.addEventListener("click", selecionaCor);
 };
 
-//função que muda a cor dos pixels
+//  função que muda a cor dos pixels
 function mudaCorPixel(event){
     let corSelecionada = document.querySelector(".selected");
     let corDoClick = corSelecionada.id;
     event.target.style.backgroundColor = corDoClick;
 };
 
-//adiciona eventlistener nos pixels
+//  adiciona eventlistener nos pixels
 for (let element of pixels){
     element.addEventListener("click", mudaCorPixel);
 };
+
+//  função que limpa a cor dos pixels
+function limpaPixel(){
+    for (let element of pixels){
+        element.style.backgroundColor = "white";
+    };
+};
+
+//  adiciona eventlistener em botão
+botao.addEventListener("click", limpaPixel);
