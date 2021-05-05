bef37d304da3745e90612c36d38ad67e772df5c1
Milton Castro <castro.milton07@gmail.com>
2021-05-05 20:17:43 -0300

    07 - Acresceta modificaçoes para atender aos requisitos 3, 5, 6, 7 e 8

--- script.js
+++ script.js
@@ -8,9 +8,22 @@ function getElementsColorPalette() {
 }
 getElementsColorPalette();
 
+function clearSelected() {
+  const oldSelected = document.querySelector('.selected');
+  if (oldSelected.parentElement.id === 'color-palette') {
+    oldSelected.className = 'color';
+  }
+  if (oldSelected.parentElement.id === 'extended-palette') {
+    oldSelected.className = 'color-extended';
+  }
+}
+
 function changeSelectedColor(event) {
   const selectedBox = document.querySelector('#color-selected');
   selectedBox.style.backgroundColor = event.target.style.backgroundColor;
+  clearSelected();
+  const newSelected = event.target;
+  newSelected.className = 'color selected';
 }
 
 function createEventsColorPalette() {
@@ -19,3 +32,25 @@ function createEventsColorPalette() {
   }
 }
 createEventsColorPalette();
+
+const pixels = [];
+function getElementsPixels() {
+  for (let index = 1; index <= 25; index += 1) {
+    const id = `pixel-td-${index}`;
+    pixels[index] = document.getElementById(id);
+  }
+}
+getElementsPixels();
+
+function changePixelColor(event) {
+  const selectedColor = document.querySelector('.selected');
+  const selectedPixel = event.target;
+  selectedPixel.style.backgroundColor = selectedColor.style.backgroundColor;
+}
+
+function createEventsPixels() {
+  for (let index = 1; index <= 25; index += 1) {
+    pixels[index].addEventListener('click', changePixelColor);
+  }
+}
+createEventsPixels();
