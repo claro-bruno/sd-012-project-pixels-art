19e95564bc15f7bfd6ea5ddf0961870e3160901a
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-05 17:09:13 -0300

    Requisito 2 correção

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const colorizeBorder = 'green';
+const shadows = '2px 2px 2px green';
 
 function selectedColorPrimary() {
   const onloadBlackColor = document.querySelector('div', '.color', '.selected');
   const pixels = document.querySelectorAll('.pixel');
+  const primary = document.getElementsByClassName('color')[0];
+  console.log(primary.style.color);
 
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].addEventListener('click', () => {
       pixels[index].style.background = 'black';
     });
   }
-  onloadBlackColor.style.borderColor = colorizeBorder;
+  onloadBlackColor.style.boxShadow = shadows;
 }
 
 window.onload = () => {
