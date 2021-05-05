e0d4bab984c5dc8d389450307e2045d9fde56f2b
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-05-05 16:12:57 -0300

    Adiciona requisito 8 - Pinta pixels

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Criando Palleta
+
+function palleteCreate () {
+    let colorPallete = document.querySelector("#color-palette")
+
+    let firstElement = document.createElement("div");
+    firstElement.className = "color selected";
+    firstElement.style.backgroundColor = "black";
+
+    colorPallete.appendChild(firstElement);
+
+
+    for (let index = 0; index < 3; index += 1){
+        let element = document.createElement("div");
+        element.className = "color";
+        element.style.backgroundColor = hexColorGeneretor();
+        colorPallete.appendChild(element);
+    };
+};
+
+palleteCreate();
+
+    // Gerar cor aeatória
+    // Conceito pego no site WallaceMaxters, disponívem em : https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript;
+
+function hexColorGeneretor () {
+    return "#" + parseInt((Math.random() * 0xFFFFFF)).toString(16).padStart(6, "0");
+}
+
 // Selecionar a cor
 
 function selectedColor (event){
@@ -13,3 +42,16 @@ for (let index = 0; index < colorsList.length; index += 1) {
     colorsList[index].addEventListener("click", selectedColor);
 };
 
+// Pintar Pixel
+
+function coloringPixel (event) {
+    let colorSelected = document.querySelector(".selected").style.backgroundColor;
+    event.target.style.backgroundColor = colorSelected;
+};
+
+let pixelsList = document.querySelectorAll(".pixel");
+
+for(let index = 0; index < pixelsList.length; index += 1) {
+    pixelsList[index].style.backgroundColor = "white";
+    pixelsList[index].addEventListener("click", coloringPixel);
+};
