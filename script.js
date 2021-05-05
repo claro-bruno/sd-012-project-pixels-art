63ce2decb527fa35757587e857eb05586a6b7705
Daniel <danielbped@gmail.com>
2021-05-05 15:26:42 -0300

    Resolve requisito 4

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 let paletaCores = document.querySelector('#color-palette');
+let quadro = document.querySelector('#pixel-board');
 
-let listaCores = ['black', 'yellow', 'blue', 'green'];
+let tamanhoDoQuadrado = '30px';
 
-for (let index = 0; index < listaCores.length; index += 1) {
-  let novaCor = document.createElement('li');
-  let novaCorSize = '30px';
-  novaCor.className = 'color';
-  novaCor.style.listStyle = 'none';
-  novaCor.style.width = novaCorSize;
-  novaCor.style.height = novaCorSize;
-  novaCor.style.border = '1px solid black';
-  novaCor.style.backgroundColor = listaCores[index];
-  paletaCores.appendChild(novaCor);
+let coresPaleta = ['black', 'yellow', 'blue', 'green'];
+
+let numeroDePixelsHorizontal = 5;
+let numeroDePixelsVertical = 5;
+let corInicialQuadro = 'white';
+
+criaPaletaCores(coresPaleta, tamanhoDoQuadrado);
+criaQuadro(
+  numeroDePixelsHorizontal,
+  numeroDePixelsVertical,
+  tamanhoDoQuadrado,
+  corInicialQuadro
+);
+
+function criaPaletaCores(cores, tamanho) {
+  for (let index = 0; index < cores.length; index += 1) {
+    let novaCor = document.createElement('li');
+    let novaCorSize = tamanho;
+    novaCor.className = 'color';
+    novaCor.style.listStyle = 'none';
+    novaCor.style.width = novaCorSize;
+    novaCor.style.height = novaCorSize;
+    novaCor.style.border = '1px solid black';
+    novaCor.style.backgroundColor = cores[index];
+    paletaCores.appendChild(novaCor);
+  }
+}
+
+function criaQuadro(height, width, tamanho, color) {
+  for (let index = 0; index < height; index += 1) {
+        let novaColuna = document.createElement('div');
+        quadro.appendChild(novaColuna);
+
+
+    for (let index = 0; index < width; index += 1) {
+        let novaLinha = document.createElement('li');
+        let novaLinhaSize = tamanho;
+        novaLinha.className = 'pixel';
+        novaLinha.style.listStyle = 'none';
+        novaLinha.style.backgroundColor = color;
+        novaLinha.style.width = novaLinhaSize;
+        novaLinha.style.height = novaLinhaSize;
+        novaLinha.style.border = '1px solid black';
+        novaColuna.appendChild(novaLinha);
+    }
+  }
 }
