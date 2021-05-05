1ff077114dc9173d5a9d9f283746ac4f759b6e6c
julio11lp <julio11lp@hotmail.com>
2021-05-05 18:51:50 -0300

    Criterio 5 utilizando JS

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function initialColor() {
 
 resetColors();
 
+function makeBoard(tamanho) {
+    let destiny = document.querySelector('#pixel-board');
+
+    if (tamanho > 10) {
+        return 'Tamanho maximo e 10.'
+    }
+    for (let index = 1; index <= tamanho; index += 1) {
+        let lines = document.createElement('div');
+        destiny.appendChild(lines);
+            for (let index = 1; index <= tamanho; index += 1) {
+                let columns = document.createElement('div');
+                columns.className = 'pixel';
+                lines.appendChild(columns);
+        }
+    }
+}
+
+makeBoard(5);
