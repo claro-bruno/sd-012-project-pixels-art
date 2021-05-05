65c788798e8c481ddbcf123a29e5535147854f3b
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-05 19:01:17 -0300

    requisito 6

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const table = document.querySelector('#pixel-board');
 
 function createSquares(numberOfLines) {
   for (let index = 0; index < numberOfLines; index += 1) {
-    let line = document.createElement('tr');
+    const line = document.createElement('tr');
     line.id = `line ${index}`;
     line.classList.add('line');
     table.appendChild(line);
-    for (let index = 0; index < numberOfLines; index += 1) {
-      let column = document.createElement('td');
-      column.id = `column ${index}`;
+    for (let index2 = 0; index2 < numberOfLines; index2 += 1) {
+      const column = document.createElement('td');
+      column.id = `column ${index2}`;
       column.classList.add('pixel');
       line.appendChild(column);
     }
