af1e9e93d20c1ea61e351b16a334ed1a8aa69695
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-05 20:37:06 -0300

    apenas remove o js que estava tentando compreender uma funcao e esqueci de apagar

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function createPixelBaordElement() {
-    const pixelBoard = document.getElementById('pixel-board');
-    for (let index = 0; index < 25; index += 1) {
-      const createPixel = document.createElement('div');
-      createPixel.className = 'pixel';
-      pixelBoard.appendChild(createPixel);
-    }
-  }
 
-  createPixelBaordElement();
