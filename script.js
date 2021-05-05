3063f6c24bd0927baf028a3a1dbf6745ec1283d9
Teika <shimworks.tk@gmail.com>
2021-05-05 17:50:41 -0300

    refatora requisito 04 e 05

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-
 function criaQuadradosDiv() {
-  let pixel = document.createElement('div');
-  pixel.className = 'pixel'
-  return pixel
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';
+  return pixel;
 }
 function criaLinhasDiv() {
-  let pixelLine = document.createElement('div');
-  pixelLine.className = 'pixel-line'
-  return pixelLine
+  const pixelLine = document.createElement('div');
+  pixelLine.className = 'pixel-line';
+  return pixelLine;
 }
 function criaLinhas(size) {
-  let pixelLineParent = document.getElementById('pixel-board');
+  const pixelLineParent = document.getElementById('pixel-board');
   for (let index = 0; index < size; index += 1) {
-    pixelLineParent.appendChild(criaLinhasDiv())
+    pixelLineParent.appendChild(criaLinhasDiv());
   }
 }
-
+function repetidor(pixelLine) {
+  for (let index2 = 0; index2 < pixelLine.length; index2 += 1) {
+    pixelLine[index2].appendChild(criaQuadradosDiv());
+  }
+}
 function criaQuadrados(size) {
-  let pixelLine = document.getElementsByClassName('pixel-line');
+  const pixelLine = document.getElementsByClassName('pixel-line');
   for (let index = 0; index < pixelLine.length; index += 1) {
-    if (index < size) {
-      for (let index2 = 0; index2 < pixelLine.length; index2 += 1) 
-      pixelLine[index2].appendChild(criaQuadradosDiv())
+  if (index < size) {
+    repetidor(pixelLine)
     }
   }
 }
-
 function createSquare(tamanho) {
   criaLinhas(tamanho)
   criaQuadrados(tamanho)
