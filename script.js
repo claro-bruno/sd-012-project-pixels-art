d0dfa3484d4bf98cba4a2b8546a609b6443292a8
Emerson Filho <emerson.dev.machado@gmail.com>
2021-05-05 16:47:36 -0400

    eslint

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-
 const gerarPaletaDeCorAleatoria = () => {
-    const caixaDeCorDaPaleta = document.querySelectorAll('.color');
+  const caixaDeCorDaPaleta = document.querySelectorAll('.color');
 
-    caixaDeCorDaPaleta[0].style.backgroundColor = 'black';
-    
-    for (let index = 1; index < caixaDeCorDaPaleta.length; index += 1) {
-        let randomColor = [];
-        for (let index = 0; index <= 2; index += 1) {
-            randomColor.push(Math.round(Math.random()*255));
-        }
-        caixaDeCorDaPaleta[index].style.backgroundColor = `rgb(${randomColor})`;
+  caixaDeCorDaPaleta[0].style.backgroundColor = 'black';
+
+  for (let index = 1; index < caixaDeCorDaPaleta.length; index += 1) {
+    let randomColor = [];
+    for (let index = 0; index <= 2; index += 1) {
+      randomColor.push(Math.round(Math.random() * 255));
     }
+    caixaDeCorDaPaleta[index].style.backgroundColor = `rgb(${randomColor})`;
+  }
 }
 
 const gerarTelaDePintura = (tamanho) => {
-    const areaDaTelaDePintura = document.querySelector('#telaDePintura')
-    
-    for (let altura = 1; altura <= tamanho; altura += 1) {
-        let pixel = document.createElement('div')
-        pixel.className = 'alturaDaTelaDePintura'
-        areaDaTelaDePintura.appendChild(pixel)
-    }
+  const areaDaTelaDePintura = document.querySelector('#telaDePintura');
 
-    const linha = document.querySelectorAll('.alturaDaTelaDePintura')
-    
-    for (let index = 0; index <= linha.length; index += 1) {
-        for (let largura = 1; largura <= tamanho; largura += 1) {
-            let pixel = document.createElement('div')
-            pixel.className = 'pixel'
-            linha[index].appendChild(pixel)
-        }
+  for (let altura = 1; altura <= tamanho; altura += 1) {
+    let pixel = document.createElement('div');
+    pixel.className = 'alturaDaTelaDePintura';
+    areaDaTelaDePintura.appendChild(pixel);
+  }
+
+  const linha = document.querySelectorAll('.alturaDaTelaDePintura');
+
+  for (let index = 0; index <= linha.length; index += 1) {
+    for (let largura = 1; largura <= tamanho; largura += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      linha[index].appendChild(pixel);
     }
+  }
 }
 
 const mudarTamanhoTelaDePintura = () => {
-    const inputDoTamanhoDaTela = document.querySelector('#tamanhoTelaDePintura').value   
+  const inputDoTamanhoDaTela = document.querySelector('#tamanhoTelaDePintura').value;
 }
