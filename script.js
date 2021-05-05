d10d69dbee0d6f5af79bb75ea514bf2ca37e64f0
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-05 17:31:38 -0300

    Insere resposta do requisito 6

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-undef */
 function colorPallete() {
   const colors = ['black', 'blue', 'red', 'green'];
-  let colorsPalette = document.querySelectorAll('.color');
+  const colorsPalette = document.querySelectorAll('.color');
   for (let index = 0; index < colorsPalette.length; index += 1) {
     colorsPalette[index].style.backgroundColor = colors[index];
   }
 }
 colorPallete();
+
+function getInicialColor() {
+  const inicialColor = document.querySelector('.selected');
+  inicialColor.style.backgroundColor = 'black';
+}
+getInicialColor();
