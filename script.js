ed765a24e898dc6cc55d9dcaf9a01bc14a3b7254
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-05 16:16:24 -0300

    Requisito 9 e todos os requisitos obrigatórios funcionando

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
 window.onload = function () {
-    let colorsList = [
-        'black',
-        'blue',
-        'yellow',
-        'pink'
-      ];
-    let pixelsColor = document.querySelectorAll('.color');
+  let colorsList = [
+    'black',
+    'blue',
+    'yellow',
+    'pink'
+  ];
+  let pixelsColor = document.querySelectorAll('.color');
+  let listOfPixels = document.querySelectorAll('.pixel');
 
-    setInitialColors();
-    changeSelectedColor();
-    changePixelsColor();
+  setInitialColors();
+  changeSelectedColor();
+  changePixelsColor();
+  clearBoard();
 
-    function setInitialColors() {    
-        for (index = 0; index < pixelsColor.length; index += 1) {
-            pixelsColor[index].style.backgroundColor = colorsList[index];
-            if(colorsList[index] === 'black'){
-                pixelsColor[index].className += ' selected';
-            };
-        };
+  function setInitialColors() {
+    for (index = 0; index < pixelsColor.length; index += 1) {
+      pixelsColor[index].style.backgroundColor = colorsList[index];
+      if (colorsList[index] === 'black') {
+        pixelsColor[index].className += ' selected';
       };
-    
-      function changeSelectedColor(){
-        for (index = 0; index < pixelsColor.length; index += 1) {
-            let currentDiv = pixelsColor[index];
-            currentDiv.addEventListener('click', nowSelected);
-        };
-      }
+    };
+  };
 
-      function nowSelected(e){
-        for (index = 0; index < pixelsColor.length; index += 1) {
-           let currentDiv = pixelsColor[index];
-           currentDiv.className = 'color';
-        };
+  function changeSelectedColor() {
+    for (index = 0; index < pixelsColor.length; index += 1) {
+      let currentDiv = pixelsColor[index];
+      currentDiv.addEventListener('click', nowSelected);
+    };
+  }
 
-          e.target.className = 'color selected';
+  function nowSelected(e) {
+    for (index = 0; index < pixelsColor.length; index += 1) {
+      let currentDiv = pixelsColor[index];
+      currentDiv.className = 'color';
+    };
+
+    e.target.className = 'color selected';
+  };
+
+  function changePixelsColor() {
+    for (let index = 0; index < listOfPixels.length; index += 1) {
+      let currentPixel = listOfPixels[index];
+
+      currentPixel.addEventListener('click', function (e) {
+        e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+      });
+    };
+  };
+
+  function clearBoard() {
+    let btn = document.querySelector('#clear-board');
+
+    btn.addEventListener('click', function () {
+      for (let index = 0; index < listOfPixels.length; index += 1) {
+        let currentPixel = listOfPixels[index];
+        currentPixel.style.backgroundColor = 'white';
       };
-    
-      function changePixelsColor(){
-        let listOfPixels = document.querySelectorAll('.pixel');
-
-        for(let index = 0; index<listOfPixels.length; index +=1){
-            let currentPixel = listOfPixels[index];
-
-            currentPixel.addEventListener('click', function(e){
-                e.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-            });
-        };
-      };
-
+    });
+  };
+  
 };
-
