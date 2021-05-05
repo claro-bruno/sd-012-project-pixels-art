350d36236f05712bd1c176419fcba00a2c18fc8f
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-05 16:07:36 -0300

    Resolve requisito 3

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Cria paleta de cores dinamicamente:
+let numberOfColors = 4;
 function createPalleteColors(number) {
-    let numberOfColors = number;
-    for (let index = 0; index < numberOfColors; index += 1) {
+    for (let index = 0; index < number; index += 1) {
         let pallete = document.getElementById("color-palette");
         let elementPallete = document.createElement('div');
         elementPallete.className = 'color';
         pallete.appendChild(elementPallete);
     }
 }
-createPalleteColors(4);
+createPalleteColors(numberOfColors);
 // Adiciona Cores dinamicamente a paleta de cores:
-let palleteColors = ['lime', 'red', 'aqua', 'deeppink']; // Array com cores para serem adicionadas.
+let palleteColors = ['black', 'lime', 'aqua', 'deeppink']; // Array com cores para serem adicionadas.
 function addColors(arrColors) {
     let colorPallete = document.getElementsByClassName('color');
     for (let index = 0; index < colorPallete.length; index += 1) {
