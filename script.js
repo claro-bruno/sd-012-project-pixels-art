17630b96e97dbd74dac72d53a9e38dc9b030aed6
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-05 18:58:41 -0300

    corrige do Desafio_4, na quantidade de li's.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const quadroPixel = document.querySelector('#pixel-board');
 
 for (let indexUl = 1; indexUl <= 5; indexUl += 1) {
-  const ulPixelQuadro = document.createElement('ul');
-  ulPixelQuadro.className = 'ulPixel';
-  quadroPixel.appendChild(ulPixelQuadro);
+  const divQuadro = document.createElement('div');
+  divQuadro.className = 'ulDivPixel';
+  quadroPixel.appendChild(divQuadro);
   for (let index = 1; index <= 5; index += 1) {
     const liPixelQuadro = document.createElement('li');
     liPixelQuadro.className = 'pixel';
-    ulPixelQuadro.appendChild(liPixelQuadro);
+    divQuadro.appendChild(liPixelQuadro);
   }
 }
-
