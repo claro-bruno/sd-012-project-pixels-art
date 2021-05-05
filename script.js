21c1c6d804fccfdc455bc5eda70a88c844e94cf2
matheus <matheus111904@hotmail.com>
2021-05-05 15:51:45 -0300

    teste desafio 2 cores

--- script.js
+++
@@ -1,8 +0,0 @@
-let corPaleta = document.getElementsByClassName('color');
-function verificaCor (){
-    if (corPaleta === "white"){
-        return "Cor inválida!"
-    } else if (corPaleta === corPaleta){
-        return "Não pode cor repetida!"
-}
-verificaCor ()
