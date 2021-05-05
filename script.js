42ee96227de47ff2807c63581d07d677f08ad960
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-05 19:32:29 -0300

    feito o botão de limpar o grid

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ window.onload = function () {
     let colorSelection = document.getElementById('color-palette');
     let selectedColor = undefined;
     let boardPixels = document.querySelector('#pixel-board');
+    let boardClear = document.querySelector("#clear-board");
 
 
     createBoard();
 
     colorSelection.addEventListener('click', function () {
-        selectedColor = event.target.id;
-        let colorName = document.querySelector(".selected-color");
+        if (event.target.className == "color") {
+            selectedColor = event.target.id;
+            let colorName = document.querySelector(".selected-color");
 
-        colorName.innerHTML = selectedColor.toUpperCase();
+            colorName.innerHTML = selectedColor.toUpperCase();
+        }
+
     });
 
     boardPixels.addEventListener("click", function () {
-        event.target.id = selectedColor;
+        if (event.target.className == "color") {
+            event.target.id = selectedColor;
+        }
+    })
+
+    boardClear.addEventListener("click", function () {
+        let square = boardPixels.querySelectorAll(".color");
+        square.forEach(element => {
+            element.id = "erased";
+        })
     })
 
     function createBoard() {
         for (let pixel = 0; pixel < 25; pixel++) {
             let square = document.createElement('div');
             square.className = "color";
-            square.id = "erased";
             document.querySelector('#pixel-board').appendChild(square);
             boardPixels.style.width = 210 + "px";
             boardPixels.style.height = 210 + 'px';
