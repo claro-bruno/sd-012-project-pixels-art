a8c293818c448e2d5e687ee05a2ce8123d30468e
Jossany Moura <mjossany@gmail.com>
2021-05-05 17:12:29 -0300

    Resolve requisito 4 e 5

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function coresPaleta(array) {
 }
 const paletaCores = ['blue', 'green', 'yellow'];
 coresPaleta(paletaCores);
+
+let numberOfLines = 5;
+function createLines (numberOfLines) {
+  let linesContainer = document.querySelector('#pixel-board');
+  for (let index = 0; index < numberOfLines; index += 1) {
+    let line = document.createElement('div');
+    line.classList.add('line');
+    linesContainer.appendChild(line);
+  }
+}
+createLines(numberOfLines);
+
+function createBox(className) {
+  let box = document.createElement('div');
+  box.classList.add(className);
+  box.style.backgroundColor = 'white';
+  return box;
+}
+
+function fillLines() {
+  let boxContainer = document.getElementsByClassName('line');
+  for (let index = 0; index < boxContainer.length; index += 1) {
+    for (let index2 = 0; index2 < numberOfLines; index2 += 1) {
+      let box = createBox('pixel')
+      boxContainer[index].appendChild(box);
+    }
+  }
+}
+fillLines();
+
+
