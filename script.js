2935b93f170d5c3481bbc4558d80b9629031aced
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-05 19:19:44 -0300

    Conserta erros do Linter :)

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-window.onload = function () {
+window.onload = function blackOnStart() {
   document.getElementById('black').classList.add('selected');
 };
 
 function selectAColor(event) {
-  let color = document.querySelectorAll('.color');
+  const color = document.querySelectorAll('.color');
   for (let index = 0; index < color.length; index += 1) {
     color[index].classList.remove('selected');
   }
   event.target.classList.add('selected');
 }
-let pickAColor = document.querySelector('#color-palette');
+const pickAColor = document.querySelector('#color-palette');
 pickAColor.addEventListener('click', selectAColor);
 
-let boxLocation = document.querySelector('#pixel-board');
+const boxLocation = document.querySelector('#pixel-board');
 function boxPaint(event) {
-  let selectedCheck = document.querySelector('.selected');
-  let pickedColor = getComputedStyle(selectedCheck).backgroundColor;
-  let paint = event.target;
+  const selectedCheck = document.querySelector('.selected');
+  const pickedColor = getComputedStyle(selectedCheck).backgroundColor;
+  const paint = event.target;
   paint.style.backgroundColor = pickedColor;
 }
 boxLocation.addEventListener('click', boxPaint);
 
-let clearButton = document.getElementById('clear-board');
-function clearTable(event) {
-  let boxesToClear = document.querySelectorAll('.pixel');
+const clearButton = document.getElementById('clear-board');
+function clearTable() {
+  const boxesToClear = document.querySelectorAll('.pixel');
   for (let index = 0; index < boxesToClear.length; index += 1) {
     if (boxesToClear[index].style.backgroundColor !== 'white') {
       boxesToClear[index].style.backgroundColor = 'white';
