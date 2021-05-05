dd9dd4ff0ab00867e0259a84638dc51efc19d9a3
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-05 16:45:41 -0300

    Resolve requisito 7

--- script.js
+++ script.js
@@ -5,4 +5,37 @@ window.onload = function () {
     const color4 = document.querySelectorAll('.color')[3];
 
     color1.classList.add('selected');
+
+    function selectColor1() {
+        color1.className = 'color selected';
+        color2.className = 'color';
+        color3.className = 'color';
+        color4.className = 'color';
+    }
+
+    function selectColor2() {
+        color1.className = 'color';
+        color2.className = 'color selected';
+        color3.className = 'color';
+        color4.className = 'color';
+    }
+
+    function selectColor3() {
+        color1.className = 'color';
+        color2.className = 'color';
+        color3.className = 'color selected';
+        color4.className = 'color';
+    }
+
+    function selectColor4() {
+        color1.className = 'color';
+        color2.className = 'color';
+        color3.className = 'color';
+        color4.className = 'color selected';
+    }
+
+    color1.addEventListener('click', selectColor1);
+    color2.addEventListener('click', selectColor2);
+    color3.addEventListener('click', selectColor3);
+    color4.addEventListener('click', selectColor4);
 }
