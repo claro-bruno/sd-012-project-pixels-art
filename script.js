e1503aa0d2715acc2f1a0732be35adb797b774cb
Abner <gedeaoabner@gmail.com>
2021-05-05 23:09:30 -0300

    adiciona funções para criar o botão 'clear-board' e adiciona sua funcionalidade - desafio 9

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let cor1 = "rgb(0, 0, 0, 1)";
 let cor2 = "rgb(255, 0, 0, 1)";
 let cor3 = "rgb(0, 100, 255, 1)";
 let cor4 = "rgb(0, 255, 0, 1)";
+let corBranco = "rgb(255, 255, 255, 1)";
 
 let cor = [cor1, cor2, cor3, cor4]
 
@@ -49,6 +50,7 @@ function colorePixel() {
   });
 }
 
+// cria dinamicamente linhas e colunas e atribui os pixels dentro
 function criaPixel() {
   let linha = 5;
   let coluna = 5;
@@ -68,9 +70,10 @@ function criaPixel() {
       idPixel += 1;
     }
   }
-  tamanhoPixel(40);
+  tamanhoPixel(40); // chama a configuração de altura/largura dos pixels
 }
 
+// função que configura o tamanho de todos os pixels. Sua chamada é dentro da função que cria os pixels.
 function tamanhoPixel(tamanho) {
   let tamanhoBorda = 1;
   let todosPixels = document.getElementsByClassName("pixel");
@@ -83,8 +86,30 @@ function tamanhoPixel(tamanho) {
     todasLinhas[index].style.height = tamanho + (tamanhoBorda * 2) + "px";
   }
 }
+// função que CRIA o botão para apagar tudo (volta o background de todos os pixels pra branco)
+function geraBotaoBranco() {
+  let lugar = document.getElementById("color-palette");
+  let botao = document.createElement("button");
+  botao.id = "clear-board";
+  botao.innerText = "Limpar";
+  lugar.appendChild(botao);
+}
+
+// Ao clicar no botão, todos os elementos com a classe "pixel" voltam a ter Branco como sua cor de fundo
+function btnBranco() {
+  document.addEventListener("click", function(botao) {
+    if (event.target.id == "btnBranco") {
+      let pixel = document.getElementsByClassName("pixel");
+      for (let index = 0; index < pixel.length; index += 1) {
+        pixel[index].style.backgroundColor = corBranco;
+      }
+    }
+  });
+}
 
 coresPaleta();
 clickCor();
 criaPixel();
-colorePixel();   
+colorePixel();
+geraBotaoBranco();
+btnBranco();
