83b7d7668582400cbc8b0d9170ea17f145bbc409
Gabriel Viana <g.viana87@hotmail.com>
2021-05-06 00:08:56 -0300

    Add pixel board

---
+++ script.js
@@ -0,0 +1,16 @@
+let pixelBoard = document.getElementById('pixel-board');
+
+for (let linha = 0; linha < 25; linha += 1){
+    let board = document.createElement('div');
+    board.className = 'pixel';
+    pixelBoard.appendChild(board)
+}
+
+    
+    
+
+
+
+
+
+
