4436e9044aaa5e445d82edb26254a8d58210d65b
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-06 14:56:42 -0300

    requisito 04

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let colorPalette = document.getElementById('color-palette');
+function paletaDeCores() {
+    let colorPalette = document.getElementById('color-palette');
+    let paleta;
 
-let paleta;
+    let cores = ['black', 'blue', 'red', 'green']
 
-let cores = ['black', 'blue', 'red', 'green']
-
-for (let index = 0; index < cores.length; index += 1) {
-    paleta = document.createElement('div');
-    paleta.className= 'color';
-    paleta.style.backgroundColor = cores[index];
-    colorPalette.appendChild(paleta);
+    for (let index = 0; index < cores.length; index += 1) {
+        paleta = document.createElement('div');
+        paleta.className= 'color';
+        paleta.style.backgroundColor = cores[index];
+        colorPalette.appendChild(paleta);
+    }
 }
+paletaDeCores();
+
+
+function quadroDePixels() {
+    let pixelBoard = document.getElementById('pixel-board');
+   
+
+    for (let index = 0; index < 5; index += 1) {
+       let linha = document.createElement('div');
+       linha.className = 'line';
+       document.querySelector('#pixel-board').appendChild(linha); 
+        
+    }
+
+    for (let index = 0; index < 5; index += 1) {
+        for (let index = 0; index < 5; index += 1) {
+            let pixel = document.createElement('div');
+            pixel.className = 'pixel';
+            document.querySelectorAll('.line')[index].appendChild(pixel)
+        }
+    }
+}
+quadroDePixels(); 
