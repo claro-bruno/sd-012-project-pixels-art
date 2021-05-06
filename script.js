e93b32a6f81e3e0d80c24d88dddfbabc2e16d1df
martinsph <ph.mmartins@gmail.com>
2021-05-06 18:31:50 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function givePaletteColor(){
-    let colors = ["blue","green","red", "black"];
-    let paletteC = document.querySelectorAll('.color');
-    for (let index = 0; index < paletteC.length; index +=1){
-        for (let index2 = 0; index2 < colors.length; index2 +=1){
-            paletteC[index].style.background = colors[index];
+// function givePaletteColor(){
+//     let colors = ["blue","green","red", "black"];
+//     let paletteC = document.querySelectorAll('.color');
+//     for (let index = 0; index < paletteC.length; index +=1){
+//         for (let index2 = 0; index2 < colors.length; index2 +=1){
+//             paletteC[index].style.background = colors[index];
+//         }
+//     };
+// };
+let areaDiv2 = document.getElementById("div2")
+
+
+
+
+
+
+function createBoard(row, column) {
+    let createTable = document.createElement('table');
+    let tbody = document.createElement('tbody');
+    areaDiv2.appendChild(createTable)
+
+    for (let index = 0; index < row; index +=1){
+        let createrow = document.createElement("tr");
+        for (let index = 0; index < row; index +=1){
+            let cell = document.createElement("td");
+            cell.className = "pixel";
+            createrow.appendChild(cell);
         }
+        tbody.appendChild(createrow);
     };
-};
+    createTable.appendChild(tbody);
+}
+createBoard(5, 5);
