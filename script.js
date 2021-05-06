cbc7e6323a5d90ed88a414fe4ce14be5fbeebf26
thales daniel <thalesdaniel2013@gmail.com>
2021-05-06 19:48:37 -0300

    feito a etapa 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 for (index = 0; index < 25; index += 1) {
     let criacao = document.createElement("div")
     criacao.className = 'pixel'
+    criacao.addEventListener('click', adicionaCor)
     const div = document.querySelector('#pixel-board').appendChild(criacao)
 }
 
@@ -11,7 +12,7 @@ window.onload = function inicializacao() {
 }
 
 let cor = document.querySelectorAll('.color')
-console.log(cor)
+
 for (let index = 0; index < cor.length; index += 1) {
     cor[index].addEventListener('click', removeClass)
 }
@@ -20,4 +21,13 @@ function removeClass(event) {
     let remover = document.querySelector('.selected')
     remover.classList.remove('selected')
     event.target.classList.add('selected')
+
+}
+
+function adicionaCor(event) {
+
+    let colorido = document.querySelector('.selected')
+    let corSeleciona = window.getComputedStyle(colorido).getPropertyValue('background-color')
+    event.target.style.backgroundColor = corSeleciona
+
 }
