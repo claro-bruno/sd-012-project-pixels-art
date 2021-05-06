0ddf4d126544c29fd606c52c84844145681e9f18
thales daniel <thalesdaniel2013@gmail.com>
2021-05-06 09:33:31 -0300

    feito etapa 6

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ for (index = 0; index < 25; index += 1) {
     let classe = criacao.className = 'pixel'
     const div = document.querySelector('#pixel-board').appendChild(criacao)
 }
+
+window.onload = function inicializacao() {
+    let preto = document.querySelector('#preto')
+    preto.classList.add('selected')
+
+
+}
