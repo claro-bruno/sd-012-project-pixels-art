e2b0f81f7a204fe8b52a948d990ea49787a248f1
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-06 00:55:41 -0400

    Abstraçao do desafio 7 - selecionar cor no click 2.0

--- script.js
+++ script.js
@@ -33,11 +33,17 @@ firstColor.classList.add('selected');
 selectFirstColor();
 
 // Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+const colorPalette = document.querySelector('#color-palette');
 // adicionar uma escuta na div color-palette
-// identificar com o target de onde veio o click
-// capturar a div da paleta em forma de array
-// verificar item por item se tem a classe selected
-// se tiver a classe selected remover
-// atribuir a classe selected a cor que cliquei
-
-
+colorPalette.addEventListener('click', (event) => {
+  // criar condição de click, só pode receber a classe selected se já tiver a classe color
+  if (event.target.className === 'color') {
+    // buscar elemento pela classe selected
+    const selectedColor = document.querySelector('.selected')
+    // remover a classe do elemento
+    selectedColor.classList.remove('selected')
+    // identificar com o target de onde veio o click
+    // atribuir a classe selected a cor que cliquei
+    event.target.classList.add('selected')
+  }
+})
