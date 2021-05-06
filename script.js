35570d3277c3674b661f741b36e53cda9e98623f
rachidb5 <jordan.rachid@gmail.com>
2021-05-06 17:46:06 -0300

    adiciona funçao apara gerar cores aleatorias

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ const preto = document.getElementById('preta')
 const azulClaro = document.getElementById('azul-claro')
 const azulEscuro = document.getElementById('azul-escuro')
 const claro = document.getElementById('clara')
+
+function gerarCor() {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+
+  return `rgb(${r},${g},${b})`
+}
+
+const zero = gerarCor();
+const one = gerarCor();
+const two = gerarCor();
+const three = gerarCor();
+
+function colors() {
+  for (let i = 0; i < 4; i += 1) {
+    const div = document.createElement('div');
+    div.className = 'color';
+    colorPalette.appendChild(div);
+  }
+}
+
+colors()
+
+document.getElementsByClassName('color')[0].style.backgroundColor = zero
+document.getElementsByClassName('color')[1].style.backgroundColor = one
+document.getElementsByClassName('color')[2].style.backgroundColor = two
+document.getElementsByClassName('color')[3].style.backgroundColor = three
+
 function board() {
   for (let i = 0; i < 25; i += 1) {
     const div = document.createElement('div');
