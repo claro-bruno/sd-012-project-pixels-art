d394a5cca71af5e2caca428aa1fb36ba0a026421
rachidb5 <jordan.rachid@gmail.com>
2021-05-06 16:08:13 -0300

    gerar cores aleatorias

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
-const pixel = document.getElementById('pixel-board');
-const preto = document.getElementById('preta')
-const azulClaro = document.getElementById('azul-claro')
-const azulEscuro = document.getElementById('azul-escuro')
-const claro = document.getElementById('clara')
+const pixelBoard = document.getElementById('pixel-board');
+const preto = document.getElementById('preta');
+const azulClaro = document.getElementById('azul-claro');
+const azulEscuro = document.getElementById('azul-escuro');
+const claro = document.getElementById('clara');
+const selectedElement = document.querySelector('.selected');
+const colorPalette = document.getElementById('color-palette')
+
+/* cores da paleta inicio */
+
+function gerarCor() {
+  let r = Math.random()*255;
+  let g = Math.random()*255;
+  let b = Math.random()*255;
+
+  return `rgb(${r},${g},${b})`
+}
+
+let zero= gerarCor();
+let one = gerarCor();
+let two = gerarCor();
+let three = gerarCor();
+
+function colors() {
+  for (let i = 0; i < 4; i += 1) {
+    const div = document.createElement('div');
+    div.className = 'color';
+    colorPalette.appendChild(div);
+  }
+}
+
+colors()
+
+document.getElementsByClassName('color')[0].style.backgroundColor = zero
+document.getElementsByClassName('color')[1].style.backgroundColor = one
+document.getElementsByClassName('color')[2].style.backgroundColor = two
+document.getElementsByClassName('color')[3].style.backgroundColor = three
+
+/*cores da paleta fim */
 
 function board() {
   for (let i = 0; i < 25; i += 1) {
     const div = document.createElement('div');
     div.className = 'pixel';
-    pixel.appendChild(div);
+    pixelBoard.appendChild(div);
   }
 }
 
 board();
 
 function select(event) {
-  const selectedElement = document.querySelector('.selected');
   selectedElement.classList.remove('selected');
   event.target.classList.add('selected');
 }
@@ -24,3 +57,15 @@ preto.addEventListener('click', select);
 azulClaro.addEventListener('click', select);
 azulEscuro.addEventListener('click', select);
 claro.addEventListener('click', select);
+
+const color = document.getElementsByClassName('selected')[0].style.backgroundColor
+
+console.log(color)
+  
+function paint(event) {
+  event.target.style.backgroundColor = 'color'
+}
+
+const pixel = document.querySelector('.pixel')
+
+pixel.addEventListener('click', paint)
