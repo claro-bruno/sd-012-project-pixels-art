cba15e540a1da3e49d3b5943fffb332ef6cef9bc
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-05 21:33:58 -0300

    modificando requisito 6

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function () {
-  document.querySelector('.black').className.toggle('selected');
-};
+document.querySelector('.black').className.toggle('selected');
 
 // propriedade .toggle para alternar classe https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
