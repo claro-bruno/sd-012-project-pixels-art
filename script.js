99b5cbcfbb605ce2d0c59431391a08edd13abb60
cristianocsl <cslcristiano@gmail.com>
2021-05-06 11:18:00 -0300

    Incício desafio 4

--- script.js
+++ script.js
@@ -1,49 +1,22 @@
-// let getIdPalette = document.querySelector('#color-palette'); //retorna o elemento com o id color-palette
+function PaletteColor () { // não entendi exatamente que será o parâmetro dessa função e se é necessário ter um;
 let divPai = document.querySelector('#color-palette');
 let colorPalette = ['preto', 'vermelho', 'azul', 'amarelo']; //cores de referência para o laço for
-
 for (let index = 0; index < colorPalette.length; index += 1) {
-  let liFilha = document.createElement('li');
-  liFilha.className = 'color';
+  let divFilha = document.createElement('div');
+  divFilha.className = 'color';
   if (colorPalette[index] === 'preto') {
-    liFilha.style.backgroundColor = 'black';
+    divFilha.style.backgroundColor = 'black';
   }
   if (colorPalette[index] === 'vermelho') {
-    liFilha.style.backgroundColor = 'red';
+    divFilha.style.backgroundColor = 'red';
   }
   if (colorPalette[index] === 'azul') {
-    liFilha.style.backgroundColor = 'blue';
+    divFilha.style.backgroundColor = 'blue';
   }
   if (colorPalette[index] === 'amarelo') {
-    liFilha.style.backgroundColor = 'yellow';
+    divFilha.style.backgroundColor = 'yellow';
   }
-  divPai.appendChild(liFilha);
+  divPai.appendChild(divFilha);
 }
-
-// requisito 2
-
-
-// function listPalette (array) {
-//   let colorPalette = ['preto', 'vermelho', 'azul', 'amarelo']; //cores de referência para o laço for
-//   let ulPai = document.createElement('ul'); //cria elemento pai 'ul'
-
-//   for (let key = 0; key < colorPalette.length; key += 1) { //percorra os valores
-//     let liMake = document.createElement('li'); //cria elemento 'li' para esta variável
-//     liMake.className = 'color'; //atribui uma classe 'color' ao elemento 'li'
-//     let getLi = document.getElementsByTagName('li'); //chama(pega) o elemento criado
-//     if (colorPalette[key] === 'preto') {
-//         getLi.style.backgroundColor = 'black';
-//     } else if (colorPalette[key] === 'vermelho') {
-//         getLi.style.backgroundColor = 'red';
-//     } else if (colorPalette[key] === 'azul') {
-//         getLi.style.backgroundColor = 'blue';
-//     } else if (colorPalette[key] === 'amarelo') {
-//         getLi.style.backgroundColor = 'yellow';
-//     }
-//     ulPai.appendChild(liMake); //li filha de ul
-//   }
-//   getIdPalette.appendChild(ulPai);
-// }
-// console.log(listPalette);
-
-
+}
+console.log(PaletteColor()); // não entendi exatamente que será o parâmetro dessa função e se é necessário ter um;
