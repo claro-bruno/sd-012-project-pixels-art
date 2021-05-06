23a17e486fc90f1cb695448d778cb714b4b4a0af
Victor <juanvictor70@gmail.com>
2021-05-06 18:12:33 -0400

    Removendo comentarios e editando nomes de algumas variaveis

--- script.js
+++ script.js
@@ -1,58 +1,35 @@
-window.onload = function pixelsArt() {
-  finalBoarderElements();
-  createPaletteElement('color1', 'black');
-  createPaletteElement('color2', generateRandomColor());
-  createPaletteElement('color3', generateRandomColor());
-  createPaletteElement('color4', generateRandomColor());
-  initialColor('color1');
-  changeSelectedClick();
-  changeColorClick();
-  clearBoardClick();
-};
-
-function generateRandomColor() {
+function randomColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return `#${randomColor}`;
 }
+
 function createPalette(id, color) {
   const colorPalette = document.getElementById('color-palette');
   const createDiv = document.createElement('div');
-  
   createDiv.className = 'color';
   createDiv.id = id;
   createDiv.style.backgroundColor = color;
   colorPalette.appendChild(createDiv);
 }
-function createPixelBoard(number) {
+
+function createPixelBoard(numero) {
   const pixelBoard = document.getElementById('pixel-board');
-  
   for (let index = 0; index < numero; index += 1) {
     const createPixel = document.createElement('div');
     createPixel.className = 'pixel';
     pixelBoard.appendChild(createPixel);
   }
 }
+
 function initialColor(id) {
-  const initialColor = document.getElementById(id);
-  initialColor.className += 'selected';
+  const corInicial = document.getElementById(id);
+  corInicial.className += ' selected';
 }
-function changeSelect(element) {
+
+function changeSelected(element) {
   const selected = document.querySelector('.selected');
   selected.className = 'color';
-  element.target.className += 'selected';
-}
-
-function changeColorClick() {
-  const pixels = document.querySelectorAll('.pixel');
-  for (const pixel of pixels) {
-    pixel.addEventListener('click', changeColor);
-  }
-}
-
-function changeColor(element) {
-  const selectedColor = document.querySelector('.selected');
-  const compStyle = window.getComputedStyle(selectedColor);
-  element.target.style.backgroundColor = compStyle.getPropertyValue('background-color');
+  element.target.className += ' selected';
 }
 
 function changeSelectedClick() {
@@ -67,10 +44,19 @@ function changeSelectedClick() {
   color4.addEventListener('click', changeSelected);
 }
 
-function clearBoardClick() {
-  const clearButton = document.getElementById('clear-board');
-  clearButton.addEventListener('click', clearBoard);
+function changeColor(element) {
+  const selectedColor = document.querySelector('.selected');
+  const compStyle = window.getComputedStyle(selectedColor);
+  element.target.style.backgroundColor = compStyle.getPropertyValue('background-color');
 }
+
+function changeColorClick() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (const pixel of pixels) {
+    pixel.addEventListener('click', changeColor);
+  }
+}
+
 function clearBoard() {
   const pixels = document.querySelectorAll('.pixel');
   for (const pixel of pixels) {
@@ -78,19 +64,44 @@ function clearBoard() {
   }
 }
 
-function sizeBoard() {
-  const pixelBoard = document.getElementById('pixel-board');
-  const userInput = document.getElementById('board-size');
-  const numberOfInput = 0;
+function clearBoardClick() {
+  const clearButton = document.getElementById('clear-board');
+  clearButton.addEventListener('click', clearBoard);
+}
 
-  if (parseInt(userInput.value) < 5) {
+function userBoarder() {
+  const inputNumber = document.getElementById('board-size');
+  let numberOfInput = 0;
+  if (parseInt(inputNumber.value) < 5) {
     numberOfInput = 5;
-  } else if (parseInt(userInput.value) > 50){
+  } else if (parseInt(inputNumber.value) > 50){
     numberOfInput = 50;
   } else {
-    numberOfInput = parseInt(userInput.value);
+    numberOfInput = parseInt(inputNumber.value);
   }
-  let numberOfPixels = {};
+  const numberOfPixels = {};
+  for (let index = 5; index <= 50; index += 1) {
+    numberOfPixels[index] = index * index;
+  }
+  const finalNumber = numberOfPixels[numberOfInput];
+  if (inputNumber.value.length < 1) {
+    return alert('Board inválido!'), createPixelBoard(25);
+  }
+  return finalNumber;
+}
+
+function sizeBoard() {
+  const pixelBoard = document.getElementById('pixel-board');
+  const inputNumber = document.getElementById('board-size');
+  let numberOfInput = 0;
+  if (parseInt(inputNumber.value) < 5) {
+    numberOfInput = 5;
+  } else if (parseInt(inputNumber.value) > 50){
+    numberOfInput = 50;
+  } else {
+    numberOfInput = parseInt(inputNumber.value);
+  }
+  const numberOfPixels = {};
   for (let index = 5; index <= 50; index += 1) {
     numberOfPixels[index] = index * index;
   }
@@ -98,23 +109,29 @@ function sizeBoard() {
   pixelBoard.style.height = `${(numberOfInput * 40) + (2 * numberOfInput)}px`;
 }
 
-function userBoarder() {
-  const userInput = document.getElementById('board-size');
-  let numberOfInput = 0;
-  if (parseInt(userInput.value) < 5) {
-    numberOfInput = 5;
-  } else if (parseInt(userInput.value) > 50){
-    numberOfInput = 50;
-  } else {
-    numberOfInput = parseInt(userInput.value);
-  }
-  const numberOfPixels = {};
-  for (let index = 5; index <= 50; index += 1) {
-    numberOfPixels[index] = index * index;
-  }
-  const finalNumber = numberOfPixels[numberOfInput];
-  if (userInput.value.length < 1) {
-    return alert('Board inválido!'), createPixelBaordElement(25);
-  }
-  return finalNumber;
+function finalBoarderElements() {
+  const pixelBoard = document.getElementById('pixel-board');
+  const vqvButton = document.getElementById('generate-board');
+  let defaultSize = createPixelBoard(25);
+
+  vqvButton.addEventListener('click', function() {
+    pixelBoard.innerHTML = '';
+    defaultSize = createPixelBoard(userBoarder());
+    sizeBoard();
+    changeSelectedClick();
+    changeColorClick();
+    clearBoardClick();
+  });
 }
+
+window.onload = function pixelsArt() {
+  finalBoarderElements();
+  createPalette('color1', 'black');
+  createPalette('color2', randomColor());
+  createPalette('color3', randomColor());
+  createPalette('color4', randomColor());
+  initialColor('color1');
+  changeSelectedClick();
+  changeColorClick();
+  clearBoardClick();
+};
