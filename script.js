7e1156c94ca781466e11cafad6c51ac47564b0b1
Sarah <sarahgsb@hotmail.com>
2021-05-06 02:29:01 -0300

    coloca hex no requisito 9

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let pixels = document.getElementsByClassName('pixel');
 let clear = document.getElementById('clear-board');
 clear.addEventListener('click', function () {
      for (let index = 0; index < pixels.length; index += 1){
-         pixels[index].style.backgroundColor = 'white';
-        }
+         pixels[index].style.backgroundColor = '#FFFFFF';
+        
+}
     });
     
 
+   
