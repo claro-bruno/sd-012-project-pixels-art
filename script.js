f531388bc99ce32953401da01f548e2bb5c898a0
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-05-05 21:21:40 -0300

    Corrige a quantidade de cores da paleta de 5 paara 4 cores

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 let corMaster = document.querySelector("#color-palette");
 let cores = ['black', 'red', 'blue', 'green', 'yellow'];
-for(let index = 0; index < 5; index +=1){
-    let cor = document.createElement('div');
+for(let index = 0; index < 4; index +=1){
+    let cor = document.createElement('div');//cria os elementos div para serem populados
     cor.className = 'color';//atribui o nome à classe do elemento cor
     cor.style.backgroundColor = cores[index];//Atribui stilo e cor de fundo ao elemento criado cor.
     corMaster.appendChild(cor);
