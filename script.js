7a7b18cc988000ed515e584ff82a112a22e7a6ed
cristianocsl <cslcristiano@gmail.com>
2021-05-06 18:15:24 -0300

    desafio 4 e 5 melhorias no eslint e stylelint

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function PaletteColor () { // não entendi exatamente que será o parâmetro dessa função e se é necessário ter um;
-let divPai = document.getElementById('color-palette');
-let colorPalette = ['black', 'red', 'blue', 'yellow']; //cores de referência para o laço for
-for (let index = 0; index < colorPalette.length; index += 1) {
-  let divFilha = document.createElement('div');
-  divFilha.className = 'color';
-  divFilha.style.backgroundColor =colorPalette[index];
-  divPai.appendChild(divFilha);
+  const divPai = document.getElementById('color-palette');
+  const colorPalette = ['black', 'red', 'blue', 'yellow']; //cores de referência para o laço for
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    const divFilha = document.createElement('div');
+    divFilha.className = 'color';
+    divFilha.style.backgroundColor =colorPalette[index];
+    divPai.appendChild(divFilha);
+  }
 }
-}
-console.log(PaletteColor()); // não entendi exatamente que será o parâmetro dessa função e se é necessário ter um;
-// esse console de cima ^ pode ficar aqui?
+// console.log(PaletteColor());
 function divFilha() { 
-  let pixelDiv = document.createElement('div'); //cria 01 (um) quadrado (pixel) usando createElement.
+  const pixelDiv = document.createElement('div'); //cria 01 (um) quadrado (pixel) usando createElement.
   pixelDiv.className = 'pixel'; // adiciona a classe 'pixel'.
   return pixelDiv; // retorna 01 pixel que é uma div.
 }
@@ -20,15 +19,15 @@ function linhaParaDivs() {
   linhaParaPixels.className = 'linha-do-pixel'; // cria classe para para linhaParaPixels.
   return linhaParaPixels; // retorna 01 linha que é uma div
 }
-function loosParaCriarLinhas(base) { //loop para crias as linhas antes de preencher; 
+function loosParaCriarLinhas(base) { //loop para crias as 5 linhas antes de preencher. Cada linha é uma div.
   const divPixel_board = document.getElementById('pixel-board'); //captura a div com Id=pixel-board, que será Pai dos elementos do loop
   for (let index = 0; index < base; index += 1) {
     divPixel_board.appendChild(linhaParaDivs()); //cada linhaParaDivs será filha de divPixel_board
   }
 }
 function repetir(linhaParaPixels) { //
-  for (let index_rep = 0; index_rep < linhaParaPixels.length; index_rep += 1) {
-    linhaParaPixels[index_rep].appendChild(divFilha());
+  for (let indexRepet = 0; indexRepet < linhaParaPixels.length; indexRepet += 1) {
+    linhaParaPixels[indexRepet].appendChild(divFilha());
   }
 }
 function quadrados(size) {
