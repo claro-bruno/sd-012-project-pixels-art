cdfe745b59f445c508acf9aabb2d5d7f624023fa
Eder Santos <3derms@gmail.com>
2021-05-06 14:54:20 -0300

    Resolve requisito 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let coresDosBlocos = document.querySelectorAll("div");
+coresDosBlocos[1].style.backgroundColor = "black";
+coresDosBlocos[2].style.backgroundColor = "red";
+coresDosBlocos[3].style.backgroundColor = "blue";
+coresDosBlocos[4].style.backgroundColor = "green";
