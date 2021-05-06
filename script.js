54bbcd9d59855567047d8999c4b2c7d1c51ec31d
Juliane Marques <mqs.juliane@gmail.com>
2021-05-06 15:03:26 -0300

    arruma css

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const pixelBoard = document.querySelector('#pixel-board');
 
 function creatingPixelBoardCollumns(row, colunas) {
-    for (let column = 1; column <= colunas; column += 1) {
-      const divColumn = document.createElement('div');
-      divColumn.classList.add('pixel');
-      divColumn.classList.add('td');
-      row.appendChild(divColumn);
-    }
+  for (let column = 1; column <= colunas; column += 1) {
+    const divColumn = document.createElement('div');
+    divColumn.classList.add('pixel');
+    divColumn.classList.add('td');
+    row.appendChild(divColumn);
   }
+}
 
-  function creatingPixelBoardRows(linhas) {
-    for (let row = 1; row <= linhas; row += 1) {
-      const divRow = document.createElement('div');
-      divRow.classList.add('tr');
-      pixelBoard.appendChild(divRow);
-      creatingPixelBoardCollumns(divRow, linhas);
-    }
+function creatingPixelBoardRows(linhas) {
+  for (let row = 1; row <= linhas; row += 1) {
+    const divRow = document.createElement('div');
+    divRow.classList.add('tr');
+    pixelBoard.appendChild(divRow);
+    creatingPixelBoardCollumns(divRow, linhas);
   }
+}
 
-  creatingPixelBoardRows(5);
+creatingPixelBoardRows(5);
 
 const paletteColors = document.querySelectorAll('.color');
 
