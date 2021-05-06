f2b36fc6f6c5769320bfdcec1d5293151bb4ad63
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-06 19:58:32 -0300

    requesito 3 resolvido, e começa a resolver requesito 4

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ for (indice = 0; indice < cores.length; indice += 1) {
     coresitem.innerHTML = cor;
     quadradocores.appendChild(coresitem);
 };
+window.onload = quadrados;
+function quadrados() {
+    let pixell = document.querySelector('#pixel-board');
+    let ospixels = [];
+    for (indice = 0; indice < 25; indice += 1) {
+        ospixels.push("")
+        let pixels = ospixels[indice];
+        let quadrado = document.createElement('div');
+        quadrado.className = 'pixel';
+        quadrado.style.backgroundColor = 'White';
+        quadrado.innerHTML = pixels;
+        pixell.appendChild(quadrado);
+    };
+};
