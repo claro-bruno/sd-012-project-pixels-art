764c9b73df4d3b3ff798e64c1ac90f20a4fdbbf5
Eder Santos <3derms@gmail.com>
2021-05-06 18:59:01 -0300

    inclusao de div pai no querySelector

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ coresDosBlocos[2].style.backgroundColor = 'red';
 coresDosBlocos[3].style.backgroundColor = 'blue';
 coresDosBlocos[4].style.backgroundColor = 'green';
 
-//function CorInicial() {
+function CorInicial() {
 // document.getElementsByClassName("selected");
-document.querySelector('.selected');
-//}
+document.querySelector(".color-palette .selected");
+}
 
 //window.onload = CorInicial;
 
