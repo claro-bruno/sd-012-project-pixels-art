011b6c3fc1fb1c2d45f0de1a36d40079a67c5eb3
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-06 15:16:22 -0400

    Desafio 8 refatorado - pinta board

--- script.js
+++ script.js
@@ -40,20 +40,16 @@ function selectColor() {
       colorSelected.classList.remove('selected');
       event.target.classList.add('selected');
     }
-  })
+  });
 }
 selectColor();
 
-// Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
-// colocar uma escuta na board completa (section)
-const board = document.querySelector('section')
-board.addEventListener('click', (event) => {
-  // capturar o elemento com a classe select
-  const colorSelected = document.querySelector('.selected')
-  // salvar o background colo do elemento em uma variável
-  colorSelected.style.backgroundColor
-  // Capturar o elemento via target
-  event.target.style.backgroundColor = colorSelected.style.backgroundColor;
-  // aplicar o background color capturado ao elemento clicado
-})
-
+function colorBoard() {
+  const board = document.querySelector('section');
+  board.addEventListener('click', (event) => {
+    const colorSelected = document.querySelector('.selected').style.backgroundColor;
+    const eventListen = event.target;
+    eventListen.style.backgroundColor = colorSelected;
+  });
+}
+colorBoard();
