7e201a871d43bcb7f804dcf14052695cdb5e89e9
Janderson Soares <jandinsm@gmail.com>
2021-05-06 19:07:24 -0300

    Desafio 4 javaScript

--- script.js
+++ script.js
@@ -4,18 +4,48 @@ const paiPaleta = document.getElementById("color-palette");
 
 function paletaArco(){
    for(let index = 0; index < arrayCores.length; index++){
-      const criaPaleta  = document.createElement("p");
+      const criaPaleta = document.createElement("p");
       criaPaleta.className = "color";
-
       criaPaleta.style.background = arrayCores[index];
       criaPaleta.style.borderStyle = "solid";
       criaPaleta.style.borderColor = "black";
       criaPaleta.style.borderWidth = "1px";
       criaPaleta.style.height = "40px";
       criaPaleta.style.width = "40px";
-      criaPaleta.style.display = "inline-block";
-
+      criaPaleta.style.display = "inline-block"
       paiPaleta.appendChild(criaPaleta);
    };
 };
 paletaArco();
+
+function quadroPixels(){
+   let paiBody = document.getElementById("bodi");
+   let quadro = document.createElement("section");
+   paiBody.appendChild(quadro);
+   quadro.id = "pixel-board";
+   let paiQuadro = document.getElementById("pixel-board");
+  
+   // paiQuadro É a section
+   const lupFive = 5; 
+   for(let index = 0; index < lupFive; index++){
+      let linha = document.createElement("ol");
+      paiQuadro.appendChild(linha);
+      linha.style.lineHeight = "0";
+      linha.style.margin = "0px"; 
+      for(let indexOl = 0; indexOl < lupFive; indexOl++){
+            let pixel = document.createElement("ul");
+            linha.appendChild(pixel);
+            pixel.className = "pixel";
+            pixel.style.borderStyle = "solid";
+            pixel.style.borderColor = "black";
+            pixel.style.borderWidth = "1px";
+            pixel.style.height = "40px";
+            pixel.style.width = "40px";
+            pixel.style.display = "inline-block";
+            pixel.style.backgroundColor = "white";
+            pixel.style.lineHeight = "0";
+            pixel.style.margin = "0px"; 
+      }
+   }
+}
+quadroPixels();
