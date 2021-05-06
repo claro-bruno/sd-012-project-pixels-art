7017fca496cb1c9cc7749414f7807d69a29c53b9
Eurides <euridesalves@yahoo.com.br>
2021-05-06 17:28:05 -0300

    correcao da classe requisito 7

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 window.onload = function () {
-  let corInicial = document.getElementById('color1');
-  corInicial.className = 'selected';
+  const corInicial = document.getElementById('color1');
+  corInicial.className = 'color selected';
   padronizaPalheta(corInicial);
   // ddEventListener(p1,p2) onde: p1 e' o evento e p2 a funcao a ser executada
-  let paletaPreta = document.getElementById('color1');
-  paletaPreta = addEventListener('click',clicouCor(paletaPreta));
-  let paletaVermelha = document.getElementById('color2');
-  paletaVermelha = addEventListener('click',clicouCor(paletaVermelha));
-  let paletaAzul = document.getElementById('color3');
-  paletaAzul = addEventListener('click',clicouCor(paletaAzul));
-  let paletaVerde = document.getElementById('color4');
-  paletaVerde = addEventListener('click',clicouCor(paletaVerde));
+  const paletaPreta = document.getElementById('color1');
+  paletaPreta.addEventListener('click', clicouCor(paletaPreta));
+  const paletaVermelha = document.getElementById('color2');
+  paletaVermelha.addEventListener('click', clicouCor(paletaVermelha));
+  const paletaAzul = document.getElementById('color3');
+  paletaAzul.addEventListener('click', clicouCor(paletaAzul));
+  const paletaVerde = document.getElementById('color4');
+  paletaVerde.addEventListener('click', clicouCor(paletaVerde));
 }; 
 
 function paletaPadrao() {
   console.log("Aqui");
 }
 
-function padronizaPalheta(palheta) {
-  palheta.style.width = 20 + 'px';
-  palheta.style.height = 20 + 'px;'
-  palheta.style.display = inline-block;
-  /* border-width: 1px;
-  border-style: solid;
-  border-color: black; */
-}
-
 function clicouCor(corEscolhida) {
+    console.log("a cor escolhida :"+corEscolhida);
   let CorAntiga = document.getElementsByClassName('selected');
   /* const idCorAntiga = */
 };
