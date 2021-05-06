55aee7843ccb217307aaf9b837eddf4680958478
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-05 21:26:13 -0300

    refeitos exs 1 e 2

--- script.js
+++
@@ -1,5 +0,0 @@
-let blockGreen = document.getElementsByClassName('color')[0];
-let blockRed = document.getElementsByClassName('color')[1];
-let blockYellow = document.getElementsByClassName('color')[2];
-let blockBlue = document.getElementsByClassName('color')[3];
-let blockBlack = document.getElementsByClassName('color')[4];
