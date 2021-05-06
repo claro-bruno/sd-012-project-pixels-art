e5854b3b234866ef4d81a859cd8b7477424910a2
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-05-06 16:37:24 -0300

    Resolvendo requisito 4:Criando funçao para adicionar as divs para o quadro no arquivo js/Adicionando um section no arquivo index.html

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ cores[0].style.background = "black";
 cores[1].style.background = "red";
 cores[2].style.background = "blue";
 cores[3].style.background = "green";
+
+function creatingBoard(contador) {
+  for (index = 0; index < contador; index++) {
+    let line = document.createElement("div");
+    line.className = "linePixel";
+    document.querySelector("#pixel-board").appendChild(line);
+  };
+  let linesPixel = document.querySelectorAll(".linePixel");
+  for (index = 0; index < linesPixel.length; index++) {
+    for (key = 0; key < contador; key++) {
+      let pixelS = document.createElement("div");
+      pixelS.className = "pixel";
+      linesPixel[index].appendChild(pixelS);
+    };
+  };
+};
+creatingBoard(5);
