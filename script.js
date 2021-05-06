43785620ae990054ee270eebea37639266896892
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-06 14:52:45 -0300

    Finalizado requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let colors = document.getElementsByClassName("color");
 
-
 document.addEventListener("click", function (event) {
+    let colorSelected = document.getElementsByClassName("selected")[0];
+
     if(event.target.classList.contains("color")) {
         for (let index = 0; index < colors.length; index += 1) {
             colors[index].classList.remove("selected");
         }
         event.target.classList.add("selected");
+    }
+
+    if(event.target.classList.contains("pixel")) {
+        event.target.style.backgroundColor = getComputedStyle(colorSelected).getPropertyValue("background-color");
     }
 })
