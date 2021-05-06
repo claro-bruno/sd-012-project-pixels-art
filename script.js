b7cfcd703aeeea27e701211c1b2aaa528a5db772
Eder Santos <3derms@gmail.com>
2021-05-06 17:24:33 -0300

    Resolve o requisito 6

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ coresDosBlocos[2].style.backgroundColor = "red";
 coresDosBlocos[3].style.backgroundColor = "blue";
 coresDosBlocos[4].style.backgroundColor = "green";
 
+window.onload = CorInicial
 
+function CorInicial(){
+document.getElementsByClassName("selected");
+}
