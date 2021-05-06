80f08ca1e5f5bcc27bbeeb3e0972c14f9d8bb48a
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-05 21:28:57 -0300

    tentando reparar avaliador zerado

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
   document.querySelector('.black').className.toggle('selected');
-}
+};
 
-//propriedade .toggle para alternar classe https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
-
+// propriedade .toggle para alternar classe https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
