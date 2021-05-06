5352c55df38f071af7790349012ff39104106e99
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-05 23:00:14 -0400

    Refatoramento dos desafios 1, 2 e 3 completados

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
-// Função que criar paleta de cores de forma dinâmica
-function createPalette(size) {
-  // 1. Capturar o elemento pai
-  const divPalette = document.getElementById('color-palette');
-  // 2. Cria um laço de repetição para adicionar 'size' divs dentro do Elemento pai
-  for (let index = 0; index < size; index += 1) {
+const colors = ['black', 'red', 'blue', 'green'];
+
+function makePalette(arrayColor) {
+  for (let index = 0; index < arrayColor.length; index += 1) {
+    const divPalette = document.getElementById('color-palette');
     const divColor = document.createElement('div');
     divColor.className = 'color';
+    divColor.style.backgroundColor = arrayColor[index];
     divPalette.appendChild(divColor);
   }
 }
-createPalette(4);
+makePalette(colors);
 
-// Função que pinta a paleta de cores a partir de um array de cores
-let arrayColor = ['black', 'red', 'blue', 'green'];
-// capturar o elemento pai em forma de array
-const divPalette = document.querySelectorAll('#color-palette')[0].children;
-console.log(divPalette);
-// pegar o primeiro filho do elemento pai
-// atribuir uma id a esse elemento
-// atribuir a primeira cor do array como id
+// Função que criar paleta de cores de forma dinâmica
+// function createPalette(size) {
+//   // 1. Capturar o elemento pai
+//   const divPalette = document.getElementById('color-palette');
+//   // 2. Cria um laço de repetição para adicionar 'size' divs dentro do Elemento pai
+//   for (let index = 0; index < size; index += 1) {
+//     const divColor = document.createElement('div');
+//     divColor.className = 'color';
+//     divPalette.appendChild(divColor);
+//   }
+// }
+// createPalette(4);
 
-function setColorPalette(arrayColor) {
+// let divLine = document.createElement('div');
+// divLine.className = 'divLine';
 
-}
+// let script = document.querySelector('script');
 
-
-
-
-
-let divLine = document.createElement('div');
-divLine.className = 'divLine';
-
-
-let script = document.querySelector('script');
-
-let body = document.querySelector('body');
-body.insertBefore(divLine, script);
+// let body = document.querySelector('body');
+// body.insertBefore(divLine, script);
 
 // divPalette.insertAdjacentElement('afterend', divLine)
-
