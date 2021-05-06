9e1bef276463834580a471c714ab244a3b0d611a
Juliane Marques <mqs.juliane@gmail.com>
2021-05-06 14:58:45 -0300

    adiciona pixel board linhas e colunas

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const pixelBoard = document.querySelector('#pixel-board');
+
+function creatingPixelBoardCollumns(row, colunas) {
+    for (let column = 1; column <= colunas; column += 1) {
+      const divColumn = document.createElement('div');
+      divColumn.classList.add('pixel');
+      divColumn.classList.add('td');
+      row.appendChild(divColumn);
+    }
+  }
