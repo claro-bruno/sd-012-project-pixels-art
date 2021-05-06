1b1d8c1cd095d2c0c1874a266438aa1b4998a911
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-05 22:08:21 -0300

    Resolve exercicios restante e adiciona background

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 window.onload = function () {
 
-  function criarPalheta() {
-    const cores = ['black', 'pink', 'purple', 'green'];
+  function criarPalheta(numeroDeCores) {
     const pegarPaleta = document.getElementById('color-palette');
-    for (let i = 0; i < cores.length; i += 1) {
+    for (let i = 0; i < numeroDeCores; i += 1) {
       const div = document.createElement('div');
-      div.style.backgroundColor = cores[i];
       if (i === 0) {
         div.className = 'color selected';
+        //div.style.transition = 'transform .2s';
+        //div.style.transform = 'scale(1.2)';
+        div.style.backgroundColor = 'black';
       } else {
         div.className = 'color';
+        div.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
       }
       pegarPaleta.appendChild(div);
     }
   }
 
-  criarPalheta();
+  criarPalheta(4);
 
   function criarPixelBoard(base) {
     const pegarPixelBoard = document.getElementById('pixel-board');
+    pegarPixelBoard.innerHTML = '';
+    pegarPixelBoard.style.width = `${base * 79}px`;
+    const totalPixels = document.querySelectorAll('.pixel');
     for (let index = 0; index < base*base; index += 1) {
       const div = document.createElement('div');
       div.className = 'pixel';
@@ -39,6 +44,8 @@ window.onload = function () {
           resetSelect();
           pintalPixel(event.target.style.backgroundColor);
           event.target.className = 'color selected';
+          //event.target.style.transition = 'transform .2s';
+          //event.target.style.transform = 'scale(1.2)';
         } 
       })
     }
@@ -48,6 +55,7 @@ window.onload = function () {
     const cores = document.querySelectorAll('.color');
     for (let i = 0; i < cores.length; i += 1) {
       cores[i].className = 'color';
+      //cores[i].style.transform = 'scale(1.0)';
     }
   }
 
@@ -73,4 +81,37 @@ window.onload = function () {
   }
 
   botaoLimpar();
+
+  function VQV() {
+    const vqvButton = document.getElementById('generate-board');
+    vqvButton.addEventListener('click', function () {
+      const input = document.getElementById('board-size');
+
+      if (!input.value.length) {
+        window.alert('Board inválido!');
+        input.value = 5;
+      }
+      if (input.value < 5) {
+        input.value = 5;
+      }
+      if (input.value > 50) {
+        input.value = 50;
+      }
+      criarPixelBoard(input.value);
+    })
+  }
+
+  VQV();
+
+  /*function gerarCoresBotao() {
+    const pegarBotaoCores = document.getElementById('generate-colors');
+    pegarBotaoCores.addEventListener('click', function (event) {
+      const coresAtuais = document.querySelectorAll('.color')
+      for (let i = 0; i < coresAtuais.length; i += 1) {
+        if (i !== 0) coresAtuais[i].style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+      }
+    })
+  }
+
+  gerarCoresBotao();*/
 }
