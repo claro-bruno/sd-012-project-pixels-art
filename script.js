c243afed24542ea6895ba12aa6e15586372725c8
Mateus <mateusbgonzaga@gmail.com>
2021-05-06 17:21:42 -0300

    fourth and fifth requirement fixed v3

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ window.onload = function(){
     let preColorSelected = document.getElementById('black');
     preColorSelected.className += ' selected';
 }
+
+let colorClick = document.getElementById('color-palette');
+colorClick.addEventListener('click', selectColor);
+
+function selectColor(eventOrigem) {
+     let idClick = eventOrigem.target.id;
+     document.getElementById("black").className = "color"; 
+     document.getElementById("red").className = "color";
+     document.getElementById("blue").className = "color";
+     document.getElementById("green").className = "color";
+
+     if (idClick == "black" || idClick == "red" || 
+     idClick == "blue" || idClick == "green") {
+         document.getElementById(idClick).className = "color selected";
+     }
+ }
