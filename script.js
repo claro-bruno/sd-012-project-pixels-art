1582b9199b593add253e5e1ee09d12f4ec9b1c48
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-05 22:06:18 -0300

    tenta corrigir requisito 6 e 7 com auxilio de colegas

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector('.black').className.toggle('selected');
+const colors = document.querySelectorAll('.color');
+const pixels = document.querySelectorAll('.pixel');
 
-// propriedade .toggle para alternar classe https://pt.stackoverflow.com/questions/225809/como-adicionar-uma-classe-em-javascript-puro
+function selectedColor (event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+for (let indexcolor = 0; indexcolor < colors.length; indexcolor += 1) {
+  colors[indexcolor].addEventListener('click', selectedColor);
+}
+
+// Após uma sucessão de erros com as funçoes, verifiquei o repositório da colega Adriana Biberg (https://github.com/tryber/sd-012-project-pixels-art/pull/122), cujo a função para acrescentar e remover a classe selected ao mesmo tempo que seleciona a cor de acordo com o click, foi de fácil compreensão.
