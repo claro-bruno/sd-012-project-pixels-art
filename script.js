9e7ac9a42ba5159f890a44edc8e8e86121732e08
rachidb5 <jordan.rachid@gmail.com>
2021-05-06 18:11:25 -0300

    adiciona funçao apara gerar cores aleatorias

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const pixel = document.getElementById('pixel-board');
-const preto = document.getElementById('preta')
-const azulClaro = document.getElementById('azul-claro')
-const azulEscuro = document.getElementById('azul-escuro')
-const claro = document.getElementById('clara')
-const colorPalette = document.getElementById('color-palette')
+const pixelBoard = document.getElementById('pixel-board');
+const preto = document.getElementById('preta');
+const azulClaro = document.getElementById('azul-claro');
+const azulEscuro = document.getElementById('azul-escuro');
+const claro = document.getElementById('clara');
+const selectedElement = document.querySelector('.selected');
+const colorPalette = document.getElementById('color-palette');
 
-/*function gerarCor() {
+/* cores da paleta inicio */
+
+function gerarCor() {
   const r = Math.random() * 255;
   const g = Math.random() * 255;
   const b = Math.random() * 255;
@@ -16,7 +19,7 @@ const colorPalette = document.getElementById('color-palette')
 const zero = gerarCor();
 const one = gerarCor();
 const two = gerarCor();
-const three = gerarCor();*/
+const three = gerarCor();
 
 function colors() {
   for (let i = 0; i < 4; i += 1) {
@@ -28,28 +31,39 @@ function colors() {
 
 colors()
 
-/*document.getElementsByClassName('color')[0].style.backgroundColor = zero
+document.getElementsByClassName('color')[0].style.backgroundColor = zero
 document.getElementsByClassName('color')[1].style.backgroundColor = one
 document.getElementsByClassName('color')[2].style.backgroundColor = two
-document.getElementsByClassName('color')[3].style.backgroundColor = three*/
+document.getElementsByClassName('color')[3].style.backgroundColor = three
+
+/*cores da paleta fim */
 
 function board() {
   for (let i = 0; i < 25; i += 1) {
     const div = document.createElement('div');
     div.className = 'pixel';
-    pixel.appendChild(div);
+    pixelBoard.appendChild(div);
   }
 }
 
 board();
+
 function select(event) {
-  const selectedElement = document.querySelector('.selected');
   selectedElement.classList.remove('selected');
   event.target.classList.add('selected');
-  input.value = '';
 }
 
 preto.addEventListener('click', select);
 azulClaro.addEventListener('click', select);
 azulEscuro.addEventListener('click', select);
 claro.addEventListener('click', select);
+
+const color = document.getElementsByClassName('selected')[0].style.backgroundColor
+  
+function paint(event) {
+  event.target.style.backgroundColor = 'color'
+}
+
+const pixel = document.querySelector('.pixel')
+
+pixel.addEventListener('click', paint)
