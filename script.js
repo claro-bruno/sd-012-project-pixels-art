f570bc7f7b8b44016984c7abae095a352a1c9930
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-06 00:08:53 -0300

    refatora o codigo - cria todo html e css com os requisitos necessarios e cria a pixel board atraves de funcoes

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const colors = document.querySelectorAll('.color');
-const pixels = document.querySelectorAll('.pixel');
+let line = 5;
+let column = 5;
+let pixelBoard = document.getElementById('pixel-board')
 
-function selectedColor (event) {
-  const selected = document.querySelector('.selected');
-  selected.classList.remove('selected');
-  event.target.classList.add('selected');
-}
+function addLine(line) {
+  for (let index = 0; index < line; index += 1) {
+    let divLine = document.createElement('div');
+    divLine.className = 'table-Row';
+    pixelBoard.appendChild(divLine);
+  };
+};
 
-for (let indexcolor = 0; indexcolor < colors.length; indexcolor += 1) {
-  colors[indexcolor].addEventListener('click', selectedColor);
-}
+addLine(line);
 
-// Após uma sucessão de erros com as funçoes, verifiquei o repositório da colega Adriana Biberg (https://github.com/tryber/sd-012-project-pixels-art/pull/122), cujo a função para acrescentar e remover a classe selected ao mesmo tempo que seleciona a cor de acordo com o click, foi de fácil compreensão.
+function addColumn (column) {
+  let columnsOfRow = document.getElementsByClassName('table-Row');
+  for (let columns of columnsOfRow) {
+    for ( let index = 0; index < column; index += 1) {
+      let divColumn = document.createElement('div');
+      divColumn.className = 'pixel';
+      columns.appendChild(divColumn);
+    };
+  };
+};
+
+addColumn(column);
