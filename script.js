4d7032ea57ccf3c3b74167390f315fcb3c8e4611
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-06 01:01:46 -0400

    Desafio 7 completo e refatorado - Seleciona cores com o click

--- script.js
+++ script.js
@@ -26,24 +26,20 @@ function makerBoardPixel(size) {
 }
 makerBoardPixel(5);
 
-function selectFirstColor () {
-const firstColor = document.querySelector('#color-palette').firstChild;
-firstColor.classList.add('selected');
+function selectFirstColor() {
+  const firstColor = document.querySelector('#color-palette').firstChild;
+  firstColor.classList.add('selected');
 }
 selectFirstColor();
 
-// Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
-const colorPalette = document.querySelector('#color-palette');
-// adicionar uma escuta na div color-palette
-colorPalette.addEventListener('click', (event) => {
-  // criar condição de click, só pode receber a classe selected se já tiver a classe color
-  if (event.target.className === 'color') {
-    // buscar elemento pela classe selected
-    const selectedColor = document.querySelector('.selected')
-    // remover a classe do elemento
-    selectedColor.classList.remove('selected')
-    // identificar com o target de onde veio o click
-    // atribuir a classe selected a cor que cliquei
-    event.target.classList.add('selected')
-  }
-})
+function selectColor() {
+  const colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', (event) => {
+    if (event.target.className === 'color') {
+      const selectedColor = document.querySelector('.selected');
+      selectedColor.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  });
+}
+selectColor();
