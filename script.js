8883469d2336abafa4578f0ac4b1dd7a7f4c05d7
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-06 00:02:45 -0300

    Resolve o requisito 7

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let boardSize = 5;
 
 createBoard(boardSize);
 
+changeSelected();
+
+
 function createBoard (boardSize) {
 
     for (let index = 0; index < boardSize; index += 1) {
@@ -19,3 +22,25 @@ function createBoard (boardSize) {
     }
 }
 
+
+
+function changeSelected() { 
+    let firstColor = document.querySelector('#first-div');
+    let secondColor = document.querySelector('#second-div');
+    let thirdColor = document.querySelector('#third-div');
+    let fourthColor = document.querySelector('#fourth-div');
+
+
+    firstColor.addEventListener('click', selectedColor(event));
+    secondColor.addEventListener('click', selectedColor(event));
+    thirdColor.addEventListener('click', selectedColor(event));
+    fourthColor.addEventListener('click', selectedColor(event));
+
+    function selectedColor(event){ //Através da contribuição do colega André Moreno no Slack conheci a propriedade target event.
+        let replaced = document.querySelector('.selected');
+        replaced.classList.remove('.selected');
+        event.target.classList.add('selected');
+    }
+
+    
+}
