a25659b1d39aadb53d6716dd248bb82e56755b2a
Victor <juanvictor70@gmail.com>
2021-05-06 17:04:51 -0400

    Adicionando listener das 4 cores

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ function changeSelect(element) {
   selected.className = 'color';
   elemento.target.className += 'selected';
 }
+
+function changeSelectedClick() {
+  let color1 = document.getElementById('color1');
+  let color2 = document.getElementById('color2');
+  let color3 = document.getElementById('color3');
+  const color4 = document.getElementById('color4');
+
+  color1.addEventListener('click', changeSelected);
+  color2.addEventListener('click', changeSelected);
+  color3.addEventListener('click', changeSelected);
+  color4.addEventListener('click', changeSelected);
+}
