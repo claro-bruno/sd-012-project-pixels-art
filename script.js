19bc83b2aee8eb56c9865d120f78bf900ea3ce2a
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-06 00:49:44 -0300

    requisito 8 - com muita ajuda e pesquisa

--- script.js
+++ script.js
@@ -25,12 +25,6 @@ function addColumn (column) {
 
 addColumn(column);
 
-/*function colorSelected (event) {
-  let initialColor = document.querySelector('.selected');
-  initialColor.classList.remove('selected');
-  event.target.classList.add('selected');
-};*/
-
 const colors = document.querySelectorAll('.color');
 const pixels = document.querySelectorAll('.pixel');
 
@@ -45,3 +39,17 @@ for (let indexcolor = 0; indexcolor < colors.length; indexcolor += 1) {
 }
 
 // Após uma sucessão de erros com as funçoes, verifiquei o repositório da colega Adriana Biberg (https://github.com/tryber/sd-012-project-pixels-art/pull/122), cujo a função para acrescentar e remover a classe selected ao mesmo tempo que seleciona a cor de acordo com o click, foi de fácil compreensão.
+
+let pixel = document.getElementsByClassName('pixel');
+
+function paintCanvas (event) {
+  let brushColor = document.querySelector('.selected');
+  let paintColor = brushColor.id;
+  event.target.style.backgroundColor = paintColor;
+};
+
+for (let element of pixel) {
+  element.addEventListener('click', paintCanvas);
+};
+
+// O requisito 8 foi gracas ao colega Ryan Laiber, compreendi a alteracao do background do elemento selecionado apenas apos verificar seu repositorio, porem a linha 47, estou deduzindo que puxe a cor pelo id do elemento com a class selected, nao localizei outra explicacao mas faz sentido para mim. (https://github.com/tryber/sd-012-project-pixels-art/pull/99/files#)
