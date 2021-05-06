8fcee9b6769c78794ad95ee8c112670c4c04cad9
Anderson B <andbmarques@gmail.com>
2021-05-06 09:06:15 -0300

    Requisito 7 adicionado

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 window.onload = function(){
-
-    let corSelecionada = 'black'
-
     gerarPixelBoard();
+    let cores = document.getElementsByClassName('color')
+    
+    selecionarCor(cores)
+    
+    
+
 }
 
 function gerarPixelBoard(){
     let pixelBoard = document.getElementById('pixel-board');
-
     for(let i = 0; i < 5; i++){
-        
         for(let j = 0; j < 5; j++){
             let cell = document.createElement('div')
             cell.style.border = '1px solid black'
@@ -20,4 +21,18 @@ function gerarPixelBoard(){
             pixelBoard.appendChild(cell)
         }
     }
+}
+
+function selecionarCor(array){
+
+    for(let item of array){
+        item.addEventListener('click', (e) => {
+            for(let itemRemovivel of array){
+                itemRemovivel.classList.remove('selected')
+            }
+            e.target.classList.toggle('selected')
+            console.log(array)
+        })
+    }
+    
 }
