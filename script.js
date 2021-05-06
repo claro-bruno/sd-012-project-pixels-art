013ae314c35f289cc314412e71482b68328267f7
rachidb5 <jordan.rachid@gmail.com>
2021-05-06 01:16:11 -0300

    numero de elementos e tamanho do quadro

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let pixel = document.getElementById('pixel-board')
 
 function board() {
-    for(let i = 0; i < 875; i +=1) {
+    for(let i = 0; i < 25; i +=1) {
         let div = document.createElement('div')
         div.className = 'pixel'
         pixel.appendChild(div)
