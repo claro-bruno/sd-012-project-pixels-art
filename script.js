7a89a9420e7504725adeecc2963092085dccc9d3
Victor <juanvictor70@gmail.com>
2021-05-06 17:33:00 -0400

    Alterando alguns Let por Const

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ window.onload = function pixelsArt() {
 };
 
 function generateRandomColor() {
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   return `#${randomColor}`;
 }
 function createPalette(id, color) {
-  let colorPalette = document.getElementById('color-palette');
-  let createDiv = document.createElement('div');
+  const colorPalette = document.getElementById('color-palette');
+  const createDiv = document.createElement('div');
   
   createDiv.className = 'color';
   createDiv.id = id;
@@ -24,42 +24,42 @@ function createPalette(id, color) {
   colorPalette.appendChild(createDiv);
 }
 function createPixelBoard(number) {
-  let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
   
   for (let index = 0; index < numero; index += 1) {
-    let createPixel = document.createElement('div');
+    const createPixel = document.createElement('div');
     createPixel.className = 'pixel';
     pixelBoard.appendChild(createPixel);
   }
 }
 function initialColor(id) {
-  let initialColor = document.getElementById(id);
+  const initialColor = document.getElementById(id);
   initialColor.className += 'selected';
 }
 function changeSelect(element) {
-  let selected = document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   selected.className = 'color';
   element.target.className += 'selected';
 }
 
 function changeColorClick() {
-  let pixels = document.querySelectorAll('.pixel');
-  for (let pixel of pixels) {
+  const pixels = document.querySelectorAll('.pixel');
+  for (const pixel of pixels) {
     pixel.addEventListener('click', changeColor);
   }
 }
 
 function changeColor(element) {
-  let selectedColor = document.querySelector('.selected');
-  let compStyle = window.getComputedStyle(selectedColor);
+  const selectedColor = document.querySelector('.selected');
+  const compStyle = window.getComputedStyle(selectedColor);
   element.target.style.backgroundColor = compStyle.getPropertyValue('background-color');
 }
 
 function changeSelectedClick() {
-  let color1 = document.getElementById('color1');
-  let color2 = document.getElementById('color2');
-  let color3 = document.getElementById('color3');
-  let color4 = document.getElementById('color4');
+  const color1 = document.getElementById('color1');
+  const color2 = document.getElementById('color2');
+  const color3 = document.getElementById('color3');
+  const color4 = document.getElementById('color4');
 
   color1.addEventListener('click', changeSelected);
   color2.addEventListener('click', changeSelected);
@@ -68,20 +68,20 @@ function changeSelectedClick() {
 }
 
 function clearBoardClick() {
-  let clearButton = document.getElementById('clear-board');
+  const clearButton = document.getElementById('clear-board');
   clearButton.addEventListener('click', clearBoard);
 }
 function clearBoard() {
-  let pixels = document.querySelectorAll('.pixel');
-  for (let pixel of pixels) {
+  const pixels = document.querySelectorAll('.pixel');
+  for (const pixel of pixels) {
     pixel.style.backgroundColor = 'white';
   }
 }
 
 function sizeBoard() {
-  let pixelBoard = document.getElementById('pixel-board');
-  let userInput = document.getElementById('board-size');
-  let numberOfInput = 0;
+  const pixelBoard = document.getElementById('pixel-board');
+  const userInput = document.getElementById('board-size');
+  const numberOfInput = 0;
 
   if (parseInt(userInput.value) < 5) {
     numberOfInput = 5;
@@ -96,4 +96,25 @@ function sizeBoard() {
   }
   pixelBoard.style.width = `${(numberOfInput * 40) + (2 * numberOfInput)}px`;
   pixelBoard.style.height = `${(numberOfInput * 40) + (2 * numberOfInput)}px`;
+}
+
+function userBoarder() {
+  const userInput = document.getElementById('board-size');
+  let numberOfInput = 0;
+  if (parseInt(userInput.value) < 5) {
+    numberOfInput = 5;
+  } else if (parseInt(userInput.value) > 50){
+    numberOfInput = 50;
+  } else {
+    numberOfInput = parseInt(userInput.value);
+  }
+  const numberOfPixels = {};
+  for (let index = 5; index <= 50; index += 1) {
+    numberOfPixels[index] = index * index;
+  }
+  const finalNumber = numberOfPixels[numberOfInput];
+  if (userInput.value.length < 1) {
+    return alert('Board inválido!'), createPixelBaordElement(25);
+  }
+  return finalNumber;
 }
