f44d4e038581c67bd7df3e6b77c2ec81eb48f52c
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-05-06 10:09:44 -0300

    Requisito 7 cumprido (select color)

--- script.js
+++ script.js
@@ -17,28 +17,24 @@ for (let i = 0; i < nPixels; i += 1) {
   }
 }
 //6
-
 const colors = document.querySelectorAll('.color');
 const black = colors[0];
+const color2 = colors[1];
+const color3 = colors[2];
+const color4 = colors[3];
 black.classList.add('selected');
 
+//7 
+function getColor(event) {
+  const currentSelectedColor = document.querySelector('.color.selected');
+  currentSelectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+black.addEventListener('click', getColor);
+color2.addEventListener('click', getColor);
+color3.addEventListener('click', getColor);
+color4.addEventListener('click', getColor);
 
 
 }
