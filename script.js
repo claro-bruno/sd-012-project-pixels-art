e00a740bdc2bf50ed0e9100aa25db996b4396101
martinsph <ph.mmartins@gmail.com>
2021-05-06 18:40:25 -0300

    corrigindo requisito 4

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let areaDiv2 = document.getElementById("div2")
 
 function createBoard(row, column) {
     let createTable = document.createElement('table');
+    createTable.className = "pixel-board";
     let tbody = document.createElement('tbody');
     areaDiv2.appendChild(createTable)
 
