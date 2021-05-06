d7a8b250c5df4176ed96064a14bbc10878372388
Eder Santos <3derms@gmail.com>
2021-05-06 18:42:22 -0300

    corrige espaçamento e pequenos erros no script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let coresDosBlocos = document.querySelectorAll("div");
-coresDosBlocos[1].style.backgroundColor = "black";
-coresDosBlocos[2].style.backgroundColor = "red";
-coresDosBlocos[3].style.backgroundColor = "blue";
-coresDosBlocos[4].style.backgroundColor = "green";
+const coresDosBlocos = document.querySelectorAll('div');
+coresDosBlocos[1].style.backgroundColor = 'black';
+coresDosBlocos[2].style.backgroundColor = 'red';
+coresDosBlocos[3].style.backgroundColor = 'blue';
+coresDosBlocos[4].style.backgroundColor = 'green';
 
-window.onload = CorInicial
-
-function CorInicial(){
-//document.getElementsByClassName("selected");
-document.querySelector(".selected");
+function CorInicial() {
+// document.getElementsByClassName("selected");
+    document.querySelector('.selected');
 }
 
-//if (clicar=true)
-//document.getElementbyClassName    
+window.onload = CorInicial;
+
+// if (clicar=true)
+// document.getElementbyClassName
