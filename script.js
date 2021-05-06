aa124f735ccd583c8aec6a9934db58dc24fe6ef0
thales daniel <thalesdaniel2013@gmail.com>
2021-05-06 16:34:49 -0300

    feito a etapa 7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for (index = 0; index < 25; index += 1) {
     let criacao = document.createElement("div")
-    let classe = criacao.className = 'pixel'
+    criacao.className = 'pixel'
     const div = document.querySelector('#pixel-board').appendChild(criacao)
 }
 
@@ -8,5 +8,16 @@ window.onload = function inicializacao() {
     let preto = document.querySelector('#preto')
     preto.classList.add('selected')
 
+}
 
+let cor = document.querySelectorAll('.color')
+console.log(cor)
+for (let index = 0; index < cor.length; index += 1) {
+    cor[index].addEventListener('click', removeClass)
+}
+
+function removeClass(event) {
+    let remover = document.querySelector('.selected')
+    remover.classList.remove('selected')
+    event.target.classList.add('selected')
 }
