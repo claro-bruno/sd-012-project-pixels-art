58e84c10df6467af1e6373b819b524c49a569503
Janderson Soares <jandinsm@gmail.com>
2021-05-06 16:13:14 -0300

    Desafio 2 pelo javascript

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// Pega os 4 p e seleciona
-let list = document.querySelectorAll(".color")
-// Variavel com 4 cores 
-let cores = ["black", "purple", "red", "blue"]
+const arrayCores = ["black", "purple", "red", "blue"];
 
-// Define a cor de cada das 4 lista para cada cor das variavel cores
+const paiPaleta = document.getElementById("color-palette");
+
 function paletaArco(){
-        for (let index = 0; index < list.length; index++) {
-           list[index].style.background = cores[index];
-        }
-}
+   for(let index = 0; index < arrayCores.length; index++){
+      const criaPaleta  = document.createElement("p");
+      criaPaleta.className = "color";
+
+      criaPaleta.style.background = arrayCores[index];
+      criaPaleta.style.borderStyle = "solid";
+      criaPaleta.style.borderColor = "black";
+      criaPaleta.style.borderWidth = "1px";
+      criaPaleta.style.height = "40px";
+      criaPaleta.style.width = "40px";
+      criaPaleta.style.display = "inline-block";
+
+      paiPaleta.appendChild(criaPaleta);
+   };
+};
 paletaArco();
