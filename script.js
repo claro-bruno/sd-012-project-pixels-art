1467603e8c9bbf6f244179abefb6a4df3e10c08b
Juan Ribeiro Rezende <rezendejuan0@gmail.com>
2021-05-06 11:29:47 -0300

    dinamizando os requisitos de paleta e quadro

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+let cores = ["black", "blue", "red", "green"];
 
-var cores = document.getElementsByClassName("color");
 var paleta = document.getElementById("color-palette");
+var quadro = document.getElementById("pixel-board");
 
-paleta.children[0].style.backgroundColor = "black";
-paleta.children[1].style.backgroundColor = "yellow";
-paleta.children[2].style.backgroundColor = "red";
-paleta.children[3].style.backgroundColor = "green";
+for(let i = 0; i < cores.length; i++){
+    let paint = document.createElement("div");
+    paint.className = "color";
+    paint.style.backgroundColor = cores[i];
+    paleta.appendChild(paint);
+}
+
+function board(size){
+    for(let i = 0; i < size; i++){
+        let linha = document.createElement("div");
+        linha.className = "linha";
+        quadro.appendChild(linha);
+    }
+    for(let i = 0; i < size; i++){
+        for(let i = 0; i < size; i++){
+            let pixel = document.createElement("div");
+            pixel.className = "pixel";
+            document.querySelectorAll(".linha")[i].appendChild(pixel);
+        }
+    }    
+}
+
+board(5);
