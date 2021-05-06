5f7977a6a51c9394e17b3f1e6106b5c722fb8fe6
Neto Silva <netosilv44445@gmail.com>
2021-05-06 02:03:42 -0300

    Refatorado ESLint

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
 const quadro = document.querySelector('#pixel-board');
-const pixel = '<div class="pixel" onclick="paint(this)"> <div>';
+const pixel = '<div class="pixel"> <div>';
 const colors = document.querySelectorAll('.color');
 const selectedColor = 'selected';
 let corSelecionada = 'black';
+
 function coresAleatoias() {
   for (let index = 1; index < colors.length; index += 1) {
     colors[index].style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 }
-
 coresAleatoias();
-function criaQuadro() {
-  for (let i = 0; i < 25; i += 1) {
+
+function criaQuadros(tamanho) {
+  for (let i = 0; i < tamanho; i += 1) {
     quadro.innerHTML += pixel;
   }
 }
 
-function quadroPersonalizado() {
+document.getElementById('generate-board').addEventListener('click', () => {
   let tamanho = document.getElementById('board-size').value;
-  if (tamanho > 0 && tamanho >= 5 && tamanho <= 50) {
-    tamanho *= tamanho;
-    quadro.innerHTML = '';
-    for (let i = 0; i < tamanho; i += 1) {
-      quadro.innerHTML += pixel;
-    }
-  } else if (tamanho > 0 && tamanho < 5) {
-    tamanho = 5 * 5;
-    quadro.innerHTML = '';
-    for (let i = 0; i < tamanho; i += 1) {
-      quadro.innerHTML += pixel;
-    }
-    } else if (tamanho > 50) {
+  if (tamanho > 50) {
     tamanho = 50 * 50;
     quadro.innerHTML = '';
-    for (let i = 0; i < tamanho; i += 1) {
-      quadro.innerHTML += pixel;
-    }
-    } else {
+    criaQuadros(tamanho);
+  } else if (tamanho <= 50 && tamanho > 4) {
+    tamanho *= tamanho;
+    quadro.innerHTML = '';
+    criaQuadros(tamanho);
+  } else if (tamanho > 0) {
+    tamanho = 5 * 5;
+    quadro.innerHTML = '';
+    criaQuadros(tamanho);
+  } else {
     window.alert('Board inválido!');
-    }
-}
-
-function selectColor(color) {
-  for (let index = 0; index < colors.length; index += 1) {
-    if (colors[index].classList.contains('selected')) {
-      colors[index].classList.remove('selected');
-    }
   }
-  color.classList.add(selectedColor);
-  for (let index = 0; index < colors.length; index += 1) {
-    if (colors[index].classList.contains('selected')) corSelecionada = colors[index].style.backgroundColor;
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('color')) {
+    const selecionada = event.target;
+    corSelecionada = selecionada.style.backgroundColor;
+    const s = document.querySelector('.selected');
+    s.classList.remove('selected');
+    selecionada.classList.add('selected');
   }
-}
+}, false);
 
-function paint(pixelSquare) {
-  const pix = pixelSquare;
-  pix.style.backgroundColor = corSelecionada;
-}
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pixel')) {
+    const selecionado = event.target;
+    selecionado.style.backgroundColor = corSelecionada;
+  }
+}, false);
 
-function resetColors() {
+document.getElementById('clear-board').addEventListener('click', () => {
   const quadros = quadro.children;
   for (let index = 0; index < quadros.length; index += 1) {
     quadros[index].style.backgroundColor = 'white';
   }
-}
+});
 
-criaQuadro();
+criaQuadros(25);
