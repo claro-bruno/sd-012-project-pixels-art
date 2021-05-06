42d9faa3258139a307fc1306499e5fb1393d69e3
Jossany Moura <mjossany@gmail.com>
2021-05-06 00:15:35 -0300

    Resolve requisito 10 e 11

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ function coresPaleta(array) {
 const paletaCores = ['blue', 'green', 'yellow'];
 coresPaleta(paletaCores);
 
-let numberOfLines = 5;
-function createLines (numberOfLines) {
-  let linesContainer = document.querySelector('#pixel-board');
-  for (let index = 0; index < numberOfLines; index += 1) {
+let linesContainer = document.getElementById('pixel-board');
+
+function createLines (numbLines, sizeControl) {
+  for (let index = 0; index < numbLines; index += 1) {
     let line = document.createElement('div');
     line.classList.add('line');
+    line.style.width = sizeControl + 'px';
     linesContainer.appendChild(line);
   }
+  fillLines();
 }
-createLines(numberOfLines);
+createLines(5);
 
 function createBox(className) {
   let box = document.createElement('div');
@@ -30,13 +32,12 @@ function createBox(className) {
 function fillLines() {
   let boxContainer = document.getElementsByClassName('line');
   for (let index = 0; index < boxContainer.length; index += 1) {
-    for (let index2 = 0; index2 < numberOfLines; index2 += 1) {
+    for (let index2 = 0; index2 < boxContainer.length; index2 += 1) {
       let box = createBox('pixel')
       boxContainer[index].appendChild(box);
     }
   }
 }
-fillLines();
 
 function setSelectedClass(event) {
   const paleta = document.querySelector('.selected');
@@ -51,9 +52,7 @@ paletas[index].addEventListener('click', setSelectedClass);
 
 function setPixelColor() {
   let selectedPaleta = document.querySelector('.selected');
-  console.log(selectedPaleta);
   let paletaColor = selectedPaleta.style.backgroundColor;
-  console.log(paletaColor);
   let pixels = document.getElementsByClassName('pixel');
    
     for (let index = 0; index < pixels.length; index += 1) {
@@ -75,8 +74,9 @@ function createCleanButton(buttonName) {
   newButton.innerHTML = 'Limpar';
   newButton.id = newButtonID;
   buttonContainer.appendChild(newButton);
+  return newButton;
 }
-createCleanButton('clear-board');
+let botaoLimpa = createCleanButton('clear-board');
 
 function cleanBoard() {
   let getCleanButton = document.querySelector('#clear-board');
@@ -92,3 +92,46 @@ function cleanBoard() {
   })
 }
 cleanBoard();
+
+
+
+
+function createLineInput(inputId) {
+  let inputContainer = document.querySelector('#buttonContainer');
+  let newInput = document.createElement('input');
+  let newInputID = inputId;
+  newInput.id = newInputID;
+  newInput.type = 'number';
+  newInput.min = 1;
+  newInput.max = 50;
+  botaoLimpa.insertAdjacentElement("afterend", newInput);
+  return newInput;
+}
+
+function createLineButton(buttonId) {
+  let buttonContainer = document.querySelector('#buttonContainer');
+  let newButton = document.createElement('button');
+  let newButtonID = buttonId;
+
+  newButton.innerHTML = 'VQV';
+  newButton.id = newButtonID;
+  buttonContainer.appendChild(newButton);
+}
+createLineButton('generate-board');
+
+let inputSize = createLineInput('board-size');
+
+let click = document.getElementById('generate-board')
+click.addEventListener('click', function() {
+  let inputValue = inputSize.value;
+  if (inputValue === '') {
+    alert('Board inválido!');
+  } else if (inputValue < 5) {
+    inputValue = 5;
+  } else if (inputValue > 50) {
+    inputValue = 50;
+  }
+  let controlLine = inputValue * 40 + 100;
+  linesContainer.innerHTML = '';
+  createLines(inputValue, controlLine);
+})
