6457e4c5cb9303d31efcb038999f0774a5752192
Wellington <junior.wel02@exemplo.br>
2021-05-05 21:36:10 -0300

    Corrigido selected

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
 let quadrad = document.querySelector('#pixel-board');
+let cor1 = document.querySelector('#black');
+let cor2 = document.querySelector('#orange');
+let cor3 = document.querySelector('#red');
+let cor4 = document.querySelector('#yellow');
 let nlinhas = 5;
 let nPixels = 5;
 
-function criaTabela () {
-    for(index = 0; index < nlinhas; index += 1){
-        let criarTr = document.createElement('tr');
-        quadrad.appendChild(criarTr);
-        for (i = 0; i < nPixels; i += 1){
-            let criarTd = document.createElement('td');
-            criarTr.appendChild(criarTd);
-        }
+function criaTabela() {
+  for (index = 0; index < nlinhas; index += 1) {
+    let criarTr = document.createElement('tr');
+    quadrad.appendChild(criarTr);
+    for (i = 0; i < nPixels; i += 1) {
+      let criarTd = document.createElement('td');
+      criarTr.appendChild(criarTd);
     }
+  }
 }
 criaTabela();
 
-function classes (){
-    let tds = document.querySelectorAll("#pixel-board tr td");
-    for (let index = 0; index < tds.length; index += 1){
-        tds[index].className = "pixel";
-    }
+function classes() {
+  let tds = document.querySelectorAll('#pixel-board tr td');
+  for (let index = 0; index < tds.length; index += 1) {
+    tds[index].className = 'pixel';
+  }
 }
 classes();
 
-function colocaClassSelected() {
-    let corPaleta = document.querySelector("#color-palette");
-    let corSelecionada = document.getElementsByClassName("selected");
-    corPaleta.addEventListener("click", function (evento) {
-      if (corSelecionada.length === 0) {
-        evento.target.className = "color selected";
-      } else {
-        evento.target.className = "color";
-      }
+function colocaClassSelected(evento) {
+ let selecao = document.querySelector('.selected');
+ selecao.classList.remove('selected');
+ evento.target.classList.add('selected')
+}
+
+cor1.addEventListener("click", colocaClassSelected);
+cor2.addEventListener("click", colocaClassSelected);
+cor3.addEventListener("click", colocaClassSelected);
+cor4.addEventListener("click", colocaClassSelected);
+
+
+function adiocionaCor() {
+  let tds = document.querySelectorAll('#pixel-board tr td');
+  let corDaPaleta = document.querySelector('.selected');
+  for (let index = 0; index < tds.length; index += 1) {
+    tds[index].addEventListener('click', function (event) {
+      event.target.id = corDaPaleta.id;
     });
   }
-  colocaClassSelected();
+}
+adiocionaCor();
