f783d4580e2827cf656e7214631dd2c48a07aba8
rachidb5 <jordan.rachid@gmail.com>
2021-05-06 10:07:20 -0300

    adiciona funçao de selecionar paleta

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let pixel = document.getElementById('pixel-board')
+const pixel = document.getElementById('pixel-board');
+const preto = document.getElementById('preta')
+const azulClaro = document.getElementById('azul-claro')
+const azulEscuro = document.getElementById('azul-escuro')
+const claro = document.getElementById('clara')
 
 function board() {
-    for(let i = 0; i < 25; i +=1) {
-        let div = document.createElement('div')
-        div.className = 'pixel'
-        pixel.appendChild(div)
+  for (let i = 0; i < 25; i += 1) {
+    const div = document.createElement('div');
+    div.className = 'pixel';
+    pixel.appendChild(div);
     }
 }
 
-board()
+board();
+
+function select(event) {
+  const selectedElement = document.querySelector('.selected');
+  selectedElement.classList.remove('selected');
+  event.target.classList.add('selected');
+  input.value = '';
+}
+
+preto.addEventListener('click', select)
+azulClaro.addEventListener('click', select)
+azulEscuro.addEventListener('click', select)
+claro.addEventListener('click', select)
