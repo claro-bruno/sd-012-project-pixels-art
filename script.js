a0f689bb7a080d4f993fa5b8d0402a59158c4625
Sarah <sarahgsb@hotmail.com>
2021-05-06 02:22:47 -0300

    requisito 9

---
+++ script.js
@@ -0,0 +1,9 @@
+let pixels = document.getElementsByClassName('pixel');
+let clear = document.getElementById('clear-board');
+clear.addEventListener('click', function () {
+     for (let index = 0; index < pixels.length; index += 1){
+         pixels[index].style.backgroundColor = 'white';
+        }
+    });
+    
+
