6d8f55561de7680483f0288e77c4d1b3a96df54f
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-05-06 19:34:09 -0300

    Adiciona exercicio 6 e 7

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const black = document.querySelector('#black');
+const red = document.querySelector('#red');
+const silver = document.querySelector('#silver');
+const golden = document.querySelector('#golden');
 
+function addSelected(event) {
+  const selectedElement = document.querySelector('.selected');
+  selectedElement.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+black.addEventListener('click', addSelected);
+red.addEventListener('click', addSelected);
+silver.addEventListener('click', addSelected);
+golden.addEventListener('click', addSelected);
