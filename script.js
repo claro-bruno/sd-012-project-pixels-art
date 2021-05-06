e154585eeb2a6f5fc094ba605899a021791a84ca
Raphael <raphael.ph2ll@gmail.com>
2021-05-06 10:26:29 -0300

    ex07 colorChange

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 function createLines(total) {
-    const all = total;
-    const pixelBoard = document.querySelector('#pixel-board');
-    for (let index = 0; index < all; index += 1){
-      const pixelLine = document.createElement('div');
-      pixelLine.className = 'pixelLine';
-      pixelBoard.appendChild(pixelLine);
-        for (let index = 0; index < all; index += 1) {
-          const pixel = document.createElement('div');
-          pixel.className = 'pixel';
-          pixelLine.appendChild(pixel);
-        };
-    };   
-};
+  const all = total;
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let index = 0; index < all; index += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.className = 'pixelLine';
+    pixelBoard.appendChild(pixelLine);
+    for (let index1 = 0; index1 < all; index1 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelLine.appendChild(pixel);
+    }
+  }
+}
 createLines(5);
 
+const color1 = document.getElementsByClassName('color')[0];
+const color2 = document.getElementsByClassName('color')[1];
+const color3 = document.getElementsByClassName('color')[2];
+const color4 = document.getElementsByClassName('color')[3];
+
+function colorChange(event) {
+  const color = document.querySelector('.selected');
+  color.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+color1.addEventListener('click', colorChange);
+color2.addEventListener('click', colorChange);
+color3.addEventListener('click', colorChange);
+color4.addEventListener('click', colorChange);
