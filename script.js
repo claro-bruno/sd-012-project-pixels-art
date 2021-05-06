5b362cd0e637fdc828fc398a76de50eb9e8d5777
Gabriel Takaki <gabitj@icloud.com>
2021-05-06 12:53:39 -0300

    Resoluçao do 10 e 11

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const colors = ['#000', '#405d27', '#034f84', '#c94c4c'];
 const clearBtn = document.getElementById('clear-board');
 
 
+
 function colorSelector() {
     for(let index = 0; index < colors.length; index+=1) {
         const eachColor = document.createElement('div');
@@ -17,17 +18,48 @@ function colorSelector() {
 }
 colorSelector();    
 
-function drawingBoarder() {    
-    for(let index = 0; index < 25; index+=1) {
-        const eachPixel = document.createElement('div');
-        const pixel = index;
-        eachPixel.innerText = pixel;
-        eachPixel.className = 'pixel';
-        pixelBoard.appendChild(eachPixel);
-        pixelBoard.style.maxWidth = index*8.75 + 'px';
+function drawingBoarder(board) {    
+    const boardWidth = board * 40 + board * 2 + 1;
+    pixelBoard.style.width = `${boardWidth}px`
+    for(let index = 0; index < board; index+=1) {
+        for(let index2 = 0; index2 < board; index2+=1) {   
+            const eachPixel = document.createElement('div');
+            eachPixel.className = 'pixel';
+            pixelBoard.appendChild(eachPixel);
+        }
     };
 }    
-drawingBoarder();
+drawingBoarder(5);
+
+function removeBoard() {
+    const board = document.querySelector('#pixel-board');
+    const pixelDivs = document.querySelectorAll('.pixel');
+
+    for(let index = 0; index < pixelDivs.length; index +=1) {
+        board.removeChild(pixelDivs[index]);
+    }
+}
+
+function changeBorderNum() {
+    const inputValue = document.getElementById('board-size');
+    const inputBtn = document.getElementById('generate-board');
+    
+    inputBtn.addEventListener('click', (e) => {
+        if(inputValue.value === '') {
+            alert('Board inválido!');
+        } else if (inputValue.value < 5) {
+            removeBoard();
+            drawingBoarder(5)
+        } else if (inputValue.value > 50) {
+            removeBoard();
+            drawingBoarder(50)
+        } else {
+            removeBoard();
+            drawingBoarder(inputValue.value);
+        }
+    })
+}
+changeBorderNum();
 
 const colorDraw = document.querySelectorAll('.pixel');
 
