6a6767be348aec4868060d0442e89cc042886937
Juliane Marques <mqs.juliane@gmail.com>
2021-05-06 14:38:02 -0300

    coloca title na header

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    let penColor = 'black';
+
+    function setPenColor(color) {
+        penColor = color;
+    }
+
+    function setPixelColor(pixel) {
+        pixel.style.backgroundColor = penColor;
+
+    }
+
+}
+
+// add event listener (dica)
