19c0b2132e8e5df031e432b975facbecd9c9369c
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-05-06 17:07:46 -0300

    Terminando exercise 7

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+let palleteColors= document.querySelectorAll(".color")
 document.querySelectorAll(".color")[0].style.backgroundColor ="black";
 document.querySelectorAll(".color")[1].style.backgroundColor ="red";
 document.querySelectorAll(".color")[2].style.backgroundColor ="blue";
 document.querySelectorAll(".color")[3].style.backgroundColor ="green";
-
 
 let pixelBoard = document.querySelector("#pixel-board")
 function createPixel (result){
@@ -19,3 +19,25 @@ function createPixel (result){
 }
 
 createPixel(5)
+
+function pixelsColors (cor){
+    let pixel = document.querySelectorAll(".pixel");
+    for(index = 0; index < pixel.length; index +=1){
+        pixel[index].addEventListener('click', function(event){
+            event.target.style.backgroundColor = cor
+        })
+    }
+}
+
+for (index = 0; index < palleteColors.length; index +=1){
+    palleteColors[index].addEventListener("click", function(event){
+    let colorSelected = document.querySelector('.selected');
+    if( event.target.classList !== "color selected"){
+        event.target.classList.add("selected");
+        colorSelected.classList.remove("selected");
+        pixelsColors(event.target.style.backgroundColor);
+        }
+    })
+}
+
+
