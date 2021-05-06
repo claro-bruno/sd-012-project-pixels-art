ccf1ab12bcf5e51559952417e78105ad84ce270f
Euller Braz <euller.braz99@gmail.com>
2021-05-06 11:16:18 -0300

    Resolvido alguns problemas dos Lintes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = function () {
   function generateRandomColors() {
-    let colorsArray = document.getElementsByClassName('color');
+    const colorsArray = document.getElementsByClassName('color');
     for (let index = 1; index < colorsArray.length; index += 1) {
-      let color =
-        '#' +
+      const color = '#' +
         parseInt(Math.random() * 0xfff)
           .toString(16)
           .padStart(3, '0');
@@ -12,14 +11,14 @@ window.onload = function () {
   }
 
   function generatePixelsLine() {
-    let divPixel = document.createElement('div');
+    const divPixel = document.createElement('div');
     divPixel.className = 'pixel';
     divPixel.style.backgroundColor = 'white';
     return divPixel;
   }
 
   function fillLine(boardSize) {
-    let pixelLine = document.createElement('div');
+    const pixelLine = document.createElement('div');
     for (let index = 1; index <= boardSize; index += 1) {
       pixelLine.appendChild(generatePixelsLine());
       pixelLine.className = 'pixel-line';
@@ -27,7 +26,7 @@ window.onload = function () {
     return pixelLine;
   }
   function fillBoard(boardSize) {
-    let pixelBoard = document.getElementById('pixel-board');
+    const pixelBoard = document.getElementById('pixel-board');
     pixelBoard.innerHTML = '';
     for (let index = 1; index <= boardSize; index += 1) {
       pixelBoard.appendChild(fillLine(boardSize));
@@ -36,11 +35,11 @@ window.onload = function () {
   fillBoard(5);
 
   function selectedColors() {
-    let colorsArray = document.querySelectorAll('.color');
+    const colorsArray = document.querySelectorAll('.color');
     colorsArray[0].className = 'color selected';
     for (let index = 0; index < colorsArray.length; index += 1) {
       colorsArray[index].addEventListener('click', function (event) {
-        let selectedColor = document.querySelector('.selected');
+        const selectedColor = document.querySelector('.selected');
         selectedColor.className = 'color';
         event.target.className = 'color selected';
       });
@@ -48,11 +47,11 @@ window.onload = function () {
   }
 
   function paintPixels() {
-    let pixelsArray = document.querySelectorAll('.pixel');
+    const pixelsArray = document.querySelectorAll('.pixel');
     for (let index = 0; index < pixelsArray.length; index += 1) {
       pixelsArray[index].addEventListener('click', function (event) {
-        let colorSelected = document.querySelector('.selected');
-        let color = getComputedStyle(colorSelected).backgroundColor;
+        const colorSelected = document.querySelector('.selected');
+        const color = getComputedStyle(colorSelected).backgroundColor;
         console.log(color);
         event.target.style.backgroundColor = color;
       });
@@ -60,8 +59,8 @@ window.onload = function () {
   }
 
   function buttonClear() {
-    let buttonClear = document.getElementById('clear-board');
-    let pixelsArray = document.querySelectorAll('.pixel');
+    const buttonClear = document.getElementById('clear-board');
+    const pixelsArray = document.querySelectorAll('.pixel');
     buttonClear.addEventListener('click', function () {
       for (let index = 0; index < pixelsArray.length; index += 1) {
         pixelsArray[index].style.backgroundColor = 'white';
@@ -70,8 +69,8 @@ window.onload = function () {
   }
 
   function generateNewBoard() {
-    let input = document.getElementById('board-size');
-    let buttonGenerate = document.getElementById('generate-board');
+    const input = document.getElementById('board-size');
+    const buttonGenerate = document.getElementById('generate-board');
     buttonGenerate.addEventListener('click', function () {
       let boardSize = input.value;
       if (boardSize.length !== 0) {
