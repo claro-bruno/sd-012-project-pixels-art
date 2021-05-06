051b902aee07ee6b355d611e99ad1449ae418fd6
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-05-06 13:52:20 -0300

    adiciona alterçao requisito 4

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 const pixelBoard = document.querySelector('#pixel-board');
-let elements = 5;
-
+const elements = 5;
 
 function pixelBox (linhas) {
     for (let file = 1; file <= linhas; file =+1); {
-        const boxIndex = document.createElement("box");
-        pixelBoard.appendChild(boxFile);
-        pixelBoxColumns(boxFile, linhas);
+      const boxFile = document.createElement('box');
+      pixelBoard.appendChild(boxFile);
+      pixelBoxColumns(boxFile, linhas);
     }
 }
 
 function pixelBoxColumns(file, colunas) {
     for (let column = 1; column <= colunas; column += 1); {
-    const boxColumn = document.createElement("box");
-    boxColumn.classList.add("pixel");
-    file.appendChild(boxColumn);
+      const boxColumn = document.createElement('box');
+      boxColumn.classList.add('pixel');
+      file.appendChild(boxColumn);
     }
 }
 pixelBox(elements);
