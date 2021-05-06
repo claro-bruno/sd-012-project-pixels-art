22150047a18f61f308379e4034fca5b9e71db3a7
Patricia Silvestre <patriciasilvestrea@gmail.com>
2021-05-06 19:03:07 -0300

    Alterei requisitos 3, 4, 5, e 6

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
-let colors = ['blue', 'pink', 'orange', 'green'];
+// requisito 1 esta no html
+// requisitos 2 e 3 esta no html e css 
+//requisito 4
+const quadroPai = document.getElementById('pixel-board');
 
-function geraCores() {
-    let getColorList = document.querySelector('#color-palette');
+const board = 5;
 
-    for(let index = 0; index < colors.length; index += 1) {
-        let color = colors[index];
-        let colorItem = document.createElement('li');
+for (let index = 0; index < board; index += 1) {
+  const newLine = document.createElement('tr');
+  newLine.classList.add('line');
+  quadroPai.appendChild(newLine);
 
-        if (color ==='blue'){
-            colorItem.className = 'color';
-            colorItem.style.backgroundColor = 'blue';
-            getColorList.appendChild(colorItem);
-        } else if (color === 'pink') {
-            colorItem.className = 'color';
-            colorItem.style.backgroundColor = 'pink';
-            getColorList.appendChild(colorItem);
-        } else if (color === 'orange') {
-            colorItem.className = 'color';
-            colorItem.style.backgroundColor = 'orange';
-            getColorList.appendChild(colorItem);
-        } else if (color === 'green') {
-            colorItem.className = 'color';
-            colorItem.style.backgroundColor = 'green';
-            getColorList.appendChild(colorItem);
-        }
-    };
-};
-geraCores();
+  for (let columns = 0; columns < board; columns += 1) {
+    const newColumn = document.createElement('td');
+    newColumn.classList.add('pixel');
+    newLine.appendChild(newColumn);
+  }
+}
+// requisito 4 feito baseado no repositorio de Adriana Biberg : adriana-biberg-pixels-art
+// requisito 5 esta no css
+// requisito 6 esta no html
 
-document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
-
-
-let linhasQuadroPixels = document.getElementsByClassName('linha-pixel');
-
-function quadroPixels() {
-
-    for (index = 0; index < linhasQuadroPixels.length; index += 1){ 
-        let linhaPixels = linhasQuadroPixels[index];
-        console.log(linhaPixels)
-        for (index2 = 0; index2 < linhasQuadroPixels.length; index2 += 1){ 
-            let pixel = document.createElement('div');
-            linhaPixels.appendChild(pixel);
-            pixel.className='pixel';
-            pixel.style.backgroundColor='white';
-        };
-    };
-};
-
-quadroPixels();
-// retirei esse código da aluna Camila Malvessi : camila-pereira-pixels-art//
-
-function letBlackColor(){
-    let corSelecionada = document.getElementsByClassName('color')[0];
-    corSelecionada.className = 'selected';
-};
-letBlackColor();
 
 
