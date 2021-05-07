1b4ee796c04ee09c0a525a2ddbc3a0f8d3929b74
Eurides <euridesalves@yahoo.com.br>
2021-05-07 16:35:12 -0300

    correcao do requisito 7

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 window.onload = function () {
   const corInicial = document.getElementById('color1');
   corInicial.className = 'color selected';
-  padronizaPalheta(corInicial);
-  // ddEventListener(p1,p2) onde: p1 e' o evento e p2 a funcao a ser executada
+   // ddEventListener(p1,p2) onde: p1 e' o evento e p2 a funcao a ser executada
   const paletaPreta = document.getElementById('color1');
-  paletaPreta.addEventListener('click', clicouCor(paletaPreta));
+  paletaPreta.addEventListener('click', clicouCor); 
   const paletaVermelha = document.getElementById('color2');
-  paletaVermelha.addEventListener('click', clicouCor(paletaVermelha));
+  paletaVermelha.addEventListener('click', clicouCor);
   const paletaAzul = document.getElementById('color3');
-  paletaAzul.addEventListener('click', clicouCor(paletaAzul));
+  paletaAzul.addEventListener('click', clicouCor);
   const paletaVerde = document.getElementById('color4');
-  paletaVerde.addEventListener('click', clicouCor(paletaVerde));
+  paletaVerde.addEventListener('click', clicouCor);
 }; 
 
-function paletaPadrao() {
-  console.log("Aqui");
-}
-
-function clicouCor(corEscolhida) {
-    console.log("a cor escolhida :"+corEscolhida);
-  let CorAntiga = document.getElementsByClassName('selected');
-  /* const idCorAntiga = */
+function clicouCor(paletaEscolhida) {
+  const corAntiga = document.getElementsByClassName('selected')[0];
+  corAntiga.className = 'color';
+  const corNova = document.getElementById(paletaEscolhida.target.id);
+  corNova.className = 'color selected';
 };
 
+/* 
+  cont cor = document.querySelector9(#palete)
+cor.addeventlistener('click, (event) => {
+  if (event.target.classname === 'color') {
+   corselec.classlist.remove(selected);
+   event.target.classlist.add(selected)
+document.getElementsByClassName("selected")[0]
+  const corAntiga = document.getElementsByClassName('color selected');
+  const teste = document.getElementById(corAntiga);
+  console.log(teste);
+  console.log(corAntiga);
+  /* document.getElementById("MyElement").className = "color"; */
+  /* corAntiga.className = 'color';
+  console.log(corAntiga);
+   */
+ 
