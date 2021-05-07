1c3afd2e91e798ba4ab239100f50f9fc41b77402
Martin <escorpmartin97@gmail.com>
2021-05-07 15:23:46 -0300

    trabalha js

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ function criaLinha () {
         pixeles.appendChild(linha);
             }
         }
-        criaLinha();
-
+       
 function criaPixel () {
     let line = document.getElementsByClassName("pixel-line");
-    for (let index = 0; index < line.length; index += 1) {
-    let quadro = document.createElement("div");
-    quadro.className = "pixel";
-    console.log(quadro)
-        if (index < line) {
-            criaLinha();
+    for (let index1 = 0; index1 < line.length; index1 += 1) {
+        if (index1 < 5) {
+        for (let index2 = 0; index2 < line.length; index2 += 1) {
+        let quadro = document.createElement("div");
+        quadro.className = "pixel";
+        line[index2].appendChild(quadro);
+            }
         }
     }
 }
-    criaPixel ();
+
+criaLinha();
+criaPixel();
