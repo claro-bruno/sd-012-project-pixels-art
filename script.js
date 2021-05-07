b1bc769cd52df584b89722291e627f095feea2be
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-07 16:24:36 -0300

    add requisito 8

--- script.js
+++ script.js
@@ -10,11 +10,24 @@ window.onload = firstColor
     const color = document.getElementsByClassName('color');
     for (let i = 0; i < color.length; i += 1) {
         let corSelecionada = color[i];
-        corSelecionada.addEventListener('click', function () {
+        corSelecionada.addEventListener('click', 
+        function() {
             const ultima = document.querySelector('.selected');
             ultima.classList.remove('selected');
             corSelecionada.className = 'color selected';
         })
     }
+    
+    const pixel = document.getElementsByClassName('pixel');
+    const cor = document.getElementsByClassName('selected');
+    function recebeClick() {
+        for (let index = 0; index < pixel.length; index +=1) {
+            pixel[i].addEventListener("click",function(){
+                if(pixel[i].style.backgroundColor != cor.style.backgroundColor) {
+                pixel[i].style.backgroundColor = cor.style.backgroundColor
+                }
+            });
+        }
+    }
+    recebeClick();
 
-  
