3e420992640e12f81d285424f3e404fc54bb3d55
Martin <escorpmartin97@gmail.com>
2021-05-07 00:07:55 -0300

    js

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let pixeles = document.querySelector("#pixel-board");
 
-    let section = document.querySelector('.pixel');
-    console.log(section)
-    
-    function pixel (section) {
-        for (let index = 0; index < 26; index += 1) {
-       
+function criaLinha () {
+    for (let index = 0; index < 5; index += 1) {
+        let linha = document.createElement("div");
+        linha.className = "pixel-line";
+        pixeles.appendChild(linha);
+            }
+        }
+        criaLinha();
+
+function criaPixel () {
+    let line = document.getElementsByClassName("pixel-line");
+    for (let index = 0; index < line.length; index += 1) {
+    let quadro = document.createElement("div");
+    quadro.className = "pixel";
+    console.log(quadro)
+        if (index < line) {
+            criaLinha();
         }
     }
+}
+    criaPixel ();
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,10 @@
-  let section = document.querySelector('.pixel');
-    console.log(section)
+let  pixeles = document.querySelector('#pixel-board');
+
+function criaPixel () {
+    for (let index = 0; index < 5; index += 1) {
+    let quadro = document.createElement("div");
+    quadro[index].className = "pixel";
+    
+    pixeles.appendChild(quadro)
+        }
+    }
