19b6d2845cbeb7c87881751678291ce0c23f3686
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 18:03:48 -0300

    Elementos em uma mesma ul req6

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let pixBoard = document.getElementById("pixel-board");
 for (let indice1 = 0; indice1 < 5; indice1 += 1) {
   for (let indice2 = 0; indice2 < 5; indice2 += 1) {
     let pixUnity = document.createElement("li");
-    pixBoard.appendChild(pixUnity);; 
+    pixBoard.appendChild(pixUnity);
+    let caughtPix = document.getElementById("pixel-board");
+    caughtPix.children[indice2].className = "pixel"; 
 }
-document.getElementById("pixel-board").children.value = "\n";
+document.getElementById("pixel-board").children[indice1] = "\n";
 }
