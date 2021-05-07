7210cd48ba5d19f5f23b1bdee2a8f9030c2d45f2
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-06 21:23:57 -0300

    ajustes do lint e requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function grid(pixelLine) {
   const pixels = document.getElementById('pixel-board');
-  for(let index = 0; index < pixelLine; index += 1) {
+  for (let index = 0; index < pixelLine; index += 1) {
     const createDivLine = document.createElement('tr');
     createDivLine.classList.add('line');
     pixels.appendChild(createDivLine);
 
-    for(let indexcolumn = 0; indexcolumn < pixelLine; indexcolumn += 1 ) {
+    for (let indexcolumn = 0; indexcolumn < pixelLine; indexcolumn += 1) {
       const createDivColumn = document.createElement('td');
       createDivColumn.classList.add('pixel');
       createDivLine.appendChild(createDivColumn);
@@ -15,12 +15,27 @@ function grid(pixelLine) {
 const pixelLine = 5;
 grid(pixelLine);
 
-let elementcolor = document.getElementsByClassName('color');
+const elementColor = document.getElementsByClassName('color');
 function colorSelect(event) {
-  let select = document.querySelector('.selected')
+  const select = document.querySelector('.selected');
   select.classList.remove('selected');
   event.target.classList.add('selected');
 }
-  for(let index = 0; index < elementcolor.length; index += 1) {
-    elementcolor[index].addEventListener('click', colorSelect);
-} 
+for (let index = 0; index < elementColor.length; index += 1) {
+  elementColor[index].addEventListener('click', colorSelect);
+}
+
+const black = document.getElementById('black').style.backgroundColor = 'black';
+const red = document.getElementById('red').style.backgroundColor = '#FF0000';
+const blue = document.getElementById('blue').style.backgroundColor = '#0000FF';
+const green = document.getElementById('green').style.backgroundColor = '#008000';
+
+const pixed = document.querySelectorAll('.pixel');
+function paintPixel(eventPaint) {
+  const paint = document.querySelector('.selected');
+  eventPaint.target.style.backgroundColor = paint.style.backgroundColor;
+}
+
+for (let indexPixels = 0; indexPixels < pixed.length; indexPixels += 1) {
+  pixed[indexPixels].addEventListener('click', paintPixel);
+}
