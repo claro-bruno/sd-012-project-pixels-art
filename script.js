fba016aaadf7908e065ebfe27a51b228a3fd1ccf
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-06 22:47:42 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const colorPalette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+
+window.onload = initialColor;
+
+function initialColor () {
+    let colorBlack = document.querySelector('.color');
+    colorBlack.className = 'color selected';
+}
