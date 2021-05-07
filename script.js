840a5c0b16ac44d9f79e896da2479d22437bbc9b
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-07 13:58:11 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+window.onload = function() {
 
+function adicionaLinha(line) {
+  for (let index = 0; index < line; index += 1) {
+    let divLine = document.createElement('div');
+    divLine.className = 'linha';
+    bordaPixel.appendChild(divLine);
+  };
+};
+
+
+function adicionaColuna (coluna) {
+  let numColunas = document.getElementsByClassName('linha');
+    for (let colunas of numColunas) {
+    for ( let index = 0; index < coluna; index += 1) {
+  let divColuna = document.createElement('div');
+      divColuna.className = 'pixel';
+      colunas.appendChild(divColuna);
+    };
+  };
+};
+let linha = 5;
+let coluna = 5;
+const bordaPixel = document.getElementById('pixel-board')
+adicionaLinha(linha);
+adicionaColuna(coluna);
+
+const cores = document.querySelectorAll('.color');
+
+function selecionaCor(event) {
+  const seleciona = document.querySelector('.selected');
+  seleciona.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+for (let index = 0; index < cores.length; index += 1) {
+  cores[index].addEventListener('click', selecionaCor);
+}
+};
