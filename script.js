a6c9e9db792de280b07ed1453f65080b439198ca
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-07 16:59:09 -0300

    Fazendo pequenos ajustes, resolvendo requisito 10

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-window.onload = function () {
-  let section1 = document.createElement('section');
+window.onload = function() {
+  const section1 = document.createElement('section');
   section1.id = 'color-palette';
 
-  let text = document.createElement('h3');
+  const text = document.createElement('h3');
   text.innerHTML = 'Insira a quantidade de linhas x colunas desejadas';
 
-  let input = document.createElement('input');
+  const input = document.createElement('input');
   input.id = 'board-size';
   input.type = 'number';
+  input.min = 0;
 
-  let btn2 = document.createElement('button');
+  const btn2 = document.createElement('button');
   btn2.id = 'generate-board';
   btn2.innerHTML = 'VQV';
 
-  let btn = document.createElement('button');
+  const btn = document.createElement('button');
   btn.id = 'clear-board';
   btn.innerHTML = 'Limpar';
 
-  let section2 = document.createElement('section');
+  const section2 = document.createElement('section');
   section2.id = 'pixel-board';
 
-  let main = document.querySelector('main');
+  const main = document.querySelector('main');
   main.appendChild(section1);
   main.appendChild(text);
   main.appendChild(input);
@@ -28,22 +29,25 @@ window.onload = function () {
   main.appendChild(btn);
   main.appendChild(section2);
 
-  let color_palette = document.querySelector('#color-palette');
-  let pixel_board = document.querySelector('#pixel-board');
+  const color_palette = document.querySelector('#color-palette');
+  const pixel_board = document.querySelector('#pixel-board');
 
-  let buttonVQV = document.querySelector('#generate-board');
+  const buttonVQV = document.querySelector('#generate-board');
 
   function clearBox () {
-    let box = document.querySelectorAll('.line');
+    const box = document.querySelectorAll('.line');
     for (let index = 0; index < box.length; index += 1) {
       pixel_board.removeChild(box[index]);
     }
   }
-
+  
   tamanho(5);
   buttonVQV.addEventListener('click', function () {
     clearBox();
     let num = document.querySelector('#board-size').value;
+    if (num === '' || num === ' ' || num < 0) {
+      alert('Board inválido!');
+    }
     if (num < 5) {
       num = 5;
     }
@@ -52,35 +56,36 @@ window.onload = function () {
     }
     document.querySelector('#board-size').value = '';
     tamanho(num);
+    getColor();
   });
 
-  function makeColor (quantity) {
+  function makeColor(quantity) {
     for(let index = 0; index < quantity; index += 1) {
-      let color = document.createElement('div');
+      const color = document.createElement('div');
       color.className = 'color';
       defineColors(index, color);
       color_palette.appendChild(color);
-    };
+    }
   }
   makeColor(4); // quantidades de cores geradas
 
-  function defineColors (index, color) {
+  function defineColors(index, color) {
     if (index !== 0) {
       color.style.backgroundColor = colorGeneration();
     }
     else {
       color.style.backgroundColor = 'black';
-    };
+    }
   }
   
-  function colorGeneration () {
+  function colorGeneration() {
     return '#' + parseInt((Math.random() * 0xFFFFFF))
     .toString(16)
     .padStart(6, '0');
   }
 
-  function makeDivs (tam) {
-    let pixel = document.createElement('div');
+  function makeDivs(tam) {
+    const pixel = document.createElement('div');
     pixel.className = 'pixel';
     if (tam >= 30) {
       pixel.style.height = '22px';
@@ -89,7 +94,7 @@ window.onload = function () {
     return pixel;
   }
 
-  function aux (num1, num2, num3) {
+  function aux(num1, num2, num3) {
     if (num1 < num2) {
       for (let index = 0; index < num3.length; index += 1) {
         num3[index].appendChild(makeDivs(num2));
@@ -97,64 +102,64 @@ window.onload = function () {
     }
   }
 
-  function makeBox (tam) {
-    let ln = document.querySelectorAll('.line');
+  function makeBox(tam) {
+    const ln = document.querySelectorAll('.line');
     for ( let index = 0; index < ln.length; index += 1) {
        aux(index, tam, ln);
     }
   }
 
-  function pixelLine (){
-    let lines = document.createElement('div');
+  function pixelLine() {
+    const lines = document.createElement('div');
     lines.className = 'line';
     return lines;
   }
 
-  function makeLine (tam) {
+  function makeLine(tam) {
     for (let index = 0; index < tam; index += 1) {
-        pixel_board.appendChild(pixelLine());
+      pixel_board.appendChild(pixelLine());
     }
   }
 
-  function tamanho (size) {
+  function tamanho(size) {
     makeLine(size)
     makeBox(size)
   }
 
-  function selecAux (param1) {
-    let rem = document.querySelector('.selected');
+  function selecAux(param1) {
+    const rem = document.querySelector('.selected');
     rem.classList.remove('selected');
     param1.target.className = 'color selected';
   }
 
-  function selec () {
+  function selec() {
     for (let index = 0; index < colors.length; index += 1) {
       colors[index].addEventListener('click', selecAux);
     }
   }
 
-  let colors = document.querySelectorAll('.color');
+  const colors = document.querySelectorAll('.color');
   colors[0].className = 'color selected';
-  selec()
+  selec();
 
-  function getColor () {
+  function getColor() {
     let pixels = document.querySelectorAll('.pixel');
     for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].addEventListener('click', inputColor);
     }
   }
-  getColor()
 
-  function inputColor (param1) {
+  function inputColor(param1) {
     let cor = document.querySelector('.selected').style.backgroundColor;
     param1.target.style.backgroundColor = cor;
   }
 
-  let button = document.querySelector('#clear-board');
+  const button = document.querySelector('#clear-board');
   button.addEventListener('click', function () {
-    let pixels = document.querySelectorAll('.pixel');
+    const pixels = document.querySelectorAll('.pixel');
     for(let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'white';
     }
   }); 
+  getColor();
 }
