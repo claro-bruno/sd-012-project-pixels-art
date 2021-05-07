4243f26a3854cb5678ed5879593ba4a13a0e876e
Victor Veloso <victorveloso13@gmail.com>
2021-05-07 13:55:52 -0300

    Adiciona um quadro de pixels

---
+++ script.js
@@ -0,0 +1,22 @@
+function createPixel (size) {
+    const table = document.querySelector('#pixel-board');
+    table.innerHTML = ''
+		for (let line = 0; line < size; line += 1) {
+			const linePixels = document.createElement('div');	
+		
+		for (let column = 0; column < size; column += 1) {
+			const columnPixels = document.createElement('div');
+			
+			columnPixels.className = 'pixel';
+			linePixels.className = 'pixel-line';
+			linePixels.appendChild(columnPixels);
+		};
+
+		table.appendChild(linePixels);
+
+		};
+
+};
+
+createPixel(5);
+
