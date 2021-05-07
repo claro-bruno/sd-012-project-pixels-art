b858417b4ba2a465e62756f93045f1fb289eb6f1
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 18:00:58 -0300

    Criaçao dos elementos em uma mesma ul req 6

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ let pixBoard = document.getElementById("pixel-board");
 for (let indice1 = 0; indice1 < 5; indice1 += 1) {
   for (let indice2 = 0; indice2 < 5; indice2 += 1) {
     let pixUnity = document.createElement("li");
-    pixBoard.appendChild(pixUnity);
-    let caughtPix = document.getElementById("pixel-board");
-    caughtPix.children[indice2].className = "pixel"; 
+    pixBoard.appendChild(pixUnity);; 
 }
- pixBoard.value = "\n";
+document.getElementById("pixel-board").children.value = "\n";
 }
