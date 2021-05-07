7ef7338ca1051658145d4cb97dd88f447fa4b936
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-07 16:15:42 -0300

    Satisfaz requisito 11

--- script.js
+++ script.js
@@ -39,22 +39,30 @@ function genValidTable(number) {
   for (let i = 0; i < number; i += 1) {
     const tr = document.createElement('tr');
     for (let j = 0; j < number; j += 1) {
-      const th = document.createElement('th');
-      th.className = 'pixel';
-      tr.appendChild(th);
+      const td = document.createElement('td');
+      td.className = 'pixel';
+      td.addEventListener('click', paint);
+      tr.appendChild(td);
     }
     table.appendChild(tr);
   }
 }
 
 function genTable() {
-  const number = document.getElementById('board-size').value;
+  let number = document.getElementById('board-size').value;
 
   if (number === '') {
     alert('Board inválido!');
+  } else if (number < 5) {
+    number = 5;
+    genValidTable(number);
+  } else if (number > 50) {
+    number = 50;
+    genValidTable(number);
   } else {
     genValidTable(number);
   }
+  console.log(number);
 }
 
 const buttonGen = document.getElementById('generate-board');
