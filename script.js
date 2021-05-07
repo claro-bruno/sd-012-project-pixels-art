35313bc7886b4e2be76ad537ea86b90d47cf105b
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-07 16:39:50 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colorPalette = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+const pixels = document.querySelectorAll('.pixel');
 
 let colorBlack = document.querySelectorAll('.color')[0];
 let colorAqua = document.querySelectorAll('.color')[1];
@@ -18,12 +19,19 @@ colorBlueViolet.addEventListener('click', changeColor);
 colorChartreuse.addEventListener('click', changeColor);
 colorBlack.addEventListener('click', changeColor);
 
-
-
-
 window.onload = initialColor;
 
 function initialColor () {
     let colorBlack = document.querySelector('.color');
     colorBlack.className = 'color selected';
+}
+
+// pixel elemento especifico
+for (const pixel of pixels) {
+    pixel.addEventListener('click', function (event){
+        const selected = document.querySelector('.selected');
+        let selectedColor = window.getComputedStyle(selected).getPropertyValue('background-color');
+        event.target.style.backgroundColor = selectedColor;    
+
+    })
 }
