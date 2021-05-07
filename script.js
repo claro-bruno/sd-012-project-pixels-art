5d390c6cb8f9e36cb3eaeadafca66c7cc5f65ca1
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-07 18:29:06 -0300

    Requesito 8

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function adicionaLinha(line) {
 };
 
 
-function adicionaColuna (coluna) {
+function adicionaColuna(coluna) {
   let numColunas = document.getElementsByClassName('linha');
     for (let colunas of numColunas) {
     for ( let index = 0; index < coluna; index += 1) {
@@ -26,7 +26,13 @@ adicionaLinha(linha);
 adicionaColuna(coluna);
 
 const cores = document.querySelectorAll('.color');
-
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('pixel')) {
+    const selected = document.querySelector('.selected');
+    const corSelecionada = selected.style.backgroundColor;
+    event.target.style.backgroundColor = corSelecionada;
+  }
+})
 function selecionaCor(event) {
   const seleciona = document.querySelector('.selected');
   seleciona.classList.remove('selected');
@@ -35,5 +41,16 @@ function selecionaCor(event) {
 for (let index = 0; index < cores.length; index += 1) {
   cores[index].addEventListener('click', selecionaCor);
 }
-};
 
+}
+
+function botaoLimpar () {
+  let botao = document.getElementById('.clear-board')
+  botao.addEventListener('click', botao)
+}
+document.querySelectorAll('.pixel')
+function limparPixel () {
+  for (linha of pixel) {
+  linha.style.backgroundColor = 'white'
+  }
+}
