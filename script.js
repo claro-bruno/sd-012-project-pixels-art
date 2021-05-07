43e098cb8c8214fe934e4dacb280c170e67e6f2a
Patricia Silvestre <patriciasilvestrea@gmail.com>
2021-05-07 11:56:18 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // requisito 1 esta no html
 // requisitos 2 e 3 esta no html e css 
 //requisito 4
-const quadroPai = document.getElementById('pixel-board');
+let getPixelBoard = document.getElementById('pixel-board');
 
-const board = 5;
+let quantidadeDeQuadros = 5;
 
-for (let index = 0; index < board; index += 1) {
-  const newLine = document.createElement('tr');
+for (let index = 0; index < quantidadeDeQuadros; index += 1) {
+  let newLine = document.createElement('tr');
   newLine.classList.add('line');
-  quadroPai.appendChild(newLine);
+  getPixelBoard.appendChild(newLine);
 
-  for (let columns = 0; columns < board; columns += 1) {
-    const newColumn = document.createElement('td');
+  for (let columns = 0; columns < quantidadeDeQuadros; columns += 1) {
+    let newColumn = document.createElement('td');
     newColumn.classList.add('pixel');
     newLine.appendChild(newColumn);
   }
@@ -23,17 +23,32 @@ for (let index = 0; index < board; index += 1) {
 
 //requisito 7
 function selected(event) {
-  const removeSelection = document.querySelector('.selected');
+  let removeSelection = document.querySelector('.selected');
   removeSelection.classList.remove('selected');
   event.target.classList.add('selected');
 }
 function selectedEvent() {
-  const paleta = document.getElementsByClassName('color');
-  for (let index = 0; index < paleta.length; index += 1) {
-    paleta[index].addEventListener('click', selected);
+  let paletaDeCores = document.getElementsByClassName('color');
+  for (let index = 0; index < paletaDeCores.length; index += 1) {
+    paletaDeCores[index].addEventListener('click', selected);
   }
 }
 selectedEvent();
 
-// tive que consultar o repositório de Caio-takeshi:caio-takeshi-pixels-art, para me ajudar na conclusao do requisito
+// tive que consultar o repositório de Caio-takeshi:caio-takeshi-pixels-art, para me ajudar na conclusao do requisito 7
   
+//requisito 8
+function pegaCor(event) {
+  let selecionaCor = document.querySelector('.select').style.backgroundColor;
+  event.target.style.backgroundColor = selecionaCor;
+}
+
+function mudaDeCor() {
+ let getPixel = documento.getElementsByClassName('pixel');
+
+ for(let index = 0; index < getPixel.length; index += 1) {
+   getPixel[index].addEventListener('click', mudaDeCor);
+       
+   }
+  }
+  // vi essa a dica  de usar o evento.target postada no slack pelo Nuwanda
