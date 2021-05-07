be636a35ff4e261c38ab537391bfb30f180da8a4
Waltton Coelho <60485084+walttoncoelho@users.noreply.github.com>
2021-05-07 18:48:11 -0300

    Refatorando

--- script.js
+++ script.js
@@ -2,65 +2,52 @@
 
 const pixelBox = document.querySelector('#pixel-board');
 
-
 // Gerando cores do Palette
 const colorsOne = document.querySelector('#palette-one');
-colorsOne.style.backgroundColor = 'black'
+colorsOne.style.backgroundColor = 'black';
 const colorsTwo = document.querySelector('#palette-two');
-colorsTwo.style.backgroundColor = 'green'
+colorsTwo.style.backgroundColor = 'green';
 const colorsThree = document.querySelector('#palette-three');
-colorsThree.style.backgroundColor = 'orange'
+colorsThree.style.backgroundColor = 'orange';
 const colorsFour = document.querySelector('#palette-four');
-colorsFour.style.backgroundColor = 'purple'
+colorsFour.style.backgroundColor = 'purple';
 // <==
-
-
 // Gera as Colunas
 function createCollumns(row, colunas) {
-
   for (let column = 1; column <= colunas; column += 1) {
     const divColumn = document.createElement('div');
     divColumn.classList.add('pixel');
     row.appendChild(divColumn);
-
   }
+
 }
 // Gera as linhas
-function createBoardRows(linhas) {
-
-  for (let row = 1; row <= linhas; row += 1) {
+function createBoardRows(lines) {
+  for (let row = 1; row <= lines; row += 1) {
     const divRow = document.createElement('div');
     divRow.classList.add('tr');
     pixelBox.appendChild(divRow);
-    createCollumns(divRow, linhas);
+    createCollumns(divRow, lines);
   }
-
 }
+
 createBoardRows(5);
 
-
-
-
-// Seleciona um pixel
-const palette = document.querySelector('#color-palette')
-
-function select() {
-  palette.addEventListener('click', function (event) {
-    let beforeColor = document.querySelector('.selected')
-    if (event.target.id !== 'color-palette') {
-      beforeColor.classList.remove('selected')
-      event.target.classList.add('selected');
-    }
-  });
-}
-select();
-// <==
-
+const palette = document.querySelector('#color-palette');
+const clear = document.getElementById('clear-board');
+const getPixel = document.querySelectorAll('.pixel');
 
 pixelBox.addEventListener('click', function (event) {
 
-  let toColor = document.querySelector('.selected').id;
+  let fillPixel = document.querySelector('.selected').id;
   if (event.target.className === 'pixel') {
-    event.target.style.backgroundColor = toColor;
+    event.target.style.backgroundColor = fillPixel;
+  }
+});
+
+
+clear.addEventListener('click', function () {
+  for (let index = 0; index < getPixel.length; index += 1) {
+    getPixel[index].style.backgroundColor = 'white'
   }
 });
