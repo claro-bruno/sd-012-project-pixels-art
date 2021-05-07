688add30a2ccafe6a0d8aec196d9bbe46a692147
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-07 18:19:38 -0300

    Itens 5 finalizado

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const quadroPixel = document.querySelector('#pixel-board');
-const pixel = '<div id="pixel-board"></div>';
+const pixel = document.getElementById('#pixel-board');
 const colores = document.querySelectorAll('.color');
 
-function criaQuadro(tamanho) {
-    for (let index = 0; index < tamanho; index += 1) {
-        quadroDiv.innerHTML += pixel;
-    }
-}       
+function criaLinhas(linhas) {
+    for (let index = 0; index < linhas; index += 1) {
+        const linhaDiv = document.createElement('div');
+        linhaDiv.classList.add('linha');
+        document.getElementById('pixel-board').appendChild(linhaDiv);
+    };   
+};
+criaLinhas(5);
+
+function criaColunas(colunas) {
+    let linhas = document.querySelectorAll('.linha');
+    for (let linha of linhas) {
+        for (let index = 0; index < colunas; index += 1) {
+            const colunaDiv = document.createElement('div');
+            colunaDiv.classList.add('pixel');
+            linha.appendChild(colunaDiv);
+        };
+    };
+};
+criaColunas(5);
