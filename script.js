e1fc74385b12013a67aed4b7d06b7d6ccb14e005
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-07 15:33:51 -0300

    Adiciona background color a classe pixel no css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let PALETTE_COLOR = ['black', 'green', 'blue', 'orange'];
+const PALETTE_COLOR = ['black', 'green', 'blue', 'orange'];
 
 function addColoPalette() {
   for (let index = 0; index < PALETTE_COLOR.length; index += 1) {
-    let colorPalette = document.createElement('div');
+    const colorPalette = document.createElement('div');
     colorPalette.classList.add('color');
     colorPalette.style.backgroundColor = PALETTE_COLOR[index];
     document.querySelector('#color-palette').appendChild(colorPalette);
@@ -11,22 +11,23 @@ function addColoPalette() {
 
 addColoPalette();
 
-function createPixelBoard(line, column) {
-  for (let lineIndex = 0; lineIndex < line; lineIndex += 1) {
-    let linePixel = document.createElement('div');
-    linePixel.classList.add('line');
-    document.querySelector('#pixel-board').appendChild(linePixel);
-    for (let columnPixel = 0; columnPixel < column; columnPixel += 1){
-      let element = document.querySelectorAll('.line');
-      addPixel(element[lineIndex]);
-    }
-  }
-}
 
 function addPixel(element) {
   let pixel = document.createElement('div');
   pixel.classList.add('pixel');
   element.appendChild(pixel);
+}
+
+function createPixelBoard(line, column) {
+  for (let lineIndex = 0; lineIndex < line; lineIndex += 1) {
+    const linePixel = document.createElement('div');
+    linePixel.classList.add('line');
+    document.querySelector('#pixel-board').appendChild(linePixel);
+    for (let columnPixel = 0; columnPixel < column; columnPixel += 1) {
+      const element = document.querySelectorAll('.line');
+      addPixel(element[lineIndex]);
+    }
+  }
 }
 
 createPixelBoard(5,5);
