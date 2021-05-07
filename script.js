a64af6d82bcc18ba88c3a001dd57e3ad325e2f49
Gabriel Viana <g.viana87@hotmail.com>
2021-05-07 12:13:27 -0300

    Function color-palette comentada

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let pixelBoard = document.getElementById('pixel-board');
+// const colorPalette = document.getElementById('color-palete');
+const pixelBoard = document.getElementById('pixel-board');
+const pixels = document.getElementsByClassName('pixel');
 
-for (let linha = 0; linha < 25; linha += 1){
-    let board = document.createElement('div');
-    board.className = 'pixel';
-    pixelBoard.appendChild(board)
+// function paletteColor (n) {
+//     for (let cont = 0; cont < n; cont += 1) {
+//         let color = document.createElement('div');
+//         color.className = 'color';
+//         color.id = 'c' + [cont];
+//         colorPalette.appendChild(color);
+//     };
+// };
+// // paletteColor(4);
+
+
+function boardPixels (n) {
+    for (let cont = 0; cont < n**2; cont += 1){
+        let board = document.createElement('div');
+        board.className = 'pixel';
+        board.id = 'p' + [cont];
+        pixelBoard.appendChild(board);
+    };
 };
+boardPixels(5);
+
+
+//window.onload = alerta
+//function alerta(){
+//    alert('Oba! Vamos colorir.');
+//};
+
+// let desfazer = document.getElementById('undo-button');
+// desfazer.addEventListener('click', undoButton);
+// function undoButton(eventoDeOrigem) {
+// console.log(eventoDeOrigem.target);
+// console.log(eventoDeOrigem.type);
+// }
