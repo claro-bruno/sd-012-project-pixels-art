bb955f661ef1d460b3abcc94a8c70a3d8add7c91
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-07 15:18:48 -0300

    Adiciona requisito 4

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function addColoPalette() {
 }
 
 addColoPalette();
+
+function createPixelBoard(line, column) {
+  for (let lineIndex = 0; lineIndex < line; lineIndex += 1) {
+    let linePixel = document.createElement('div');
+    linePixel.classList.add('line');
+    document.querySelector('#pixel-board').appendChild(linePixel);
+    for (let columnPixel = 0; columnPixel < column; columnPixel += 1){
+      let element = document.querySelectorAll('.line');
+      addPixel(element[lineIndex]);
+    }
+  }
+}
+
+function addPixel(element) {
+  let pixel = document.createElement('div');
+  pixel.classList.add('pixel');
+  element.appendChild(pixel);
+}
+
+createPixelBoard(5,5);
