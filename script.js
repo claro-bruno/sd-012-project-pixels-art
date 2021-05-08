cab14b7fa8d86871952512f4a3d37d9f68fd7a3f
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-08 16:25:34 -0300

    Tentativa de resoluçao do req 4

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function pallet(palletNumber) {
   let pixBoard = document.getElementById("pixel-board");
   for (indice1 = 0; indice1 < palletNumber; indice1 += 1) {
-    let bornLine = document.createElement("div");
-    pixBoard.appendChild(bornLine);
-    pixBoard.children[indice1].className = "linha";
+    let bornColumn = document.createElement("div");
+    pixBoard.appendChild(bornColumn);
+    pixBoard.children[indice1].className = "coluna";
     for (let indice2 = 0; indice2 < palletNumber; indice2 += 1) {
-      let bornColumn = document.createElement("div");
-      let memberLine = pixBoard.children[indice1];
-      memberLine.appendChild(bornColumn);
+      let bornLine = document.createElement("div");
+      let memberColumn = pixBoard.children[indice1];
+      memberColumn.appendChild(bornLine);
       pixBoard.children[indice1].children[indice2].className = "pixel";
     }
   }
