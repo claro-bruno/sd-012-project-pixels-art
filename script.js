eab299e5d42abd98c1e13c811cc0e9d5459002ca
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-08 20:43:44 -0300

    Retirada do inline-block e manutençao do float; problema da width da pixel-board resolvido

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 function pallet(palletNumber) {
   let pixBoard = document.getElementById("pixel-board");
-  for (indice1 = 0; indice1 < palletNumber; indice1 += 1) {
-    let bornLine = document.createElement("div");
-    pixBoard.appendChild(bornLine);
-    pixBoard.children[indice1].className = "linha";
-    for (let indice2 = 0; indice2 < palletNumber; indice2 += 1) {
-      let bornColumn = document.createElement("div");
-      let memberLine = pixBoard.children[indice1];
-      memberLine.appendChild(bornColumn);
-      pixBoard.children[indice1].children[indice2].className = "pixel";
-    }
+  let allPixels = palletNumber * palletNumber;
+  for (indice1 = 0; indice1 < allPixels; indice1 += 1) {
+    let bornPixel = document.createElement("div");
+    pixBoard.appendChild(bornPixel);
+    pixBoard.children[indice1].className = "pixel";
   }
 }
 pallet(5);
