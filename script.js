dc7c4c7fd2023987c947ac7836b1eb9f6c3726c7
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-05-07 23:17:19 -0300

    Melhorias do lint, nao todas, adicionando do input e o botao sem colocar as funcionalidades

--- script.js
+++ script.js
@@ -1,89 +1,90 @@
-function cores(){
-let cores = document.querySelectorAll('.color');
-let arraycolor = ['rgb(0, 0, 0)','rgb(250, 240, 202)','rgb(244, 211, 94)','rgb(238, 150, 75)'];
-for(let index =0; index < cores.length;index +=1){
-cores[index].style.backgroundColor = arraycolor[index];
+function cores() {
+  let cores = document.querySelectorAll('.color');
+  let arraycolor = ['rgb(0, 0, 0)','rgb(250, 240, 202)','rgb(244, 211, 94)','rgb(238, 150, 75)'];
+  for(let index =0; index < cores.length;index +=1) {
+  cores[index].style.backgroundColor = arraycolor[index];
 };
 };
 cores();
 
-function quadroPixel(){
-  let quadro = document.querySelector('#pixel-board')
+function quadroPixel() {
+  let quadro = document.querySelector('#pixel-board');
   
-  for(let linha = 1; linha <= 5;linha +=1){
+  for(let linha = 1; linha <= 5; linha +=1) {
   let quadrinhoLinha = document.createElement('tr');
 
-      for(let coluna =1 ; coluna <= 5 ; coluna +=1){
+      for(let coluna =1 ; coluna <= 5 ; coluna +=1) {
           let quadrinhoData = document.createElement('td');
           quadrinhoData.className = 'pixel';
-          quadrinhoLinha.appendChild(quadrinhoData)
+          quadrinhoLinha.appendChild(quadrinhoData);
       }
-      quadro.appendChild(quadrinhoLinha)
+      quadro.appendChild(quadrinhoLinha);
   };
 };
 quadroPixel();
 
-function corInicial(){
-//adiciona a nova classe das classes color
-let cor = document.querySelector('.color');
-cor.className += ' selected';
+function corInicial() {
+
+  let cor = document.querySelector('.color');
+  cor.className += ' selected';
 }
 corInicial();
 
-function trocaClass(){
+function trocaClass() {
 
-let cor = document.querySelectorAll('.color');
+  let cor = document.querySelectorAll('.color');
 
-for(let i =0;i < cor.length;i+=1){
+  for(let i =0;i < cor.length;i+=1) {
+    cor[i].addEventListener('click', clickCor);
+  }
+}
+function clickCor() {
   let color = 'color';
   let select = 'color selected';
-  cor[i].addEventListener('click', function(){
-    let selecao = document.querySelector('.selected');
-    selecao.className = color;
-    cor[i].className = select;
-  })
-}
+  let selecao = document.querySelector('.selected');
+  selecao.className = color;
+  cor[i].className = select;
 }
 trocaClass();
 
-function colorir(){
+function colorir() {
 
   let pixel= document.querySelectorAll('.pixel');
 
- for(let i=0;i<pixel.length;i+=1){
+  for(let i=0;i<pixel.length;i+=1) {
    
-   pixel[i].addEventListener('click', function(){
-     let itemCor = document.querySelector('.selected') ;
-     let arraycolor = ['rgb(0, 0, 0)','rgb(250, 240, 202)','rgb(244, 211, 94)','rgb(238, 150, 75)'];
-       switch(pixel[i].style.backgroundColor){
+    pixel[i].addEventListener('click', function() {
+      let itemCor = document.querySelector('.selected') ;
+      let arraycolor = ['rgb(0, 0, 0)','rgb(250, 240, 202)','rgb(244, 211, 94)','rgb(238, 150, 75)'];
+        switch(pixel[i].style.backgroundColor) {
                   case arraycolor[0]:
-         pixel[i].style.backgroundColor = 'rgb(255,255,255)';
-         break;
+          pixel[i].style.backgroundColor = 'rgb(255, 255, 255)';
+          break;
                   case arraycolor[1]:
-         pixel[i].style.backgroundColor = 'rgb(255,255,255)';
-         break;
+          pixel[i].style.backgroundColor = 'rgb(255, 255, 255)';
+          break;
                   case arraycolor[2]:
-         pixel[i].style.backgroundColor = 'rgb(255,255,255)';
-         break;
+          pixel[i].style.backgroundColor = 'rgb(255, 255, 255)';
+          break;
                   case arraycolor[3]:
-         pixel[i].style.backgroundColor = 'rgb(255,255,255)';
-         break;
-         default:
-         pixel[i].style.backgroundColor = itemCor.style.backgroundColor;
-         break;
+          pixel[i].style.backgroundColor = 'rgb(255, 255, 255)';
+          break;
+          default:
+          pixel[i].style.backgroundColor = itemCor.style.backgroundColor;
+          break;
 
-       }
+        }
 
     })
  }
 }
 colorir();
 
-function LimpaPainel(){
+function LimpaPainel() {
   let limpaPainelBTN = document.querySelector('#clear-board')
-  limpaPainelBTN.addEventListener('click', function(){
-    let getPixel = document.querySelectorAll('.pixel')
-    for(index = 0; index < getPixel.length; index += 1){
+  limpaPainelBTN.addEventListener('click', function() {
+  let getPixel = document.querySelectorAll('.pixel')
+  for(index = 0; index < getPixel.length; index += 1){
       getPixel[index].style.backgroundColor = '#fff';
     }
   })
