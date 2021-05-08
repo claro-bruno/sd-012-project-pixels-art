9d3423f1b1a3381d18dbc56dc0cbe1eaa1b61b7e
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-08 20:46:27 -0300

    9-Adiciona button que limpa as cores do quadro

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const cores = document.querySelectorAll('.color');
 
 function criaLinhas(linhas) {
     for (let index = 0; index < linhas; index += 1) {
-        const linhaDiv = document.createElement('div');
+        let linhaDiv = document.createElement('div');
         linhaDiv.classList.add('linha');
         document.getElementById('pixel-board').appendChild(linhaDiv);
     };   
 };
 criaLinhas(5);
 
+
 function criaColunas(colunas) {
     let linhas = document.querySelectorAll('.linha');
     for (let linha of linhas) {
         for (let index = 0; index < colunas; index += 1) {
-            const colunaDiv = document.createElement('div');
+            let colunaDiv = document.createElement('div');
             colunaDiv.classList.add('pixel');
             linha.appendChild(colunaDiv);
         };
@@ -23,20 +24,20 @@ function criaColunas(colunas) {
 };
 criaColunas(5);
 
-function selectedEvent(click) {
+function selecionaEvento(click) {
     let removeSelected = document.querySelector('.selected');
     removeSelected.classList.remove('selected'); 
     click.target.classList.add('selected');
 };
 
-
-function clickEvent() {
+function clickEvento() {
     for (let index = 0; index < cores.length; index += 1 ) {
-    cores[index].addEventListener('click', selectedEvent);
+    cores[index].addEventListener('click', selecionaEvento);
     };
 };
-clickEvent();
 
-function geraCor() {
-    
-}
+function mudaCor(event) {
+    let selectedColor = document.querySelector('.selected');
+    let colorClick = window.getComputedStyle(selectedColor);
+    event.target.style.backgroundColor = colorClick.getPropertyValue('backgraund-color');
+};
