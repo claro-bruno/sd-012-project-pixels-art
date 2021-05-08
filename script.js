70a815b5503e546d906e26b0e4031115b0b5b8d9
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-05-08 14:52:33 -0300

    Adiciona até requisito 5.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const colorPalette = document.querySelector('#color-palette');
 const colorArray = ['black', 'red', 'blue', 'pink', 'purple', 'green', 'orange', 'yellow'];
+const pixelBoard = document.querySelector('#pixel-board');
 
-function paletaDeCores(colorNumber, colorArray) {
-  
+function paletaDeCores(colorNumber, colorArray) {  
   for (let index = 0; index < colorNumber; index ++) {
     const divColorPalette = document.createElement('div');
     divColorPalette.className = 'color';
@@ -11,4 +11,17 @@ function paletaDeCores(colorNumber, colorArray) {
   }
 };
 
+function fillPixelBoard(lineNumber, columnNumber) {
+  for (let indexL = 0; indexL < lineNumber; indexL ++) {
+    const boardLine = document.createElement('div');
+    pixelBoard.appendChild(boardLine);
+    for (let indexC = 0; indexC < columnNumber; indexC ++) {
+      const boardColumn = document.createElement('div');
+      boardColumn.className = 'pixel';
+      pixelBoard.appendChild(boardColumn);
+    }
+  }
+};
+
 paletaDeCores(4, colorArray);
+fillPixelBoard(5, 5);
