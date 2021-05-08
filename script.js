02c9fcc30d219a4eec7a218ee10497bddf8fcd86
Gabriel Viana <g.viana87@hotmail.com>
2021-05-08 03:08:45 -0300

    3 am por hj chega - Corrigir selectedSetColor

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function changeSelected(event) {
-    const selectedChange = document.querySelector('.selected');
-    selectedChange.classList.remove('selected');
-    event.target.classList.add('selected');
-};
-
 function paletteColor() {
     for (let cont = 0; cont < 4; cont += 1) {
         let colorPalette = document.createElement('div');
         colorPalette.classList.add ('color');
-        colorPalette.id = 'cp' + [cont];
         document.querySelector('#color-palette').appendChild(colorPalette);  
         switch (cont){
             case 0:
@@ -29,16 +22,30 @@ function paletteColor() {
     document.querySelector('.color').classList.add('selected'); 
 };
 paletteColor();
- 
-function selectedSetColor(event) {
-    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+
+function changeSelected(event) {
+    const selectedChange = document.querySelector('.selected');
+    selectedChange.classList.remove('selected');
+    event.target.classList.add('selected');
 };
-        
+// function selectedSetColor(event) {
+//    const selectedColor = document.querySelector('.selected');
+//    const pixelPalette = document.querySelectorAll('.color');
+//    for (cont = 0; cont < pixelPalette.length; cont += 1) {
+//        console.log(pixelPalette[cont])
+
+//        if (pixelPalette[cont] === 0) {
+//         event.target.style.backgroundColor = 'black'
+//        }
+//    }
+
+
+   
+};         
 function boardPixels (n) {
     for (let cont = 0; cont < n**2; cont += 1){
         let board = document.createElement('div');
         board.classList.add ('pixel');
-        board.id = 'pb' + [cont];
         document.querySelector('#pixel-board').appendChild(board);
     };
 };
