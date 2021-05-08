a336ed7865a6ca8cdd7397fc97dc1e4f5c27dbcd
Eurides <euridesalves@yahoo.com.br>
2021-05-07 21:38:01 -0300

    correcao da classe requisito 8 incompleto

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-window.onload = function () {
-  const corInicial = document.getElementById('color1');
-  corInicial.className = 'color selected';
-   // ddEventListener(p1,p2) onde: p1 e' o evento e p2 a funcao a ser executada
-  const paletaPreta = document.getElementById('color1');
-  paletaPreta.addEventListener('click', clicouCor); 
-  const paletaVermelha = document.getElementById('color2');
-  paletaVermelha.addEventListener('click', clicouCor);
-  const paletaAzul = document.getElementById('color3');
-  paletaAzul.addEventListener('click', clicouCor);
-  const paletaVerde = document.getElementById('color4');
-  paletaVerde.addEventListener('click', clicouCor);
-}; 
+function clicouCor() {
+  const corAntiga = document.getElementById('color-palette');
+  corAntiga.addEventListener('click', function(event) {
+    document.querySelector(".selected").classList.remove("selected");
+    event.target.classList.add("selected");
+  })
+ };
 
-function clicouCor(paletaEscolhida) {
-  const corAntiga = document.getElementsByClassName('selected')[0];
-  corAntiga.className = 'color';
-  const corNova = document.getElementById(paletaEscolhida.target.id);
-  corNova.className = 'color selected';
-};
+ function pintar() {
+  const tabelaPixel = document.querySelectorAll(".pixel");
+  console.log(tabelaPixel);
+  for (let indice = 0; indice < tabelaPixel.length; indice +=1) {
+    tabelaPixel[indice].addEventListener('click', function(event) {
+      let corEscolhida = document.getElementsByClassName('selected')[0].style.background;
+      console.log(corEscolhida);
+      event.target.style.background = corEscolhida;
+    })
+  }
+ }
+/* window.onload = function () { */
+const corPixel = document.getElementById("color1");
+corPixel.style.color = black; 
+corPixel.style.background = black;
 
-/* 
-  cont cor = document.querySelector9(#palete)
-cor.addeventlistener('click, (event) => {
-  if (event.target.classname === 'color') {
-   corselec.classlist.remove(selected);
-   event.target.classlist.add(selected)
-document.getElementsByClassName("selected")[0]
-  const corAntiga = document.getElementsByClassName('color selected');
-  const teste = document.getElementById(corAntiga);
-  console.log(teste);
-  console.log(corAntiga);
-  /* document.getElementById("MyElement").className = "color"; */
-  /* corAntiga.className = 'color';
-  console.log(corAntiga);
-   */
- 
+/* corPixel.style.background-color = black; */
+const corInicial = document.getElementById('color1');
+corInicial.className = 'color selected';
+clicouCor();
+pintar();
