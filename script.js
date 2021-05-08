5db10879940d79bd165d2ff976e39458c77378e6
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-05-08 16:01:49 -0300

    Adiciona até requisito 8.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const colorPalette = document.querySelector('#color-palette');
 const colorArray = ['black', 'red', 'blue', 'pink', 'purple', 'green', 'orange', 'yellow'];
 const pixelBoard = document.querySelector('#pixel-board');
+let selectedColor = 'black';
+
+window.onload = applyColor();
 
 function paletaDeCores(colorNumber, colorArray) {  
   for (let index = 0; index < colorNumber; index ++) {
@@ -8,6 +11,9 @@ function paletaDeCores(colorNumber, colorArray) {
     divColorPalette.className = 'color';
     divColorPalette.style.backgroundColor = colorArray[index];
     colorPalette.appendChild(divColorPalette);
+    if (divColorPalette.style.backgroundColor === 'black') {
+      divColorPalette.classList.add('selected');
+    }
   }
 };
 
@@ -23,5 +29,26 @@ function fillPixelBoard(lineNumber, columnNumber) {
   }
 };
 
+function applyColor() {
+  document.addEventListener('click', function(event) {
+    if (event.target.className === 'pixel') {
+      event.target.style.backgroundColor = selectedColor;
+    }
+  })
+}
+
+function copyColor() {
+  document.addEventListener('click', function(event) {
+    if (event.target.className === 'color') {
+      const classSelected = document.querySelector('.selected');
+      classSelected.classList.remove('selected');
+      event.target.classList.add('selected');
+      selectedColor = event.target.style.backgroundColor;
+    }
+  })
+}
+
 paletaDeCores(4, colorArray);
 fillPixelBoard(5, 5);
+copyColor();
+applyColor();
