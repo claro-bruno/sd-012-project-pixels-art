a2c6152488da833a1230d4fa6b912b626ee8cf42
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-08 20:59:45 -0300

    refatorando o desafio 2 Projeto Pixels Art

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-for (let index = 0; index < 4; index += 1) {
+let arrayColor = ['black','yellow','red','green'];
+
+for (let index = 0; index < arrayColor.length; index += 1) {
   const divColor = document.createElement('div');
   divColor.className = 'color';
+  divColor.style.backgroundColor = arrayColor[index];
   const colorPalette = document.getElementById('color-palette');
 
   colorPalette.appendChild(divColor);
 }
+
+
+
+
+
+colorPalette.style.backgroundColor = arrayColor[1];
+
+const colorPalette = document.getElementById('color-palette');
+
+console.log(colorPalette.style.backgroundColor);
+
+console.log(colorPalette.style);
+/*
+1 - Escolher quais são as cores que eu quero.
+2 - A primeira cor tem que ser preta.
+3 - Criar um array com as quatro cores.
+4 - Acessar o background-color da minha div filho.
+5 - Mudar o background-color da minha div.
+*/
