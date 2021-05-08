1b96c2623eb816b7f9faea12cb707c4bf55d4f60
martinsph <ph.mmartins@gmail.com>
2021-05-08 18:31:00 -0300

    adiciona req 8

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
-const areaDiv2 = document.getElementById("div2")
+const areaDiv2 = document.getElementById("div2");
+const colors = document.getElementsByClassName("color");
+const colorPixel = document.querySelector('.pixel');
+const printPixel = document.getElementsByClassName("pixel");
 
+// Populando PixelBoard
 function createBoard(row, column) {
     let createTable = document.createElement('table');
     createTable.id = "pixel-board";
@@ -20,11 +24,22 @@ function createBoard(row, column) {
 }
 createBoard(5, 5);
 
+//preenchendo cores da paleta
+function paletteColor() {
+    const arrayCores = ["black", "orange", "blue", "green"];
+    for (let index = 0; index < colors.length; index += 1) {
+      colors[index].style.backgroundColor = arrayCores[index];
+    };
+  };
+paletteColor();
+
+//selecionando preto como cor primária
 window.onload = function () {
     let firstColor = document.querySelector(".color1");
     firstColor.classList.add("selected");
 };
 
+//evento para atribuir .selected ao clicar
 function colorselector() {
     let color1 = document.querySelector(".color1");
     color1.addEventListener('click', function(event) {
@@ -55,3 +70,15 @@ function colorselector() {
     })
 };
 colorselector();
+
+
+function pixelBoardColor() {
+    for (let index = 0; index < printPixel.length; index += 1) {
+      printPixel[index].addEventListener('click', (event) => {
+        let selectedColor = document.querySelector('.selected');
+        let selectedBackgroundColor = window.getComputedStyle(selectedColor).backgroundColor;
+        event.target.style.backgroundColor = selectedBackgroundColor;
+      });
+    };
+  };
+  pixelBoardColor();
