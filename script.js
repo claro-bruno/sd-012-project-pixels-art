978fc65d415bb49adfdcd3f2d408b0a502c1f315
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 21:36:49 -0300

    termino do req 6

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let pixBoard = document.getElementById("pixel-board");
-for (let indice1 = 0; indice1 < 5; indice1 += 1) {
-  for (let indice2 = 0; indice2 < 5; indice2 += 1) {
-    let pixUnity = document.createElement("li");
-    pixBoard.appendChild(pixUnity);
-    let caughtPix = document.getElementById("pixel-board");
-    caughtPix.children[indice2].className = "pixel"; 
+function pallet(palletNumber) {
+  let pixBoard = document.getElementById("pixel-board");
+  for (indice1 = 0; indice1 < palletNumber; indice1 += 1) {
+    let bornline = document.createElement("div");
+    pixBoard.appendChild(bornline);
+    pixBoard.children[indice1].className = "linha";
+    for (let indice2 = 0; indice2 < palletNumber; indice2 += 1) {
+      let bornColumn = document.createElement("div");
+      let memberLine = pixBoard.children[indice1];
+      memberLine.appendChild(bornColumn);
+      pixBoard.children[indice1].children[indice2].className = "coluna";
+    }
+  }
 }
-document.getElementById("pixel-board").children[indice1] = "\n";
-}
+pallet(5);
