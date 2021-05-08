e6b137864a53cca91c15d90800d42a0eec9aea0f
Gustavo-Mourao <gcmbh1@gmail.com>
2021-05-08 19:28:47 -0300

    Q6

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function pixelboard() {
-    let lines = document.querySelectorAll(".pixel")
+// Funçao para criar a tabela
+const armazNumeroTabela = [];
+let n = 24;
+for (index = 0; index < n; index += 1){
+    armazNumeroTabela.push(index);
+const tabela = document.querySelector('#pixel-board');
+const pixelPoint = document.createElement('div');
+pixelPoint.className = 'pixel';
+tabela.appendChild(pixelPoint)
+}
+// Funçao Click mouse
+let corSelecionada = document.querySelector('.selected');
+corSelecionada.addEventListener('click', apertarMouse)
+function apertarMouse(){
+console.log('click')
 }
