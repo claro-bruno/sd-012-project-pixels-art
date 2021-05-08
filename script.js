335bde60a49cfa19973fec1affbcfc3c1a6f252a
thales daniel <thalesdaniel2013@gmail.com>
2021-05-08 18:49:32 -0300

    feito a etapa 9

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ for (index = 0; index < 25; index += 1) {
     criacao.className = 'pixel'
     criacao.addEventListener('click', adicionaCor)
     const div = document.querySelector('#pixel-board').appendChild(criacao)
-}
 
+    //criaçao de botão e adicionado o evento de apagar todas as cores das divs
+    let botao = document.querySelector('#clear-board')
+    botao.addEventListener('click', function(event) {
+        criacao.style.backgroundColor = 'white'
+    })
+}
 window.onload = function inicializacao() {
     let preto = document.querySelector('#preto')
     preto.classList.add('selected')
