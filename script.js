948c3503f410704c8c121063aea858d27b759661
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 22:18:11 -0300

    Tentativa de adeq req 5

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function pallet(palletNumber) {
   let pixBoard = document.getElementById("pixel-board");
   for (indice1 = 0; indice1 < palletNumber; indice1 += 1) {
-    let bornline = document.createElement("div");
-    pixBoard.appendChild(bornline);
+    let bornLine = document.createElement("div");
+    pixBoard.appendChild(bornLine);
     pixBoard.children[indice1].className = "linha";
     for (let indice2 = 0; indice2 < palletNumber; indice2 += 1) {
       let bornColumn = document.createElement("div");
       let memberLine = pixBoard.children[indice1];
       memberLine.appendChild(bornColumn);
-      pixBoard.children[indice1].children[indice2].className = "coluna";
+      pixBoard.children[indice1].children[indice2].className = "pixel";
     }
   }
 }
