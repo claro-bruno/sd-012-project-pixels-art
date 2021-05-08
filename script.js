42e9178fb3a9e8bedbffa1e1ded7f35d47fe163e
Gabriel Viana <g.viana87@hotmail.com>
2021-05-08 01:35:39 -0300

    selectedChange on paletteBoard

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-// const colorPalette = document.getElementById('color-palete');
-const pixelBoard = document.getElementById('pixel-board');
 const pixels = document.getElementsByClassName('pixel');
+function changeSelected(event) {
+    const selectedChange= document.querySelector('.selected');
+    selectedChange.classList.remove('selected');
+    event.target.classList.add('selected');
+};
 
-// function paletteColor (n) {
-//     for (let cont = 0; cont < n; cont += 1) {
-//         let color = document.createElement('div');
-//         color.className = 'color';
-//         color.id = 'c' + [cont];
-//         colorPalette.appendChild(color);
-//     };
-// };
-// // paletteColor(4);
-
-
+function paletteColor() {
+    for (let cont = 0; cont < 4; cont += 1) {
+        let colorPalette = document.createElement('div');
+        colorPalette.classList.add ('color');
+        colorPalette.id = 'cp' + [cont];
+        document.querySelector('#color-palette').appendChild(colorPalette);  
+        switch (cont){
+            case 0:
+                colorPalette.style.background = 'black';
+                break;
+            case 1:
+                colorPalette.style.background = 'green';
+                break;
+            case 2:
+                colorPalette.style.background = 'yellow';
+                break;
+            case 3:
+                colorPalette.style.background= 'red';
+                break;
+        };
+        colorPalette.addEventListener('click', changeSelected);
+    }; 
+    document.querySelector('.color').classList.add('selected'); 
+};
+paletteColor();
+ 
+function selectedSetColor(event) {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+};
+        
 function boardPixels (n) {
     for (let cont = 0; cont < n**2; cont += 1){
         let board = document.createElement('div');
-        board.className = 'pixel';
-        board.id = 'p' + [cont];
-        pixelBoard.appendChild(board);
+        board.classList.add ('pixel');
+        board.id = 'pb' + [cont];
+        document.querySelector('#pixel-board').appendChild(board);
     };
 };
 boardPixels(5);
+        
+
+
+
+
+
+
 
 
 //window.onload = alerta
@@ -29,9 +58,3 @@ boardPixels(5);
 //    alert('Oba! Vamos colorir.');
 //};
 
-// let desfazer = document.getElementById('undo-button');
-// desfazer.addEventListener('click', undoButton);
-// function undoButton(eventoDeOrigem) {
-// console.log(eventoDeOrigem.target);
-// console.log(eventoDeOrigem.type);
-// }
