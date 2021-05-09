62d1816a2cec8901558bc12f171081ffb37c9721
Lucas Basques <lukasbasques@gmail.com>
2021-05-09 20:40:40 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ function addColumn(column) {
     let divColumn = document.createElement('div');
     divColumn.className = 'column';
     board.appendChild(divColumn);
-  };
-};
+  }
+}
 
-addColumn(column);
-
-addline(line)
 function addline (line) {
   let lineColumn = document.getElementsByClassName('column');
   for (let lines of lineColumn) {
-    for ( let index = 0; index < line; index += 1) {
+    for (let index = 0; index < line; index += 1) {
       let divline = document.createElement('div');
       divline.className = 'pixel';
       lines.appendChild(divline);
-    };
-  };
-};
+    }
+  }
+}
 
+document.body.addEventListener("click", function(event){
+  document.querySelector(".selected").classList.remove("selected");
+  event.target.classList.add("selected");
+})
+
+addColumn(column);
+addline(line);
