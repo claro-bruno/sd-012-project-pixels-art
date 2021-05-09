6f4d6047ddb81fa7161da88f5000e3b2563fe876
Lucas Basques <lukasbasques@gmail.com>
2021-05-09 20:03:40 -0300

    Corrige requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let line = 5;
 let column = 5;
-let board = document.getElementById('pixel-board')
+let board = document.getElementById('pixel-board');
 
 function addColumn(column) {
   for (let index = 0; index < column; index += 1) {
