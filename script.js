a514fc9313c11a34f95967f554ca6dbeee2ec40a
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-09 11:25:33 -0300

    add pixels dinamicamente

--- script.js
+++ script.js
@@ -1,56 +1,50 @@
 window.onload = firstColor
 
-    let black = document.getElementById('black');
-    black.addEventListener('click', firstColor);
+let black = document.getElementById('black');
+black.addEventListener('click', firstColor);
 
-    function firstColor() {
-        black.className = 'color selected';
-    }
+function firstColor() {
+    black.className = 'color selected';
+}
 
-    function createPixel() {
-        let section = document.getElementsByTagName('section');
-        let div = document.createElement('div');
-        div.className = 'pixel';
-        section.appendChild(pixel);
+const section = document.getElementById('pixel-board'); 
+function RenderBoard(boardSize) {
+    for (let i = 0; i < boardSize; i += 1){
+      const Line = document.createElement('div');
+      Line.className = 'linha'
+      section.appendChild(Line);
+      for (let i = 0; i < boardSize; i += 1){
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        Line.appendChild(pixel);
+      }
     }
-    function createBoardPixel(tam) {
-        for(let i = 0; i < tam; i += 0) {
-            for(let index = 0; index < tam; i += 0){
-                createPixel()
+  
+  }
+  
+RenderBoard(5)
+  
+const color = document.getElementsByClassName('color');
+for (let i = 0; i < color.length; i += 1) {
+    let corSelecionada = color[i];
+    corSelecionada.addEventListener('click', 
+    function() {
+        const ultima = document.querySelector('.selected');
+        ultima.classList.remove('selected');
+        corSelecionada.className = 'color selected';
+    })
+}
+
+const pixel = document.querySelectorAll('.pixel');
+function recebeClick() {
+    for (let index = 0; index < pixel.length; index +=1) {
+        pixel[i].addEventListener("click",function(){
+            const cor = document.querySelector('.selected');
+            if(pixel[i].style.backgroundColor != cor.style.backgroundColor) {
+            pixel[i].style.backgroundColor = cor.style.backgroundColor
             }
-        let br = document.createElement('br');
-        document.getElementsByTagName('section').appendChild(br);
-        }
-
-    }
-    createBoardPixel()
-
-
-
-
-
-    
-    const color = document.getElementsByClassName('color');
-    for (let i = 0; i < color.length; i += 1) {
-        let corSelecionada = color[i];
-        corSelecionada.addEventListener('click', 
-        function() {
-            const ultima = document.querySelector('.selected');
-            ultima.classList.remove('selected');
-            corSelecionada.className = 'color selected';
-        })
-    }
-    
-    const pixel = document.querySelectorAll('.pixel');
-    function recebeClick() {
-        for (let index = 0; index < pixel.length; index +=1) {
-            pixel[i].addEventListener("click",function(){
-                const cor = document.querySelector('.selected');
-                if(pixel[i].style.backgroundColor != cor.style.backgroundColor) {
-                pixel[i].style.backgroundColor = cor.style.backgroundColor
-                }
-            });
-        }
-    }
-    recebeClick();
+        });
+}
+}
+recebeClick();
 
