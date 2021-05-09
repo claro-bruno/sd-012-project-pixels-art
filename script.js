a66754494d603a9e90c1dda5a215b44d4546a3e6
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-09 01:57:11 -0300

    questao 8 - PRECISO REFAZER TUDO COM JS

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const colorPalette = document.querySelector('#color-palette');
+window.onload = function(){
 
+const colorPalette = document.querySelector('#color-palette');
+const getPixel = document.querySelectorAll('.pixel')
+const xButton = document.querySelector('#button')
+let getColor = 'black';
+let newColor = 'white';
 
 colorPalette.addEventListener('click', eventColor => {
   
@@ -7,8 +12,16 @@ colorPalette.addEventListener('click', eventColor => {
     const selectedColor = document.querySelector('.selected')
     selectedColor.classList.remove('selected')
     eventColor.target.classList.add('selected')
+    getColor = eventColor.target.style.backgroundColor
   }
 })
 
 
+for(index = 0; index < getPixel.length; index += 1){
+  getPixel[index].addEventListener('click', eventPixel => {
+    eventPixel.target.style.backgroundColor = getColor
+     
+   }) 
+}
+}
 
