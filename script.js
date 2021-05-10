654c4a21447942afe5525fca190856e92f28bc7d
Josimar Schneider <josimarschneider@gmail.com>
2021-05-10 11:47:09 -0400

    adiciona cor da paleta

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-GeraCores();
+const arrayCores =['black', 'red', 'blue', 'orange'];
+const cores = document.querySelectorAll('.color');
+
+GeraCores();//atribui cores a paleta
 function GeraCores (){
-    const arrayCores =['black', 'red', 'blue', 'orange'];
-    const cores = document.querySelectorAll('.color');
     for (let index = 0; index < arrayCores.length; index +=1) {
       cores[index].style.backgroundColor = arrayCores[index];
     } 
+}
+cores[0].classList.add('selected'); //cor preto já está selecionada
+function mudaClasse(event) {//adiciona a classe selected no elemento que receber o evento de click
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+eventClick();
+function eventClick(){//captura o click na paleta
+for (let indexSelect = 0; indexSelect < cores.length; indexSelect += 1) {
+    cores[indexSelect].addEventListener('click', mudaClasse);
+}
+}
+ColorePixel();
+function ColorePixel(event){//colore a classe pixel clicada 
+  document.body.addEventListener('click', ColorePixel);
+  if (event.target.className === 'pixel') {
+    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+  }
 }
