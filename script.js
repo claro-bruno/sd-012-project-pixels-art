6c4c6f4a93b4fd952ca079c1e270798293bd2a47
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-10 11:45:12 -0300

    refatoracao de funcoes e requisito de cores aleatorias

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function sizePixels() {
-  let xablau = document.getElementById('board-size').value;
-  let n;
-  n = parseInt(xablau);
-  clear();
-  if (n >= 5 && n <= 50) {
-    pixelLine = n;
-  } else {
-    alert('Board inválido');
-  }
-}
-
 let pixelLine = 5;
 function grid(pixelLine) {
   let pixels = document.getElementById('pixel-board');
@@ -25,42 +13,59 @@ function grid(pixelLine) {
     }
   }
 }
-
 grid(pixelLine);
 
+function oi() {
+  let newGrid = document.getElementById('board-size').value;
+  parseInt(newGrid);
+  if (newGrid >= 5 && newGrid <= 50) {
+    pixelLine = newGrid;
+    let tabela = document.getElementById('pixel-board')
+    while (tabela.lastElementChild) {
+      tabela.removeChild(tabela.lastElementChild);
+    }
+    grid(pixelLine);
+  } else {
+    alert('Board inválido');
+  }
+}
 
-const elementColor = document.getElementsByClassName('color');
 function colorSelect(event) {
   const select = document.querySelector('.selected');
   select.classList.remove('selected');
   event.target.classList.add('selected');
 }
+
+//Achei incrivel o que o Bruno Augusto Claro fez com essa função!
+function newColors() {
+  let rgb1 = Math.random() * 255;
+  let rgb2 = Math.random() * 255;
+  let rgb3 = Math.random() * 255;
+  return `rgb(${rgb1}, ${rgb2}, ${rgb3})`
+}
+
+const elementColor = document.getElementsByClassName('color');
+const colors = ['black', newColors(), newColors(), newColors()];
+for (let index = 0; index < elementColor.length; index += 1) {
+  elementColor[index].style.backgroundColor = colors[index];
+}
 for (let index = 0; index < elementColor.length; index += 1) {
   elementColor[index].addEventListener('click', colorSelect);
 }
 
-const black = (document.getElementById('black').style.backgroundColor =
-  'black');
-const red = (document.getElementById('red').style.backgroundColor = '#FF0000');
-const blue = (document.getElementById('blue').style.backgroundColor =
-  '#0000FF');
-const green = (document.getElementById('green').style.backgroundColor =
-  '#008000');
-
-const pixed = document.querySelectorAll('.pixel');
+const pixed = document.getElementsByClassName('pixel');
+for (let indexPixels = 0; indexPixels < pixed.length; indexPixels += 1) {
+  pixed[indexPixels].addEventListener('click', paintPixel);
+}
 function paintPixel(eventPaint) {
   const paint = document.querySelector('.selected');
   eventPaint.target.style.backgroundColor = paint.style.backgroundColor;
 }
 
-for (let indexPixels = 0; indexPixels < pixed.length; indexPixels += 1) {
-  pixed[indexPixels].addEventListener('click', paintPixel);
-}
-
-const buttonClick = document.getElementById('clear-board');
 function clear() {
   for (let pix = 0; pix < pixed.length; pix += 1) {
     pixed[pix].style.backgroundColor = 'white';
   }
 }
+const buttonClick = document.getElementById('clear-board');
 buttonClick.addEventListener('click', clear);
