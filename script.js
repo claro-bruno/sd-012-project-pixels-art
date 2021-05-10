6cbcda3e1fb8b14ec54f01066dd7b4e561dfab8a
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-05-09 21:29:08 -0300

    Refatorando o codigo para deixar automatizado

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 window.onload = function () {
     
+    function criaQuadro() {
+        btn.addEventListener('click', function() {
+            let entrada = document.getElementById('inputTamanho').value;
+            let entradaFormatado = parseInt(entrada);
+            let tamanhoQuadro = Math.pow(entradaFormatado, 2);
+            let btn = document.getElementById('btn');
+            for(index = 0; index < tamanhoQuadro; index += 1){
+                let criaPixel = document.createElement('div');
+                criaPixel.className = 'pixel';
+                const div = document.querySelector('#pixel-board').appendChild(criaPixel);
+            };
+        })
+    }
+    criaQuadro();
 }
