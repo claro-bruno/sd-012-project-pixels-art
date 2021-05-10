24379fb5fade5b1d82a7c5dd099d48e32dbf29e7
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-10 11:50:12 -0300

    iniciando o desafio 4

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function createPalette(arrColor) {
   }
 }
 createPalette(arrayColor);
+
+const divLine = document.createElement('div');
+for (let index = 0; index < 5; index += 1) {
+  const divPixel = document.createElement('div');
+  divPixel.className = 'pixel';
+  
+  divLine.className = 'line';
+  divLine.appendChild(divPixel);
+
+}
+
+
+const divBoard = document.querySelector('#pixel-board');
+
+divBoard.appendChild(divLine);
+
+console.log(divBoard);
+
+/*
+1 - Criar cinco div.
+2 - Criar uma classe para nossa cinco div.
+3 - Fazer a nossa as nossas divs serem filhas do nosso pixel board.
+4 - Criar for para repetir isso cinco vezes.
+*/
