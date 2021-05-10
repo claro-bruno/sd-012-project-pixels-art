f7abdc01c39b886506ae9212a71ef461d00a9e2e
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-05-10 13:14:10 -0300

    requisitos ate o 5

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 window.onload = function () {
     
     function criaQuadro() {
+        let btn = document.getElementById('btn');
         btn.addEventListener('click', function() {
             let entrada = document.getElementById('inputTamanho').value;
             let entradaFormatado = parseInt(entrada);
-            let tamanhoQuadro = Math.pow(entradaFormatado, 2);
-            let btn = document.getElementById('btn');
-            for(index = 0; index < tamanhoQuadro; index += 1){
+            if(entradaFormatado > 5 && entradaFormatado < 50){
+            for(indexLinha = 0; indexLinha < entradaFormatado; indexLinha += 1){
+                let linha = document.createElement('div');
+                linha.className = 'linha';
+                document.querySelector('#pixel-board').appendChild(linha);
+                for(indexColuna = 0; indexColuna < entradaFormatado; indexColuna += 1){
                 let criaPixel = document.createElement('div');
                 criaPixel.className = 'pixel';
-                const div = document.querySelector('#pixel-board').appendChild(criaPixel);
+                criaPixel.addEventListener('click', function(event){
+                })
+                document.querySelectorAll('.linha')[indexLinha].appendChild(criaPixel);
+                }   
             };
+            } else if(entradaFormatado < 5){
+                entradaFormatado = 5;
+                for(indexLinha = 0; indexLinha < entradaFormatado; indexLinha += 1){
+                    let linha = document.createElement('div');
+                    linha.className = 'linha';
+                    document.querySelector('#pixel-board').appendChild(linha);
+                    for(indexColuna = 0; indexColuna < entradaFormatado; indexColuna += 1){
+                    let criaPixel = document.createElement('div');
+                    criaPixel.className = 'pixel';
+                    criaPixel.addEventListener('click', function(event){
+                    })
+                    document.querySelectorAll('.linha')[indexLinha].appendChild(criaPixel);
+                    }   
+                };
+            } else if(entradaFormatado > 50){
+                entradaFormatado = 50;
+                for(indexLinha = 0; indexLinha < entradaFormatado; indexLinha += 1){
+                    let linha = document.createElement('div');
+                    linha.className = 'linha';
+                    document.querySelector('#pixel-board').appendChild(linha);
+                    for(indexColuna = 0; indexColuna < entradaFormatado; indexColuna += 1){
+                    let criaPixel = document.createElement('div');
+                    criaPixel.className = 'pixel';
+                    criaPixel.addEventListener('click', function(event){
+                    })
+                    document.querySelectorAll('.linha')[indexLinha].appendChild(criaPixel);
+                    }   
+                };
+            }
         })
     }
+/*
+    function paletaCores() {
+        let cores = document.querySelectorAll('.color');
+        cores.addEventListener('click', function(event))
+    }
+    */
     criaQuadro();
 }
