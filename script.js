b17bdc2e6c0ec53cd1c9f415586e3b4091552126
gscuri <gscuri6@hotmail.com>
2021-05-10 01:20:31 -0300

    resolucao 8 selecionando todas as cores

--- script.js
+++ script.js
@@ -8,13 +8,50 @@ const bloco3 = document.querySelector('#bloco3');
 const bloco4 = document.querySelector('#bloco4');
 
 // funcao para remover e adicionar o nome selected na classe
-function removeEAdd(mouseClick) {
+function removeEAdd(mouseClick1) {
   let changeClass = document.querySelector('.selected');
   changeClass.classList.remove('selected');
-  mouseClick.target.classList.add('selected');
+  mouseClick1.target.classList.add('selected');
 }
 // envento para executar acao click do mouse
 bloco1.addEventListener('click', removeEAdd);
 bloco2.addEventListener('click', removeEAdd);
 bloco3.addEventListener('click', removeEAdd);
 bloco4.addEventListener('click', removeEAdd);
+
+let selectedColor = 'black';
+const bloco1Color = document.querySelector('#bloco1');
+const bloco2Color = document.querySelector('#bloco2');
+const bloco3Color = document.querySelector('#bloco3');
+const bloco4Color = document.querySelector('#bloco4');
+const pixelBoard = document.querySelector('#pixel-board');
+
+function colorPaletteSelected1(color1) {
+  let colorOne = color1.target;
+  selectedColor = 'black';
+}
+bloco1Color.addEventListener('click', colorPaletteSelected1);
+
+function colorPaletteSelected2(color2) {
+  let colorTwo = color2.target;
+  selectedColor = '#04C696';
+}
+bloco2Color.addEventListener('click', colorPaletteSelected2);
+
+function colorPaletteSelected3(color3) {
+  let colorThree = color3.target;
+  selectedColor = '#089DBE';
+}
+bloco3Color.addEventListener('click', colorPaletteSelected3);
+
+function colorPaletteSelected4(color4) {
+  let colorFour = color4.target;
+  selectedColor = '#0C75E6';
+}
+bloco4Color.addEventListener('click', colorPaletteSelected4);
+
+function colorAll(painting) {
+  let paint = painting.target;
+  paint.style.backgroundColor = selectedColor;
+}
+pixelBoard.addEventListener('click', colorAll);
