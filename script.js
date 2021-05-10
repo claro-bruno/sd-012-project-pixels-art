e2bde6a74d427aebb733f35f1a7ec39df30e8e86
Victor Emmanuel <victorpassos2012@gmail.com>
2021-05-09 21:15:03 -0300

    7 tentando novamente

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let selectColor = document.getElementById('color-palette');
-selectColor.addEventListener('click', function(event){
-    let select = document.getElementsByClassName('selected');
-    select[0].className = 'color';
-    event.target.className = 'color selected'
-})
+function selectColor(){
+    let color1 = document.getElementById('color1');
+    let color2 = document.getElementById('color2');
+    let color3 = document.getElementById('color3');
+    let color4 = document.getElementById('color4');
+    
+    color1.addEventListener('click', select);
+    color2.addEventListener('click', select);
+    color3.addEventListener('click', select);
+    color4.addEventListener('click', select);
+
+    function select(event){
+        let element = document.querySelector('.selected');
+        element.classList.remove('selected');
+        event.target.classList.add('selected');
+    }
+}
