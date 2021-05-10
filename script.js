7b1fd9fa015cf84c3e02ae69557a708e1c09b678
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-10 11:42:31 -0300

    2 - Adicionando à página uma paleta de quatro cores distintas

---
+++ script.js
@@ -0,0 +1,19 @@
+//Cores da paleta inicial(atribuindo cor aos filhos da div color-palete)
+let paleta = document.getElementById("color-palette");
+let cor = document.getElementsByClassName("color");
+
+
+// // Função gera cores: Com referência do stackoverflow.
+// function geraCores() {
+//     let r = Math.random() * 255;
+//     let g = Math.random() * 255;
+//     let b = Math.random() * 255;
+
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// for (let index of paleta){
+//         paleta[index].style.backgroundColor = (geraCores());
+
+// }
+
