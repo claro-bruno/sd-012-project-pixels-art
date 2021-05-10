c3a221e3af0b29deefa63a9b92cbed23840c7bb4
Kelvin Costa <kelvin.tecnologia@hotmail.com>
2021-05-10 11:06:45 -0300

    desafio 3 refatorado

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-let arrayColor = ['black','yellow','red','green'];
+const arrayColor = ['black', 'yellow', 'red', 'green'];
 
-for (let index = 0; index < arrayColor.length; index += 1) {
-  const divColor = document.createElement('div');
-  divColor.className = 'color';
-  divColor.style.backgroundColor = arrayColor[index];
-  const colorPalette = document.getElementById('color-palette');
+function createPalette(arrColor) {
+  for (let index = 0; index < arrColor.length; index += 1) {
+    const divColor = document.createElement('div');
+    divColor.className = 'color';
+    divColor.style.backgroundColor = arrColor[index];
+    const colorPalette = document.getElementById('color-palette');
 
-  colorPalette.appendChild(divColor);
+    colorPalette.appendChild(divColor);
+  }
 }
-
-/*
-1 - Escolher quais são as cores que eu quero.
-2 - A primeira cor tem que ser preta.
-3 - Criar um array com as quatro cores.
-4 - Acessar o background-color da minha div filho.
-5 - Mudar o background-color da minha div.
-*/
+createPalette(arrayColor);
