93caef67f0d309b3c1bd51c4c37ffc64980dda07
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-11 11:41:17 -0300

    6 - Define a cor preta como cor inicial. Ao carregar a página, a cor preta ja fica selecionada para pintar os pixels

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ quadroPixels(5);
 
 
 
-//Não consegui identificar o erro nesse código, por enquanto estou fazendo as ateraçoes de forma manual no CSS.
-// // Função gera cores: Com referência do https://wallacemaxters.com.br/.
+// Agora funciona!.
+// Função gera cores: Com referência do https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 
 function geraCores()
 {
@@ -26,28 +26,22 @@ function geraCores()
     .padStart(6, '0');
 }
 
-//Função selecionaCor, verifiquei o repositório do colega Rafael Primom, isso me ajudou a compreender o que eu estava fazendo no meu próprio código.
-let cor = document.getElementsByClassName("color");
-let paleta = document.querySelector("#color-palette");
-
-function selectColor() {
-    paleta.addEventListener('click', function(event) {
-      let corEscolha = document.querySelector('.selected');
-      
-      corEscolha.classList.remove('selected');
-      let clicked = event.target;
-      clicked.classList.add('selected');
-    });
-  }
-  
-  for (let index = 0; index < cor.length; index += 1) {
-    cor[index].addEventListener('click', selecionaCor);
-  }
-
-  let pixelI = document.getElementsByClassName("pixel");
-
-
 const cor1 = document.getElementById('color1').style.backgroundColor = "black";
 const cor2 = document.getElementById('color2').style.backgroundColor = geraCores();
 const cor3 = document.getElementById('color3').style.backgroundColor = geraCores();
 const cor4 = document.getElementById('color4').style.backgroundColor = geraCores();
+
+
+//Função selecionaCor, verifiquei o repositório do colega Rafael Primom, isso me ajudou a compreender o que eu estava fazendo no meu próprio código.
+
+let cor = document.getElementsByClassName("color");
+let paleta = document.querySelector("#color-palette");
+
+function selecionaColor() {
+    paleta.addEventListener('click', function(event) {
+      let corEscolha = document.querySelector('.selected');
+      corEscolha.classList.remove('selected');
+      let selecao = event.target;
+      selecao.classList.add('selected');
+    });
+  }
