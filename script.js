810f5bb315ee66ab57533b16383e8efaebeee549
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-11 15:53:29 -0300

    Corrige itens 7 e 8

--- script.js
+++ script.js
@@ -16,9 +16,7 @@ quadroPixels(5);
 
 
 
-// Agora funciona!.
 // Função gera cores: Com referência do https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
-
 function geraCores()
 {
   return '#' + parseInt((Math.random() * 0xFFFFFF))
@@ -26,43 +24,50 @@ function geraCores()
     .padStart(6, '0');
 }
 
-const cor1 = document.getElementById('color1').style.backgroundColor = "black";
-const cor2 = document.getElementById('color2').style.backgroundColor = geraCores();
-const cor3 = document.getElementById('color3').style.backgroundColor = geraCores();
-const cor4 = document.getElementById('color4').style.backgroundColor = geraCores();
 
-
-//Função selecionaCor, verifiquei o repositório do colega Rafael Primom, isso me ajudou a compreender o que eu estava fazendo no meu próprio código.
-
-let cor = document.getElementsByClassName("color");
+//ERRO
+let cores = document.getElementsByClassName("color");
 let paleta = document.querySelector("#color-palette");
 
-function selecionaColor() {
-    paleta.addEventListener('click', function(event) {
-      let corEscolha = document.querySelector('.selected');
-      corEscolha.classList.remove('selected');
-      let selecao = event.target;
-      selecao.classList.add('selected');
-    });
-  } 
+function selecionaCor(event) {
+  let selected = document.getElementsByClassName('selected')[0];
+    selected.classList.remove('selected');
+  event.target.classList.add('selected');
 
-// //Função pinta quadro
-//   function pintaPixel(event) {
-//     let corPixel = document.querySelector('.selected');
-//     event.target.style.corPixel = corPixel.style.backgroundColor;
-//   }
   
-//   for (let index = 0; index < pixels.length; index += 1) {
-//     pixels[index].addEventListener('click', pintaPixel());
-//   }
+}
+
+for (let index = 0; index < cores.length; index += 1) {
+  cores[index].addEventListener('click', selecionaCor);
+}
+
+
+//Atribui as cores aleatórias geradas na funçao geraCores
+let cor1 = document.getElementById('color1').style.backgroundColor = "black";
+let cor2 = document.getElementById('color2').style.backgroundColor = geraCores();
+let cor3 = document.getElementById('color3').style.backgroundColor = geraCores();
+let cor4 = document.getElementById('color4').style.backgroundColor = geraCores();
+
+//erro desconhecido
+//Função pinta quadro
+let pixels = document.querySelectorAll(".pixel");
+  function pintaPixel(event) {
+    let corPixel = document.querySelector('.selected');
+    console.log(event.target.style.backgroundColor)
+    event.target.style.backgroundColor = corPixel.style.backgroundColor;
+  }
+  
+  
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', pintaPixel);
+  }
 
 //Função limpa quadro
-let pixels = document.querySelectorAll("#pixel");
 let botao = document.getElementById('clear-board');
 
 function limpar() {
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = "white";
+    pixels[index].style.backgroundColor = "rgb(255, 255, 255)";
   }
 }
 botao.addEventListener('click', limpar);
