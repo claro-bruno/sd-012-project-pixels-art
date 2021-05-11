9874429804b620f86526130593e4653d17e567dc
Alice Gonçalves <galice9992@gmail.com>
2021-05-11 20:31:59 -0300

    resolve requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 window.onload = function () {
     document.querySelector('#cor1').style.backgroundColor = 'black'
+    document.querySelector('#cor2').style.backgroundColor = 'blueviolet'
+    document.querySelector('#cor3').style.backgroundColor = 'red'
+    document.querySelector('#cor4').style.backgroundColor = 'royalblue'
     document.querySelector('#cor1').className = "color selected"
+    document.body.addEventListener ('click', function(event){
+        if (event.target.className === 'color') {
+            document.querySelector('.selected').className = 'color'
+            event.target.className = 'color selected'
+        }
+    }
+    )
     document.body.addEventListener ('click', function (event){
         if (event.target.className === 'pixel') {
             event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor
         }
-    })
+    }
+    )
 }
