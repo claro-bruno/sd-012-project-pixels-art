2d41566d4a43161cc68fd4e06e158635789d4386
Ana Laura <analauraalves25@gmail.com>
2021-05-12 12:52:28 -0300

    requisito 2 e 12

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let randomColor = document.querySelectorAll("color");
+// Código para gerar cor de fundo aleátoria:
+// Referência: 
+// https://stackoverflow.com/questions/1484506/random-color-generator
 
-console.log(randomColor.target.style.backgroundColor = "#" + Math.ceil(Math.random() * 1000)); 
+const paletaDeCores = document.querySelectorAll('.color');
 
+window.onload = () => {
+  const blackColor = document.querySelector('#c1');
+  blackColor.classList.add('selected');
+  function randomColors() {
+    const orange = Math.random() * 255;
+    const blue = Math.random() * 255;
+    const grey = Math.random() * 255;
 
+    return `rgb(${orange},${blue},${grey})`;
+  }
+
+  for (let color = 1; color < paletaDeCores.length; color += 1) {
+    paletaDeCores[color].style.backgroundColor = randomColors();
+  }
+};
