bdf83e94a31ecdb41e122266d049e54c253fdfaf
Darciano Utzig <darciano@utzig.com.br>
2021-05-12 15:20:20 -0300

    realiza requisito 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let cores = document.querySelectorAll(".color")
+cores[0].getElementsByClassName.background = "black";
+cores[0].getElementsByClassName.background = "blue";
+cores[0].getElementsByClassName.background = "orange";
+cores[0].getElementsByClassName.background = "ciano";
