a33043410727a61799185c629370ea5baa366450
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-12 14:24:47 -0300

    1234

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ let quadradinho = document.getElementsByClassName('pixel');
 let botao1 = document.getElementById('clear-board');
     botao1.addEventListener('click',function (){
     for (let index = 0; index < quadradinho.length; index +=1){
-        quadradinho[index].style.backgroundColor = 'rgb(0, 0, 255)';
+        quadradinho[index].style.backgroundColor = 'rgb(255, 255, 255)';
     }
     })
