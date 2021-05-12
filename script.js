985330a2d3abd22596ac5f0d569ed04714864658
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-12 19:00:25 -0300

    Conserto dos erros  do addEventListener e do indice3

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 function pallet(palletNumber) {
+  let pixBoard = document.getElementById("pixel-board");
   let allPixels = palletNumber * palletNumber;
-  for (indice1 = 0; indice1 < allPixels; indice1 += 1) {
+  for (indice0 = 0; indice0 < allPixels; indice0 += 1) {
     let bornPixel = document.createElement("div");
     pixBoard.appendChild(bornPixel);
-    pixBoard.children[indice1].className = "pixel";
+    pixBoard.children[indice0].className = "pixel";
   }
 }
-let pixBoard = document.getElementById("pixel-board");
 pallet(5);
 
 function selection(event) {
-  for (let indice = 0; indice < colorSelection.length; indice += 1) {
-    colorSelection[indice].classList.remove('selected');
+  for (let indice1 = 0; indice1 < colorSelection.length; indice1 += 1) {
+    colorSelection[indice1].classList.remove('selected');
   }
   event.target.classList.add('selected');
 };
 
 let colorSelection = document.getElementsByClassName("color");
 let memberSelected = document.getElementsByClassName("selected");
-colorSelection.addEventListener("click", selection);
+for (let indice2 = 0; indice2 < colorSelection.length; indice2+=1) {
+  colorSelection[indice2].addEventListener("click", selection);
+}
 
-function changeColor(event){
- event.target.style.color == memberSelected[0].style.color;
+function changeColor(event) {
+  event.target.style.color == memberSelected[0].style.color;
 };
 
-pixBoard.addEventListenner("click", changeColor);
+let membersPix = document.getElementsByClassName("pixel");
+for(let indice3=0; indice3<membersPix.length; indice3+=1){
+  console.log(membersPix.length)
+  membersPix[indice3].addEventListener("click", changeColor);
+}
+
