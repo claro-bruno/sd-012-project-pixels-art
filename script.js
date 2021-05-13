6d4a6a3726c7acf26b27d412497e9e6223bd8c64
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-05-13 17:52:45 -0300

    corrige script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
 let quadrado = document.getElementById('pixel-board');
 function board(num) {
-    for (let index = 0; index < num; index += 1) {
-        let linha = document.createElement('div')
-        quadrado.appendChild = (linha);
-        for (let index = 0; index < num; index +=1) {
+    for(let index = 0; index < num; index += 1) {
+        let linha = document.createElement('div');
+        quadrado.appendChild(linha);
+    for(let index = 0; index < num; index +=1) {
         let coluna = document.createElement('div')
         coluna.className = 'pixel';
-        quadro.appendChild(coluna);
+        quadrado.appendChild(coluna);
     }
-  }
+}
 }
 board(5);
