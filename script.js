e68c264e85dcfb3a4b2b49bfc30ec60ae2a8d2d1
Gutemberg <gttj2010@hotmail.com>
2021-05-13 12:08:22 -0300

    Requisitos 1 ao 4. Refatoraçao 3.

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let colorPalette = document.querySelector('#color-palette');
 let colors = ['black', 'red', 'green',  'blue'];
 let ident = '';
 
-for (let index = 0; index < colors.length; index += 1) {    
+for (let index = 0; index < colors.length; index += 1) {
     let div = document.createElement('div');
     div.className = 'color ' + colors[index];
     colorPalette.appendChild(div);
 }
  
-for (let index = 1; index <= 15; index += 1) {    
-    let div = document.createElement('div');  
+for (let index = 1; index <= 15; index += 1) {
+    let div = document.createElement('div');
     div.className = 'pixel';
     
     if (index > 10) {
@@ -20,5 +20,5 @@ for (let index = 1; index <= 15; index += 1) {
         ident = '#secao1';
     }
     let pixelBoard = document.querySelector(ident);
-    pixelBoard.appendChild(div);   
+    pixelBoard.appendChild(div);
 }
