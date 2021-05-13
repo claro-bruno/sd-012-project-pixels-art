b4be2dc0c97504e9e9e7f85ef888459e2e7eef78
Pedro Resck <pedroresck1@gmail.com>
2021-05-12 21:48:10 -0300

    Desafio 8: resolvido

--- script.js
+++ script.js
@@ -6,23 +6,36 @@ function board(tamanhoBoard) {
   for (let index = 0; index < tamanhoBoard; index += 1) {
     let linha = document.createElement("div");
     quadroPixels.appendChild(linha);
-  for (let index = 0; index < tamanhoBoard; index += 1) {
-    let coluna = document.createElement("div");
-    coluna.className = "pixel";
-    quadroPixels.appendChild(coluna);
+    for (let index = 0; index < tamanhoBoard; index += 1) {
+      let coluna = document.createElement("div");
+      coluna.className = "pixel";
+      quadroPixels.appendChild(coluna);
+    }
   }
-}
 }
 board(5);
 
 // Resolvendo: DESAFIO 6 / DESAFIO 7 
 
 let selecionaCor = document.getElementsByClassName("color");
-for(index = 0; index < selecionaCor.length; index += 1){
-  selecionaCor[index].addEventListener("click", function(event){
+for (index = 0; index < selecionaCor.length; index += 1) {
+  selecionaCor[index].addEventListener("click", function (event) {
     let elemento = document.querySelector(".selected");
     elemento.classList.remove("selected");
-    event.target.classList.add("selected");  
+    event.target.classList.add("selected");
   })
 }
+
+
+// Resolvendo: DESAFIO 8
+let quadradinho = document.querySelectorAll(".pixel");
+for(let key of quadradinho){
+  key.addEventListener("click", function(event){
+    let corSelecionada = document.getElementsByClassName("selected")[0];
+    let temCor = window.getComputedStyle(corSelecionada).getPropertyValue("background-color");
+    event.target.style.backgroundColor = temCor;  
+  })
+}
+
+
 
