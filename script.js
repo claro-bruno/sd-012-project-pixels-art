41db6906212dd625723ec49f00b0b86cceb40869
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-13 14:23:11 -0300

    test1

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 let quadrado = document.getElementById('pixel-board');
 function quadrado25(num) {
+  for (let index = 0; index < num; index += 1) {
+    let linha = document.createElement('div');
+    quadrado.appendChild(linha);
     for (let index = 0; index < num; index += 1) {
-        let linha = document.createElement('div');
-        quadrado.appendChild(linha);
-        for (let index = 0; index < num; index += 1) {
-            let coluna = document.createElement('div')
-            coluna.className = 'pixel';
-            quadrado.appendChild(coluna);
-        }
+      let coluna = document.createElement('div')
+      coluna.className = 'pixel';
+      quadrado.appendChild(coluna);
     }
+  }
 }
 quadrado25(5)
 
 let initColor = document.querySelectorAll('.pixel');
-    for (let index = 0 ; index < initColor.length; index +=1){
-    initColor[index].addEventListener('click', function (event) {
-       event.target.style.backgroundColor = 'black';
+for (let index = 0; index < initColor.length; index += 1) {
+  initColor[index].addEventListener('click', function (event) {
+    event.target.style.backgroundColor = 'black';
 
-});
+  });
 }
+let selecionador = document.getElementsByClassName("color");
+for (let index = 0; index < selecionador.length; index += 1) {
+  selecionador[index].addEventListener('click', function (event) {
+    let elemento = document.querySelector('.selected');
+    elemento.classList.remove('selected');
+    event.target.classList.add('selected');
+  })
+}
+
 let quadradinho = document.getElementsByClassName('pixel');
 let botao1 = document.getElementById('clear-board');
-    botao1.addEventListener('click',function (){
-    for (let index = 0; index < quadradinho.length; index +=1){
-        quadradinho[index].style.backgroundColor = 'white';
-    }
-    })
+botao1.addEventListener('click', function () {
+  for (let index = 0; index < quadradinho.length; index += 1) {
+    quadradinho[index].style.backgroundColor = 'white';
+  }
+})
