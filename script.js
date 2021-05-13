1a772f959c52232aeeb75e350ee06f1a431b1805
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-05-13 18:49:33 -0300

    Finaliza requisitos

--- script.js
+++ script.js
@@ -1,1 +1,96 @@
-let a = 50;
+const paletaDeCores = document.querySelectorAll('.color');
+
+window.onload = () => {
+
+    const blackColor = document.querySelector('#fs-color');
+
+    blackColor.classList.add('selected');
+
+    function randomColors() {
+
+        const orange = Math.random() * 255;
+        const blue = Math.random() * 255;
+        const grey = Math.random() * 255;
+
+        return `rgb(${orange},${blue},${grey})`;
+    }
+
+    for (let color = 1; color < paletaDeCores.length; color += 1) {
+
+        paletaDeCores[color].style.backgroundColor = randomColors();
+
+    }
+};
+
+// Funcao que cria o pixel-board
+const pixelBoard = document.querySelector('#pixel-board');
+
+function pixelBoardColunas(linhas, colunas) {
+
+    for (let coluna = 1; coluna <= colunas; coluna += 1) {
+
+        const pixelColuna = document.createElement('div');
+
+        pixelColuna.classList.add('pixel');
+
+        linhas.appendChild(pixelColuna);
+
+    }
+}
+
+function pixelBoardLinhas(linhas) {
+
+    for (let linha = 1; linha <= linhas; linha += 1) {
+
+        const pixelLinha = document.createElement('div');
+
+        pixelBoard.appendChild(pixelLinha);
+
+        pixelBoardColunas(pixelLinha, linhas);
+
+    }
+}
+
+pixelBoardLinhas(5);
+
+// Funcao que seleciona a cor e da as cores ao board
+let cor = 'black';
+
+function selecionaCor(corSelecionada) {
+
+    const corEscolhida = corSelecionada.target;
+
+    if (corEscolhida.classList.contains('pixel')) {
+
+        corEscolhida.style.backgroundColor = cor;
+
+    } else if (corEscolhida.classList.contains('color')) {
+
+        document.querySelector('.selected').className = ('color');
+
+        corEscolhida.className = ('color selected');
+
+        cor = window.getComputedStyle(corEscolhida).backgroundColor;
+
+    }
+}
+
+document.addEventListener('click', selecionaCor);
+
+// Funcao que reseta o board
+function resetBoard() {
+
+    const button = document.querySelector('#clear-board');
+
+    button.addEventListener('click', () => {
+
+        const pixelSquare = document.querySelectorAll('.pixel');
+
+        for (let index = 0; index < pixelSquare.length; index += 1) {
+
+            pixelSquare[index].style.backgroundColor = 'white';
+
+        }
+    });
+}
+resetBoard();
