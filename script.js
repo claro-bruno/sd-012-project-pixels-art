91477ce1eaf7c26407bf9eeb5ef8fba148b1b9af
Emmanuel <acquabriall2020@gmail.com>
2021-05-14 02:37:28 -0300

    resolve o 8 e 9 quase 100%

--- script.js
+++ script.js
@@ -14,13 +14,24 @@ for (let indexTr = 0; indexTr < 5; indexTr += 1) {
 
 let getClassColor = document.querySelectorAll('.color');
 
+
 for (let indexClassColor = 0; indexClassColor < getClassColor.length; indexClassColor += 1) {
-    getClassColor[indexClassColor].addEventListener('click', function (event){
+    getClassColor[indexClassColor].addEventListener('click', function(event){
         let getFirstSelection = document.getElementsByClassName('selected')[0];
         getFirstSelection.classList.remove('selected');
         event.target.classList.add('selected');
+        let getSelectedColor = document.querySelectorAll('.selected');          //pega cor selecionada
+        let getSelectedActualColor = window.getComputedStyle(getSelectedColor[0]).getPropertyValue('background-color'); //pega o valor da cor adicionada
+        let getPixel = document.querySelectorAll('.pixel');     //pega todos os elementos pixels
+            for (let mudaCorPixel = 0; mudaCorPixel  < getPixel.length; mudaCorPixel += 1) {
+                getPixel[mudaCorPixel].addEventListener('click', function(){
+                    getPixel[mudaCorPixel].style.backgroundColor = getSelectedActualColor;
+                })
+                
+            }
     });
 };
+
 
 let getButton = document.getElementById('clear-board');
 let getPixelsId = document.querySelectorAll('.pixel');
