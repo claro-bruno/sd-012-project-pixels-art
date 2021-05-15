2d60501824aa253aba943bdc113b0fcff299c0fd
igor <igorsife80@gmail.com>
2021-05-15 11:41:00 -0300

    obg rafa te amo

--- script.js
+++ script.js
@@ -21,62 +21,64 @@ function colorPixel() {
       event.target.style.backgroundColor = selectedColor;
     });
   };
-const botaoLimpar = document.getElementById('clear-board');
-botaoLimpar.addEventListener('click', function () {
+  const botaoLimpar = document.getElementById('clear-board');
+  botaoLimpar.addEventListener('click', function () {
     let pixels = document.getElementsByClassName('pixel');
-  for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].style.backgroundColor = 'white';
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    };
+  });
+
+  const board = document.querySelector('#pixel-board');
+  const input = document.querySelector('#board-size');
+  const generateBoardButton = document.querySelector('#generate-board');
+  generateBoardButton.addEventListener('click', function () {
+    if (!input.value) {
+      alert("Board inválido!");
+    }
+    if (input.value < 5) {
+      input.value = 5;
+    }
+    if (input.value > 50) {
+      input.value = 50;
+    }
+    removeOldBoard();
+    for (let index = 0; index < input.value; index += 1) {
+      let row = criandoLinhas(input.value);
+      board.appendChild(row);
+    };
+    colorPixel();
+  });
+
+
+  for (let index = 1; index < colors.length; index += 1) {
+    colors[index].style.backgroundColor = generateRandomColor();
   };
-});
 
-const board = document.querySelector('#pixel-board');
-const input = document.querySelector('#board-size');
-const generateBoardButton = document.querySelector('#generate-board');
-generateBoardButton.addEventListener('click', function () {
-  if (!input.value) {
-    alert("Board inválido!");
-  }
-  if (input.value < 5) {
-    input.value = 5;
-  }
-  if (input.value > 50) {
-    input.value = 50;
-  }
-  removeOldBoard();
-  for (let index = 0; index < input.value; index += 1) {
-    let row = criandoLinhas(input.value);
-    board.appendChild(row);
+  function generateRandomColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let color = `rgb(${r} ,${g} ,${b})`;
+    return color;
   };
-  colorPixel();
-});
 
-
-for (let index = 1; index < colors.length; index += 1) {
-  colors[index].style.backgroundColor = generateRandomColor();
-};
-function generateRandomColor() {
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
-  let color = `rgb(${r} ,${g} ,${b})`;
-  return color;
-};
-
-function criandoLinhas(numberOfPixels) {
-  let row = document.createElement('tr');
-  for (let index = 0; index < numberOfPixels; index += 1) {
-    let col = document.createElement('td');
-    col.className = 'pixel';
-    row.appendChild(col);
+  function criandoLinhas(numberOfPixels) {
+    let row = document.createElement('tr');
+    for (let index = 0; index < numberOfPixels; index += 1) {
+      let col = document.createElement('td');
+      col.className = 'pixel';
+      row.appendChild(col);
+    }
+    return row;
   }
-  return row;
-}
 
-function removeOldBoard() {
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
+  function removeOldBoard() {
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
+    };
   };
-};
+
   function removeOldBoard() {
     while (board.firstChild) {
       board.removeChild(board.firstChild);
