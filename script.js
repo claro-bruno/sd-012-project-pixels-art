6250f66ce4d8d60e3b8b087cfafa6f4ba8fb8dfc
Darciano Utzig <darciano@utzig.com.br>
2021-05-19 15:00:47 -0300

    resolve requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-let cores = document.querySelectorAll(".color")
-cores[0].getElementsByClassName.background = "black";
-cores[0].getElementsByClassName.background = "blue";
-cores[0].getElementsByClassName.background = "orange";
-cores[0].getElementsByClassName.background = "ciano";
+console.log("oi")
