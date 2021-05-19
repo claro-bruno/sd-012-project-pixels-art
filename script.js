f95bcdb676060a17a95a8de8020387bd1cc6752b
Yuri <yurifranco18@live.com>
2021-05-19 20:00:31 -0300

    foi consertado algumas coisas ate o req. 8

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ function selecionar () {
 
 selecionar();
 
-// pegar os pixels
 const pixels = document.querySelectorAll('.pixel')
-// adicionar um elemento de click para cada um dos pixels
+
 for (index = 0; index < pixels.length; index +=1) {
     pixels[index].addEventListener('click', addColor)
 }
-//
+
 function addColor(event) {
     const selecionaCor = document.getElementsByClassName('selected')[0];
     const cor = window.getComputedStyle(selecionaCor);
