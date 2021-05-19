446b937484f03cd974230d693dc88adbaeb486a1
Yuri <yurifranco18@live.com>
2021-05-19 19:59:32 -0300

    foi feito req. 8

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function selecionar () {
 }
 
 selecionar();
+
+// pegar os pixels
+const pixels = document.querySelectorAll('.pixel')
+// adicionar um elemento de click para cada um dos pixels
+for (index = 0; index < pixels.length; index +=1) {
+    pixels[index].addEventListener('click', addColor)
+}
+//
+function addColor(event) {
+    const selecionaCor = document.getElementsByClassName('selected')[0];
+    const cor = window.getComputedStyle(selecionaCor);
+    event.target.style.backgroundColor = cor.backgroundColor
+}
