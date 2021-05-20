8d2b10350b90e7c9126f7c201b7c8dcece43cbbd
Darciano Utzig <darciano@utzig.com.br>
2021-05-19 22:09:49 -0300

    inicia resolucao requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("oi")
+// 7.. capturar os elementos classe color
+// tem 4 elementos (for para acessar cada um dos elementos)
+// adicionar evento de clique (addEventList...) em cada elemento
+// evento precisa capturar o elemento com a classe selected
+// remover classe selected do elemento
+// adicionar classe selected ao elemento selecionado.
+let selecionaCor = document.querySelector('color');
+
+function capturaClasseColor() {
+    for (let indice = 0; indice < selecionaCor.length ; indice += 1) {
+        selecionaCor[indice].addEventListener('click', seleciona);
+    }
+};
+
+function seleciona(event) {
+    let selecionado = document.querySelector('selected');
+    selecionado.classList.remove('selected');
+    event.target.classList.add('selected');
+};
+
+
+//8.. capturar todos os pixels "brancos"
+// for na lista que retornar e adicionar um evento de clique para cada pixel
+// o evento de clique tem q pegar o elemento com a classe selected e descobrir a cor dele
+// adicionar essa cor como bg-color do pixel
+// funciona como o requisito 7 - ppega lista de elementos e faz for na lista
+// pegar elemento com a cor selecionada e a cor e atribuir para o pixel
+// atraves do evento (addEvent...)
