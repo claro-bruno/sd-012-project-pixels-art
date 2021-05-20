2336f2e0b1f0f4e889eb130b3f89e33e5bfb95f7
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-05-20 11:37:14 -0300

    requisito 7

--- script.js
+++ script.js
@@ -20,19 +20,20 @@ function criandoColuna (row, coluna){
 
 criandoLinha(quadrados)
 
-//1.capturar os elementos da paleta de cores
+
 let paletaDeCores = document.getElementsByClassName('color')
 
-//2. Colocar um for para rodar o passo 1 e depois adicionar um eventli.. click
-for(let index = 0; index <= paletaDeCores; index += 1){
-    paletaDeCores.addEventListener('click', myFunction)
+
+for(let index = 0; index <= paletaDeCores.length; index += 1){
+    paletaDeCores[index].addEventListener('click', myFunction)
 }
 
-//3. capturar um elemento com a classe selected e remover esse class no elemento depois de clicar na cor deve estar dentro evento click passo 2
-function myFunction (){
-    
+
+function myFunction (event){
+    let selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected')
 }
 
-//4. Adicionar a classe selected para elemento que foi clicado
-  
+
 
