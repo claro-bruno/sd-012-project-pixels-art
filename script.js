0b65b47dc40ad073d84f26719b07bbe2290e0ac2
Wadson <wadson.dev92@gmail.com>
2021-05-19 23:36:30 -0300

    identaçao ok

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let quadro = document.getElementById('pixel-board');
 
-function board(num){
+function board(num) {
   //for(let cont = 0; cont < num; cont += 1)
-  for(let i = 0; i < num; i += 1){
+  for (let i = 0; i < num; i += 1) {
     let linha = document.createElement('div');
     quadro.appendChild(linha);
-      for(let index = 0; index < num; index += 1){
-        let pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        linha.appendChild(pixel);
-      }
+    for (let index = 0; index < num; index += 1) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      linha.appendChild(pixel);
+    }
   }
 
 }
@@ -22,21 +22,21 @@ board(5);
 
 
 let trocaCorPaleta = document.querySelector('#pixel-board')
-trocaCorPaleta.addEventListener('click', function(event){
-  if(event.target.className === 'pixel'){
+trocaCorPaleta.addEventListener('click', function (event) {
+  if (event.target.className === 'pixel') {
     let selectedColor = document.querySelector('.selected');
     let cor = window.getComputedStyle(selectedColor).backgroundColor;
     event.target.style.backgroundColor = cor;
   }
-  });
+});
 // Primeiro capturar os elementos da paleta de cores, posso usar o getElementByclassName ou querySelectorAll
 // Atribuir o evento de clique para cada um dos elementos, através de um "for"
 // Meu evento de clique precisa capturar o elemento que possui a classe selected, e depois deletar a classe selected desse elemento anterior
 // Atribuir a classe selected ao elemento clicado
 
 let paleta = document.querySelectorAll('.color');
-for(let index = 0; index < paleta.length; index +=1){
-  paleta[index].addEventListener('click', function(event){
+for (let index = 0; index < paleta.length; index += 1) {
+  paleta[index].addEventListener('click', function (event) {
     let selectedColor = document.querySelector('.selected')
     selectedColor.classList.remove('selected')
     event.target.classList.add('selected')
@@ -45,8 +45,8 @@ for(let index = 0; index < paleta.length; index +=1){
 }
 
 
-document.body.addEventListener('click', function(event){
-  if(event.target.getElementById === 'clear-board'){
+document.body.addEventListener('click', function (event) {
+  if (event.target.getElementById === 'clear-board') {
     event.target.style.backgroundColor = 'white';
   }
 });
