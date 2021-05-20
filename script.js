0cef90bc8aaa396e7da7f592b8270242772a6f7d
Wadson <wadson.dev92@gmail.com>
2021-05-19 23:44:15 -0300

    identaçao arquivo script

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
-let quadro = document.getElementById('pixel-board');
+const quadro = document.getElementById('pixel-board');
 
 function board(num) {
-  //for(let cont = 0; cont < num; cont += 1)
   for (let i = 0; i < num; i += 1) {
-    let linha = document.createElement('div');
+    const linha = document.createElement('div');
     quadro.appendChild(linha);
     for (let index = 0; index < num; index += 1) {
-      let pixel = document.createElement('div');
+      const pixel = document.createElement('div');
       pixel.className = 'pixel';
       linha.appendChild(pixel);
     }
   }
-
 }
 board(5);
 
-// Em primeiro lugar eu preciso selecionar meu elemento color-palette, usando getElementById ou querySelector
-// Atribuir o evento de clique para que eu selecione a cor desejada
-// Ao clicar em qualquer cor da paleta faz com que minha cor seja trocada
-// Ao clicar nos pixels faz com que
-
-
-let trocaCorPaleta = document.querySelector('#pixel-board')
+const trocaCorPaleta = document.querySelector('#pixel-board')
 trocaCorPaleta.addEventListener('click', function (event) {
   if (event.target.className === 'pixel') {
-    let selectedColor = document.querySelector('.selected');
-    let cor = window.getComputedStyle(selectedColor).backgroundColor;
+    const selectedColor = document.querySelector('.selected');
+    const cor = window.getComputedStyle(selectedColor).backgroundColor;
     event.target.style.backgroundColor = cor;
   }
 });
-// Primeiro capturar os elementos da paleta de cores, posso usar o getElementByclassName ou querySelectorAll
-// Atribuir o evento de clique para cada um dos elementos, através de um "for"
-// Meu evento de clique precisa capturar o elemento que possui a classe selected, e depois deletar a classe selected desse elemento anterior
-// Atribuir a classe selected ao elemento clicado
 
-let paleta = document.querySelectorAll('.color');
+const paleta = document.querySelectorAll('.color');
 for (let index = 0; index < paleta.length; index += 1) {
   paleta[index].addEventListener('click', function (event) {
-    let selectedColor = document.querySelector('.selected')
+    const selectedColor = document.querySelector('.selected')
     selectedColor.classList.remove('selected')
     event.target.classList.add('selected')
-
   });
 }
-
 
 document.body.addEventListener('click', function (event) {
   if (event.target.getElementById === 'clear-board') {
