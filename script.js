dae0ed032bc04879cef9e01238a0d98f855ebeb5
Darciano Utzig <darciano@utzig.com.br>
2021-05-23 00:36:52 -0300

    requisito bonus 10

--- script.js
+++ script.js
@@ -13,17 +13,6 @@ function capturaClasseColor() {
 }
 capturaClasseColor();
 
-//Desafio 8
-
-let selecionaPixel = document.getElementsByClassName('pixel');
-for (let index = 0; index < selecionaPixel.length ; index +=1){
-  selecionaPixel[index].addEventListener('click', pintar);
-}
-function pintar(event) {
-  let corSelecionado = document.querySelector('.selected');
-  let pintarPixel = window.getComputedStyle(corSelecionado).getPropertyValue("background-color");
-  event.target.style.backgroundColor = pintarPixel;
-}
 
 //Desafio 9
 
@@ -38,3 +27,54 @@ function limpar() {
   }
 }
 clearBoard();
+
+//Desafio 10
+function novaTabelaDinamica(number) {
+  let tabela = document.getElementById('pixel-board');
+  tabela.innerHTML = '';
+  for (let linha = 0; linha < number; linha += 1) {
+    let tr = document.createElement('tr');
+    for (let col = 0; col < number; col += 1) {
+      let th = document.createElement('th');
+      th.className = 'pixel';
+      tr.appendChild(th);
+    }
+    tabela.appendChild(tr);
+  }
+
+  let selecionaPixel = document.getElementsByClassName('pixel');
+  for (let index = 0; index < selecionaPixel.length ; index +=1){
+    selecionaPixel[index].addEventListener('click', pintar);
+  }
+  function pintar(event) {
+    let corSelecionado = document.querySelector('.selected');
+    let pintarPixel = window.getComputedStyle(corSelecionado).getPropertyValue("background-color");
+    event.target.style.backgroundColor = pintarPixel;
+  }
+}
+novaTabelaDinamica(5)
+
+function tabelaValida() {
+  let number = document.getElementById('board-size').value;
+
+  if (number === '') {
+    alert('Board inválido!');
+  } else {
+    novaTabelaDinamica(number);
+  }
+}
+
+let botaoVQV = document.getElementById('generate-board');
+botaoVQV.addEventListener('click', tabelaValida);
+
+//Desafio 8
+
+// let selecionaPixel = document.getElementsByClassName('pixel');
+// for (let index = 0; index < selecionaPixel.length ; index +=1){
+//   selecionaPixel[index].addEventListener('click', pintar);
+// }
+// function pintar(event) {
+//   let corSelecionado = document.querySelector('.selected');
+//   let pintarPixel = window.getComputedStyle(corSelecionado).getPropertyValue("background-color");
+//   event.target.style.backgroundColor = pintarPixel;
+// }
