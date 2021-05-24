ad5a205b5c2d50c0fbcd599010e53bba73c9c1a9
Aldria Melo <aldriamelo.academico@gmail.com>
2021-05-24 20:23:36 -0300

    adiciona requisito 08

---
+++ script.js
@@ -0,0 +1,21 @@
+// capturar todos os elementos da class color 
+// adicionar evento de click a cada um dos elementos do primeiro passo 
+// capturar o elemento com a classe select dentro da função click 
+// remover a classe select do elemento selecionado 
+// adicionar a classe select ao elemento clicado
+
+let capturaCor = document.getElementsByClassName('color')
+for(let index = 0; index < capturaCor.length; index +=1){
+    capturaCor[index].addEventListener('click',alteraCor)
+}
+function alteraCor(event){
+    let capturaSelect = document.querySelector('.selected')
+    capturaSelect.classList.remove("selected")
+   event.target.classList.add('selected')
+}
+
+// capturar todos os elementos com a class pixel 
+// rodar o for para add undefined, evendo de  click para cada pixel
+// dentro da função --- selecionar o elemento com a class selected
+// pegar a cor de fundo do elemento selecionado 
+// adicionar a cor de fundo ao elemento clicado 
