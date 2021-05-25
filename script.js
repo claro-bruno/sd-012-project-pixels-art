ea2487dcc0f0f1dbf082d1b7589024310c3511ad
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-24 23:12:08 -0300

    reviso ultimo

--- script.js
+++ script.js
@@ -7,65 +7,40 @@ let frame = document.getElementById("pixel-board");;
     startcolor.style.backgroundColor = "black";
 };
  
-   //função cria 25 frames
-   function frameBoard (size) {
-    for ( let index=0 ; index < size; index +=1) {
+ //função cria 25 frames
+function frameBoard (size) {
+  for ( let index=0 ; index < size; index +=1) {
      let linha = document.createElement("div");
       frame.appendChild(linha);
         for ( let index=0 ; index < size; index +=1) {
         let pixel = document.createElement("div");
-        pixel.className = "pixel"  
-        pixel.style.backgroundColor = "white" 
+        pixel.className = "pixel";  
+        
         linha.appendChild(pixel);      
-      }
+    }
     }
     }  
   frameBoard(5); 
 
   
+  document.addEventListener('click', function (event) {
+    let selectColor = document.querySelector(".selected");
+    if (event.target.classList.contains('color1')) {        
+        selectColor.classList.toggle("selected");
+        event.target.classList.toggle("selected");
+     };
+    if (event.target.classList.contains('color2')) {        
+        selectColor.classList.toggle("selected");
+        event.target.classList.toggle("selected");
+      };
+    if (event.target.classList.contains('color3')) {       
+        selectColor.classList.toggle("selected");
+        event.target.classList.toggle("selected");
+      };
+    if (event.target.classList.contains('color4')) {       
+        selectColor.classList.toggle("selected");
+        event.target.classList.toggle("selected");
+      };
+  })
+
   
-
-
-
- 
-// function OneSelected (event) {
-//   let selecionada = document.querySelector(".selected");
-//   selecionada.classList.remove("selected");
-//     event.target.className.add("color selected");
-// };
-
-// let listaDasCores = document.querySelectorAll(".color");
-
-// for (let index = 0; index < listaDasCores.length; index += 1) {
-//     listaDasCores[index].addEventListener("click", OneSelected);
-// };
-
-
-// botao limparAll 
-
-
-
-
-const btnLimpar = document.getElementById('clear-board')
-
-btnLimpar.addEventListener('click', () => {
-let limpaPixels = document.querySelectorAll(".pixel");
-
-  for(let index = 0; index < limpaPixels.length; index += 1) {
-      limpaPixels[index].style.backgroundColor = "white";
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
