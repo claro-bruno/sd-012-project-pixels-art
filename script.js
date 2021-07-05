afc146331761c08dddf815f556d6d2bb9fc22dc3
Eder Santos <3derms@gmail.com>
2021-07-04 22:12:43 -0300

    Resolve erros - exceto de atribuiçao ao target

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-let corSelecionada = 'black'
+let corSelecionada = 'black';
 
-document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('color')){
-       let selecionaClasse = document.querySelector('.selected');
-       selecionaClasse.classList.remove('selected');
-       event.target.classList.add('selected');
-       corSelecionada = event.target.style.backgroundColor;
+document.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('color')) {
+    const selecionaClasse = document.querySelector('.selected');
+    selecionaClasse.classList.remove('selected');
+    target.classList.add('selected');
+    corSelecionada = target.style.backgroundColor;
+  }
+});
+
+document.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('pixel')) {
+    target.style.backgroundColor = corSelecionada;
+  }
+});
+
+document.addEventListener('click', ({ target }) => {
+  if (target.id === 'clear-board') {
+    const quadro = document.querySelector('#pixel-board');
+    const pixels = quadro.children;
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
     }
- })
-
- document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('pixel')){
-       event.target.style.backgroundColor = corSelecionada;
-    }
- })
-
- document.addEventListener('click', (event) => {
-    if (event.target.id === 'clear-board'){
-       const quadro = document.querySelector('#pixel-board')
-       const pixels = quadro.children
-        for (let index = 0; index < pixels.length; index += 1) {
-            pixels[index].style.backgroundColor = 'white';
-        }
-
-    }
- })
-
-
- 
+  }
+});
